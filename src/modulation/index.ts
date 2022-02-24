d205f94ccf3622a724741129fc95099bbc309e87
CH <clint@thestaticvoid.net>
2022-02-24 14:24:18 +0100

    Updates

--- src/modulation/index.ts
+++ src/modulation/index.ts
@@ -1,6 +1,19 @@
-// import * as Easing from './Easing.js';
-// import * as Envelope from './Envelope.js';
-// export {Easing, Envelope};
+/**
+ * Easings module
+ * 
+ * Overview:
+ * * {@link Easings.time} - Ease by time
+ * * {@link Easings.tick} - Ease by tick
+ * * {@link Easings.get} - Get an easing function by name
+ */
 export * as Easings from './Easing.js';
+
+/**
+ * Envelope
+ */
 export * from './Envelope.js';
+
+/**
+ * Oscillator
+ */
 export * as Oscillators from './Oscillator.js';
