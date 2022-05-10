a45ef563da9f8b09b0e7d8be320f798a87c8bfd4
CH <clint@thestaticvoid.net>
2022-05-10 13:11:52 +0200

    Export intervalTracker

--- src/temporal/index.ts
+++ src/temporal/index.ts
@@ -1,4 +1,4 @@
 export * as Normalise from './Normalise.js';
 export * from './FrequencyMutable.js';
 export * from './MovingAverage.js';
-export {tracker} from './Tracker.js';
+export {tracker, intervalTracker} from './Tracker.js';
