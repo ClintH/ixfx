bbf426de3cd644629143907c2344e2c301d4e59c
CH <clint@thestaticvoid.net>
2022-06-22 18:20:10 +0200

    relativeDifference

--- src/Util.ts
+++ src/Util.ts
@@ -528,6 +528,24 @@ export const wrapRange = (min:number, max:number, fn:(distance:number)=>number, 
  */
 export const isPowerOfTwo = (x:number) => Math.log2(x) % 1 === 0;
 
+/**
+ * Returns the relative difference from the `initial` value
+ * ```js
+ * const rel = relativeDifference(100);
+ * rel(100); // 1
+ * rel(150); // 1.5
+ * rel(50);  // 0.5
+ * ```
+ * 
+ * The code for this is simple:
+ * ```js
+ * const relativeDifference = (initial) => (v) => v/initial
+ * ```
+ * @param {number} initial 
+ * @returns 
+ */
+export const relativeDifference = (initial:number) => (v:number) => v/initial;
+
 
 export const runningiOS = () => [
   `iPad Simulator`,
