2cf5ec37927271711b156525b9c683e88995151f
CH <clint@thestaticvoid.net>
2022-07-31 17:21:21 +0200

    Modulation.jitter takes a random source in option object, rather than separate arg

--- src/modulation/index.ts
+++ src/modulation/index.ts
@@ -89,16 +89,18 @@ export {Oscillators};
 
 export type JitterOpts = {
   readonly type?: `rel`|`abs`,
-  readonly clamped?: boolean
+  readonly clamped?: boolean,
+  readonly random?: RandomSource
 }
 
 /**
  * Jitters `value` by the absolute `jitter` amount.
- * All values should be on a 0..1 scale, and the return value is by default clamped to 0..1. Pass `clamped:false` as an option
+ * All values should be on a 0..1 scale, and the return value is by default clamped to 0..1. 
+ * Pass `clamped:false` as an option
  * to allow for arbitary ranges.
  * 
  * ```js
- * import {jitter} from 'https://unpkg.com/ixfx/dist/modulation.js';
+ * import { jitter } from 'https://unpkg.com/ixfx/dist/modulation.js';
  * 
  * // Jitter 0.5 by 10% (absolute)
  * // yields range of 0.4-0.6
@@ -106,13 +108,13 @@ export type JitterOpts = {
  * 
  * // Jitter 0.5 by 10% (relative, 10% of 0.5)
  * // yields range of 0.45-0.55
- * jitter(0.5, 0.1, {type:`rel`});
+ * jitter(0.5, 0.1, { type:`rel` });
  * ```
  * 
  * You can also opt not to clamp values:
  * ```js
  * // Yields range of -1.5 - 1.5
- * jitter(0.5, 1, {clamped:false});
+ * jitter(0.5, 1, { clamped:false });
  * ```
  * 
  * A custom source for random numbers can be provided. Eg, use a weighted
@@ -120,7 +122,7 @@ export type JitterOpts = {
  * 
  * ```js
  * import {weighted} from 'https://unpkg.com/ixfx/dist/random.js';
- * jitter(0.5, 0.1, {}, weighted);
+ * jitter(0.5, 0.1, { random: weighted };
  * ```
  * 
  * Options
@@ -131,13 +133,13 @@ export type JitterOpts = {
  * @param value Value to jitter
  * @param jitter Absolute amount to jitter by
  * @param opts Jitter options
- * @param rand Source of random numbers, Math.random by default.
  * @returns Jittered value
  */
-export const jitter = (value:number, jitter:number, opts:JitterOpts = {}, rand:RandomSource = defaultRandom) => {
+export const jitter = (value:number, jitter:number, opts:JitterOpts = {}) => {
  
   const type = opts.type ?? `abs`;
   const clamped = opts.clamped ?? true;
+  const rand = opts.random ?? defaultRandom;
 
   guardNumber(value, clamped ? `percentage` : `bipolar`, `value`);
   guardNumber(jitter, clamped ? `percentage` :`bipolar`, `jitter`);
