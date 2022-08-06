31d294499b4dcbc7b2cf6de8614f988334efe0f7
CH <clint@thestaticvoid.net>
2022-08-06 17:51:01 +0200

    Added spring

--- src/modulation/Oscillator.ts
+++ src/modulation/Oscillator.ts
@@ -3,6 +3,95 @@
 import * as Timers from '../flow/Timer.js';
 
 const piPi = Math.PI*2;
+
+export type SpringOpts = {
+  /**
+   * Default: 1
+   */
+	readonly mass?: number; // = 1.0
+  /**
+   * Default: 10
+   */
+	readonly damping?: number; // = 10.0
+  /**
+   * Default: 100
+   */
+	readonly stiffness?: number; // = 100.0
+  /**
+   * Default: false
+   */
+	readonly soft?: boolean; // = false
+
+  readonly velocity?:number
+}
+
+const springRaw = (opts: SpringOpts = {}, from: number = 0, to: number = 1) => {
+  /** MIT License github.com/pushkine/ */
+  const mass = opts.mass ?? 1;
+  const stiffness = opts.stiffness ?? 100;
+  const soft = opts.soft ?? false;
+  const damping = opts.damping ?? 10;
+  const velocity = opts.velocity ?? 0.1;
+  const delta = to - from;
+  if (true === soft || 1.0 <= damping / (2.0 * Math.sqrt(stiffness * mass))) {
+    const angularFrequency = -Math.sqrt(stiffness / mass);
+    const leftover = -angularFrequency * delta - velocity;
+    return (t: number) => to - (delta + t * leftover) * Math.E ** (t * angularFrequency);
+  } else {
+    const dampingFrequency = Math.sqrt(4.0 * mass * stiffness - damping ** 2.0);
+    const leftover = (damping * delta - 2.0 * mass * velocity) / dampingFrequency;
+    const dfm = (0.5 * dampingFrequency) / mass;
+    const dm = -(0.5 * damping) / mass;
+    return (t: number) => to - (Math.cos(t * dfm) * delta + Math.sin(t * dfm) * leftover) * Math.E ** (t * dm);
+  }
+};
+
+/**
+ * Spring-style oscillation
+ * 
+ * ```js
+ * const spring = Oscillators.spring();
+ * 
+ * continuously(() => {
+ *  const v = spring.next().value;
+ *  // Yields values 0...1
+ *  //  undefined is yielded when spring is estimated to have stopped
+ * });
+ * ``` 
+ * 
+ * Parameters to the spring can be provided.
+ * ```js
+ * const spring = Oscillators.spring({
+ *  mass: 5,
+ *  damping: 10
+ *  stiffness: 100
+ * });
+ * ```
+ * @param opts 
+ * @param timerOrFreq 
+ */
+//eslint-disable-next-line func-style
+export function* spring(opts:SpringOpts = {}, timerOrFreq:Timers.Timer|undefined) {
+  if (timerOrFreq === undefined) timerOrFreq = Timers.msElapsedTimer();
+  
+  const fn = springRaw(opts, 0, 1);
+
+  // Give it 100 iterations to settle
+  //eslint-disable-next-line functional/no-let
+  let doneCountdown = 100;
+
+  //eslint-disable-next-line functional/no-loop-statement
+  while (doneCountdown > 0) {
+    const s = fn(timerOrFreq.elapsed/1000);
+    yield s;
+    if (s === 1) {
+      doneCountdown--;
+    } else {
+      doneCountdown = 100;
+    }
+  }
+}
+
 /**
  * Sine oscillator.
  * 
