26b233400562b7c2024fe748a67ea11c3b3d893c
CH <clint@thestaticvoid.net>
2022-11-02 15:41:45 +0100

    Pool export

--- src/data/index.ts
+++ src/data/index.ts
@@ -14,6 +14,7 @@ export * from './Flip.js';
 export * from './Interpolate.js';
 export * from './Wrap.js';
 export * as Correlate from './Correlate.js';
+export * as Pool from './Pool.js';
 
 export const piPi = Math.PI*2;
 export type NumberFunction = ()=>number;
