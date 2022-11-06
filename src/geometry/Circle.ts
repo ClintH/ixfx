47c74a291c9c00bef7dd894d7eb3ad1a10f14749
CH <clint@thestaticvoid.net>
2022-11-06 12:11:20 +0100

    Added toPositioned

--- src/geometry/Circle.ts
+++ src/geometry/Circle.ts
@@ -448,6 +448,25 @@ export const nearest = (circle:CirclePositioned|readonly CirclePositioned[], b:P
 };
 
 /**
+ * Returns a positioned version of a circle.
+ * If circle is already positioned, it is returned.
+ * If no default position is supplied, 0,0 is used.
+ * @param circle 
+ * @param defaultPositionOrX 
+ * @param y 
+ * @returns 
+ */
+export const toPositioned = (circle:Circle|CirclePositioned, defaultPositionOrX?:Points.Point|number, y?:number):CirclePositioned => {
+  if (isPositioned(circle)) return circle;
+
+  // Returns 0,0 if params are undefined
+  const pt = Points.getPointParam(defaultPositionOrX, y);
+  return Object.freeze({
+    ...circle,
+    ...pt
+  });
+};
+/**
  * Returns a `CircularPath` representation of a circle
  *
  * @param {CirclePositioned} circle
