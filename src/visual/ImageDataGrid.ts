8c43921252a2b452d1c57109fa41ae44dc2bd43a
CH <clint@thestaticvoid.net>
2023-01-02 23:05:19 +0100

    Create ImageDataGrid.ts

---
+++ src/visual/ImageDataGrid.ts
@@ -0,0 +1,22 @@
+import * as Grids from '../geometry/Grid.js';
+import { Rgb } from './Colour';
+
+export const accessor = (image:ImageData):Grids.CellAccessor<Rgb> => {
+  const grid = { rows: image.width, cols: image.height };
+  const data = image.data;
+
+  const fn:Grids.CellAccessor<Rgb> = (cell:Grids.Cell, bounds) => {
+    const index = Grids.indexFromCell(grid, cell, bounds);
+    if (index === undefined) {
+      return undefined;
+    } 
+    const pxIndex = index*4;
+    return {
+      r: data[pxIndex],
+      g: data[pxIndex+1],
+      b: data[pxIndex+2],
+      opacity: data[pxIndex+3]
+    };
+  };
+  return fn;
+};
