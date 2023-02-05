dcf70033d7266f880671c14a2ef785c768c7b2ab
CH <clint@thestaticvoid.net>
2023-02-05 10:37:58 +0100

    continuously improvements
    Now takes an options object:
       - `fireBeforeWait`: if true, callback is run before waiting, rather than the default of after the initial wait
       - `onStartCalled`: return value determines whether a call to .start() continues as normal, cancels, resets or disposes loop
       - disposable: if true, the loop will throw an error if a start is attempted

--- src/flow/Continuously.ts
+++ src/flow/Continuously.ts
@@ -1,42 +1,77 @@
-import {integer as guardInteger} from '../Guards.js';
-import {HasCompletion} from './index.js';
+import { integer as guardInteger } from '../Guards.js';
+import { HasCompletion } from './index.js';
 
 /**
  * Runs a function continuously, returned by {@link Continuously}
  */
 export type Continuously = HasCompletion & {
   /**
-   * Starts loop. If already running, it is reset
+   * Starts loop. If already running, does nothing
    */
-  start(): void
+  start():void
+
+  /**
+   * (Re-)starts the loop. If an existing iteration has been
+   * scheduled, this is cancelled and started again.
+   * 
+   * This can be useful when adjusting the interval
+   */
+  reset():void
   /**
    * How many milliseconds since start() was last called
    */
-  get elapsedMs(): number
+  get elapsedMs():number
   /**
    * How many iterations of the loop since start() was last called
    */
-  get ticks(): number
+  get ticks():number
   /**
-   * Whether loop has finished
+   * Returns true if the loop is not running (for some reason or another)
    */
-  get isDone(): boolean
+  get isDone():boolean
   /**
-   * Stops loop
+   * If disposed, the continuously instance won't be re-startable
    */
-  cancel(): void
-
+  get isDisposed():boolean
+  /**
+   * Stops loop. It can be restarted using .start()
+   */
+  cancel():void
+  /**
+   * Set interval. Change will take effect on next loop. For it to kick
+   * in earlier, call .reset() after changing the value.
+   */
   set intervalMs(ms:number);
   get intervalMs():number
 }
 
-export type ContinuouslySyncCallback = (ticks?:number, elapsedMs?:number) => boolean|void
-export type ContinuouslyAsyncCallback = (ticks?:number, elapsedMs?:number) => Promise<boolean|void>
+export type ContinuouslySyncCallback = (ticks?:number, elapsedMs?:number)=>boolean|void
+export type ContinuouslyAsyncCallback = (ticks?:number, elapsedMs?:number)=>Promise<boolean|void>
 
-const raf = typeof window !== `undefined` ? (cb:()=>void) => window.requestAnimationFrame(cb) : (cb:()=>void) => setTimeout(cb, 1);
+const raf = typeof window !== `undefined` ? (cb:()=>void) => window.requestAnimationFrame(cb) : (cb:()=>void) => window.setTimeout(cb, 1);
+
+export type OnStartCalled = `continue` | `cancel` | `reset` | `dispose`
+
+//eslint-disable-next-line functional/no-mixed-type
+export type ContinuouslyOpts = {
+  readonly fireBeforeWait?:boolean
+  /**
+   * Called whenever .start() is invoked.
+   * If this function returns:
+   *  - `continue`: the loop starts if it hasn't started yet, or continues if already started
+   *  - `cancel`: loop stops, but can be re-started if .start() is called again
+   *  - `dispose`: loop stops and will throw an error if .start() is attempted to be called 
+   *  - `reset`: loop resets (ie. existing scheduled task is cancelled)
+   *  
+   */
+  readonly onStartCalled?:((ticks?:number, elapsedMs?:number)=>OnStartCalled)
+}
 
 /**
- * Returns a {@link Continuously} that continuously executes `callback`. 
+ * Returns a {@link Continuously} that continuously at `intervalMs`, executing `callback`.
+ * By default, first the sleep period happens and then the callback happens.
+ * Use {@link Timeout} for a single event.
+ *  
  * If callback returns _false_, loop exits.
  * 
  * Call `start` to begin/reset loop. `cancel` stops loop.
@@ -65,6 +100,8 @@ const raf = typeof window !== `undefined` ? (cb:()=>void) => window.requestAnima
  * c.cancel();   // Stop the loop, cancelling any up-coming calls to `fn`
  * c.elapsedMs;  // How many milliseconds have elapsed since start
  * c.ticks;      // How many iterations of loop since start
+ * c.intervalMs; // Get/set speed of loop. Change kicks-in at next loop. 
+ *               // Use .start() to reset to new interval immediately
  * ```
  * 
  * Asynchronous callback functions are supported too:
@@ -80,21 +117,41 @@ const raf = typeof window !== `undefined` ? (cb:()=>void) => window.requestAnima
  * }).start();
  * ```
  * 
- * And if `callback` explicitly returns _false_, the loop will exit:
+ * If the callback explicitly returns _false_, the loop will be cancelled
  * ```js
- * continuously((ticks) => {
+ * continuously(ticks => {
  *  // Stop after 100 iterations
  *  if (ticks > 100) return false;
  * }).start();
  * ```
+ * 
+ * You can intercept the logic for calls to `start()` with `onStartCalled`. It can determine
+ * whether the `start()` proceeds, if the loop is cancelled, or the whole thing disposed,
+ * so it can't run any longer.
+ * ```js
+ * continuously(callback, intervalMs, {
+ *  onStartCalled:(ticks, elapsedMs) => {
+ *    if (elapsedMs > 1000) return `cancel`;
+ *  }
+ * }).start();
+ * ```
+ * 
+ * To run `callback` *before* the sleep happens, set `fireBeforeWait`:
+ * ```js
+ * continuously(callback, intervalMs, { fireBeforeWait: true });
+ * ```
  * @param callback Function to run. If it returns false, loop exits.
- * @param resetCallback Callback when/if loop is reset. If it returns false, loop exits
+ * @param opts Additional options
  * @param intervalMs 
  * @returns 
  */
-export const continuously = (callback:ContinuouslyAsyncCallback|ContinuouslySyncCallback, intervalMs?:number, resetCallback?:((ticks?:number, elapsedMs?:number) => boolean|void)):Continuously => {
+export const continuously = (callback:ContinuouslyAsyncCallback|ContinuouslySyncCallback, intervalMs?:number, opts:ContinuouslyOpts = {}):Continuously => {
   if (intervalMs !== undefined) guardInteger(intervalMs, `positive`, `intervalMs`);
+  const fireBeforeWait = opts.fireBeforeWait ?? false;
+  const onStartCalled = opts.onStartCalled;
 
+  //eslint-disable-next-line functional/no-let
+  let disposed = false;
   //eslint-disable-next-line functional/no-let
   let running = false;
   //eslint-disable-next-line functional/no-let
@@ -103,13 +160,18 @@ export const continuously = (callback:ContinuouslyAsyncCallback|ContinuouslySync
   let startedAt = performance.now();
   //eslint-disable-next-line functional/no-let
   let iMs = (intervalMs === undefined) ? 0 : intervalMs;
+  //eslint-disable-next-line functional/no-let
+  let currentTimer = 0;
 
-  const schedule = (iMs === 0) ? raf : (cb:()=>void) => setTimeout(cb, iMs);
-  
+  const schedule = (iMs === 0) ? raf : (cb:()=>void) => window.setTimeout(cb, iMs);
+  const deschedule = (iMs === 0) ? (_:number) => { /** no-op */ } : (timer:number) => window.clearTimeout(timer);
+
   const cancel = () => {
     if (!running) return;
     running = false;
     ticks = 0;
+    if (currentTimer !== 0) deschedule(currentTimer);
+    currentTimer = 0;
   };
 
   const loop = async () => {
@@ -126,31 +188,54 @@ export const continuously = (callback:ContinuouslyAsyncCallback|ContinuouslySync
       cancel();
       return;
     }
-
-    schedule(loop);
+    currentTimer = schedule(loop);
   };
 
   const start = () => {
-    // Already running, but theres a resetCallback to check if we should keep going
-    if (running && resetCallback !== undefined) {
-      const r = resetCallback(ticks, performance.now() - startedAt);
-      startedAt = performance.now();
-      if (r !== undefined && !r) {
-        // Reset callback tells us to stop
+    if (disposed) throw new Error(`Disposed`);
+    
+    if (onStartCalled !== undefined) {
+      // Function governs whether to allow .start() to go ahead
+      const doWhat = onStartCalled(ticks, performance.now() - startedAt);      
+      if (doWhat === `cancel`) {
         cancel();
-        return; // Skip starting again
-      } 
-    } else if (running) {
-      return; // already running
+        return;
+      } else if (doWhat === `reset`) {
+        reset();
+        return;
+      } else if (doWhat === `dispose`) {
+        disposed = true;
+        cancel();
+        return;
+      }
     }
 
-    // Start running
-    running = true;
-    schedule(loop);
+    if (!running) {
+      // Start running
+      startedAt = performance.now();
+      running = true;
+      if (fireBeforeWait) {
+        loop(); // Exec first, then wait
+      } else {
+        currentTimer = schedule(loop); // Wait first, then exec
+      }
+    }
+  };
+
+  const reset = () => {
+    if (disposed) throw new Error(`Disposed`);
+    
+    // Cancel scheduled iteration
+    if (running) {
+      cancel();
+    }
+    start();
   };
 
   return {
     start,
+    reset,
+    cancel,
     get intervalMs() {
       return iMs;
     },
@@ -161,12 +246,14 @@ export const continuously = (callback:ContinuouslyAsyncCallback|ContinuouslySync
     get isDone() {
       return !running;
     },
+    get isDisposed() {
+      return disposed;
+    },
     get ticks() {
       return ticks;
     },
     get elapsedMs() {
       return performance.now() - startedAt;
-    },
-    cancel
+    }
   };
 };
--- tests/flow/continuously/script.js
+++ tests/flow/continuously/script.js
@@ -22,19 +22,21 @@ function testTicksInCallback() {
 }
 
 /**
- * Loop runs every 2s, and we pepper it with .start() calls.
- * After 2s it cancels 
+ * Loop runs every 500ms, and we pepper it with .start() calls.
+ * After 1s it disposes, to prevent restarting.
+ * 
+ * Behaviour should be a few `run` messages and everything stopping after 1s
  */
-async function testRestartCancel() {
+async function testRestartDispose() {
   let t = 0;
   let left = 5;
-  log.log(`-- testRestartCancel --`);
+  log.log(`-- testRestartDispose --`);
   const c = continuously(() => {
     log.log(`run`);
-  }, 1*1000, {
+  }, 500, {
     onStartCalled:(ticks, elapsedMs) => {
       log.log(`OnStartCalled. ticks: ${ticks} elapsedMs: ${elapsedMs}`);
-      if (elapsedMs >= 2000) return `cancel`;
+      if (elapsedMs >= 1000) return `dispose`;
     }
   });
 
@@ -46,4 +48,78 @@ async function testRestartCancel() {
   c.start();
   log.log(`Done.`);
 }
-testRestartCancel();
+
+const ms = (ms) => {
+  if (ms < 1000) return Math.floor(ms) +'ms';
+  return (ms/1000).toFixed(1) + 's';
+}
+
+/**
+ * Loop runs every 500ms. After 1000 it will be cancelled, and after 5s restarted.
+ * 
+ * Behaviour:
+ *  - `run` appears once or so, stops for a while and then continues running
+ */
+async function testCancelStart() {
+  let t = 0;
+  let left = 5;
+  log.log(`-- testCancelStart --`);
+  const c = continuously((ticks, intervalMs) => {
+    log.log(`run. ticks: ${ticks} interval: ${ms(intervalMs)}`);
+  }, 500);
+
+  // Cancel
+  setTimeout(() => {
+    c.cancel();
+  }, 1000);
+
+  // Restart
+  setTimeout(() => {
+    log.log(`Attempting restart`);
+    // reset/start has the same outcome
+    //c.reset();
+    c.start();
+  }, 5000);
+
+  c.start();
+  log.log(`Done.`);
+}
+
+/**
+ * Long interval loop, but because `fireBeforeWait` is true, run message
+ * appears early.
+ */
+async function testFireBeforeWait() {
+  let t = 0;
+  let left = 5;
+  log.log(`-- testFireBeforeWait --`);
+  const c = continuously((ticks, intervalMs) => {
+    log.log(`run. ticks: ${ticks} interval: ${ms(intervalMs)}`);
+  }, 10*1000, { fireBeforeWait: true });
+
+  c.start();
+}
+
+
+/**
+ * Behaviour:
+ * - No `run` messages unil the change of tempo kicks in,
+ * - Since we are resetting, change will activate immediately
+ */
+async function testLongReset() {
+  log.log(`-- testLongReset --`);
+  const c = continuously((ticks, intervalMs) => {
+    log.log(`run. ticks: ${ticks} interval: ${ms(intervalMs)}`);
+  }, 60*1000);
+
+  // Change tempo and reset
+  setTimeout(() => {
+    log.log(`Changing tempo and resetting`);
+    c.intervalMs = 100;
+    c.reset();
+  }, 5000);
+
+  c.start();
+  log.log(`Done.`);
+}
+testLongReset();
