e167a1d35afdcc3a9c1df54ceecd7ca9d97d152f
CH <clint@thestaticvoid.net>
2023-05-05 21:48:50 +0200

    Improved test coverage

--- src/__tests__/text.test.ts
+++ src/__tests__/text.test.ts
@@ -1,6 +1,24 @@
 /* eslint-disable */
 import { expect, test } from '@jest/globals';
-import { startsEnds } from '../Text.js';
+import { startsEnds, between, betweenChomp, omitChars, splitByLength } from '../Text.js';
+
+test('splitByLength', () => {
+  const t1 = 'hello there';
+  const t11 = splitByLength(t1, 2);
+  expect(t11).toEqual(['he', 'll', 'o ', 'th', 'er', 'e']);
+
+  // Test with chunk size longer than input string
+  const t2 = 'hello';
+  const t22 = splitByLength(t2, 5);
+  expect(t22).toEqual(['hello']);
+  const t23 = splitByLength(t2, 50);
+  expect(t23).toEqual(['hello']);
+  
+});
+
+test('omitChars', () => {
+  expect(omitChars('hello there', 1, 3)).toEqual('ho there');
+});
 
 test(`startsEnds`, () => {
   expect(startsEnds(`test`, `t`)).toBeTruthy();
@@ -9,3 +27,38 @@ test(`startsEnds`, () => {
   expect(startsEnds(`This is a test`, `this`, `Test`)).toBeFalsy();
   expect(startsEnds(`This is a test`, `This`, `not`)).toBeFalsy();
 });
+
+test(`between`, () => {
+  expect(between('hello [there] pal', '[', ']')).toEqual('there');
+  expect(between('hello [there] p]al', '[', ']')).toEqual('there] p');
+  expect(between('hello [there] p]al', '[', ']', false)).toEqual('there');
+  expect(between('hello !there! pal', '!')).toEqual('there');
+
+  expect(between('hello [there pal', '[', ']')).toBeUndefined();
+  expect(between('hello there] pal', '[', ']')).toBeUndefined();
+});
+
+test(`betweenChomp`, () => {
+  const r1 = betweenChomp('hello [there] pal', '[', ']');
+  
+  expect(r1[1]).toEqual('there');
+  expect(r1[0]).toEqual('hello  pal');
+  
+  const r2 = betweenChomp('hello [there] p]al', '[', ']');
+  expect(r2[1]).toEqual('there] p');
+  expect(r2[0]).toEqual('hello al');
+
+  const r3 = betweenChomp('hello [there] p]al', '[', ']', false);
+  expect(r3[1]).toEqual('there');
+  expect(r3[0]).toEqual('hello  p]al');
+
+  const r4 = betweenChomp('hello !there! pal', '!');
+  expect(r4[1]).toEqual('there');
+  expect(r4[0]).toEqual('hello  pal');
+
+  const r5 = betweenChomp('hello [there] pal', '{', '}');
+  expect(r5[0]).toEqual('hello [there] pal');
+  expect(r5[1]).toBeUndefined();
+
+});
+
