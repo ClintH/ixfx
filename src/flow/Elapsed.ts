dd090c6b1ba8f4c4745e5b827bfd33f92f9693e1
CH <clint@thestaticvoid.net>
2023-07-26 16:48:02 +0200

    Flow.completionMs -> Elapsed.progress

--- src/flow/Elapsed.ts
+++ src/flow/Elapsed.ts
@@ -1,50 +1,95 @@
-import {intervalToMs} from "./Interval.js";
+import { intervalToMs, type Interval } from './Interval.js';
+import { msElapsedTimer, relativeTimer } from './Timer.js';
 
-export type SinceFn = ()=>number;
+export type SinceFn = () => number;
 
 /**
  * Returns elapsed time since initial call.
  * ```js
  * // Record start
  * const elapsed = Elapsed.since();
- * 
+ *
  * // Get elapsed time in millis
  * elapsed(); // Yields number
  * ```
- * @returns 
+ *
+ * If you want to initialise a elapsed timer, but not yet start it, consider:
+ * ```js
+ * // Init
+ * let state = {
+ *  clicked: Elapsed.infinity()
+ * };
+ *
+ * state.click(); // Returns a giant value
+ *
+ * // Later, when click happens:
+ * state = { click: Elapsed.since() }
+ * @returns
  */
-export const since = ():SinceFn => {
+export const since = (): SinceFn => {
   const start = Date.now();
-  return ():number => {
+  return (): number => {
     return Date.now() - start;
-  }
-}
+  };
+};
 
 /**
  * Returns a function that reports an 'infinite' elapsed time.
  * this can be useful as an initialiser for `elapsedSince`.
- * 
+ *
  * ```js
  * // Init clicked to be an infinite time
  * let clicked = Elapsed.infinity();
- * 
+ *
  * document.addEventListener('click', () => {
  *  // Now that click has happened, we can assign it properly
  *  clicked = Elapsed.since();
  * });
  * ```
- * @returns 
+ * @returns
  */
-export const infinity = ():SinceFn => {
-  return ():number => {
+export const infinity = (): SinceFn => {
+  return (): number => {
     return Number.POSITIVE_INFINITY;
-  }
+  };
+};
+
+/**
+ * Returns a function that returns the percentage of timer completion.
+ * Starts timing immediately.
+ *
+ * ```js
+ * const timer = Elapsed.progress(1000);
+ * timer(); // Returns 0..1
+ * ```
+ *
+ * Takes an {@link Interval} for more expressive time:
+ * ```js
+ * const timer = Elapsed.progress({ mins: 4 });
+ * ```
+ * See also {@link hasElapsedMs}.
+ * @param totalMs
+ * @returns
+ */
+export function progress(duration: Interval): () => number {
+  const totalMs = intervalToMs(duration);
+  if (!totalMs) throw new Error(`duration invalid`);
+  const t = relativeTimer(totalMs, msElapsedTimer());
+  return () => t.elapsed;
 }
 
-
-export const toString = (millisOrFn:number|SinceFn):string => {
-  const interval = (typeof millisOrFn === `number`) ? millisOrFn : millisOrFn();
-  
+export const toString = (millisOrFn: number | SinceFn | Interval): string => {
+  //eslint-disable-next-line functional/no-let
+  let interval = 0;
+  if (typeof millisOrFn === `function`) {
+    const intervalResult = millisOrFn();
+    if (typeof intervalResult === `object`) interval = intervalToMs(interval)!;
+    else interval = intervalResult;
+  } else if (typeof millisOrFn === `number`) {
+    interval = millisOrFn;
+  } else if (typeof millisOrFn === `object`) {
+    interval = intervalToMs(interval)!;
+  }
   //eslint-disable-next-line functional/no-let
   let ms = intervalToMs(interval);
   if (!ms) return '(undefined)';
@@ -55,4 +100,4 @@ export const toString = (millisOrFn:number|SinceFn):string => {
   if (ms < 60) return `${ms.toFixed(2)}mins`;
   ms /= 60;
   return `${ms.toFixed(2)}hrs`;
-}
+};
