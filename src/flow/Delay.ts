cb28aee3861005b87bf9f1f4b81f3cdee36ac969
CH <clint@thestaticvoid.net>
2023-07-31 16:27:46 +0200

    Take Interval, improved when delay happens

--- src/flow/Delay.ts
+++ src/flow/Delay.ts
@@ -1,26 +1,23 @@
+import { intervalToMs, type Interval } from './Interval.js';
 import { sleep } from './Sleep.js';
 
 /**
  * Delay options
  */
-export type DelayOpts = {
+export type DelayOpts = Interval & {
   /**
-   * Milliseconds
-   */
-  readonly intervalMs: number;
-  /**
-   * Signal to cancel
+   * Signal for cancelling delay
    */
   readonly signal?: AbortSignal;
   /**
-   * When delay is applied
+   * When delay is applied. "before" is default.
    */
   readonly delay: 'before' | 'after' | 'both';
 };
 
 /**
- * Pauses execution for `timeoutMs` after which the asynchronous `callback` is executed and awaited.
- * Use {@link delayIterable} to step through an iterable with delays
+ * Pauses execution for interval after which the asynchronous `callback` is executed and awaited.
+ * Must be called with `await` if you want the pause effect.
  *
  * @example Pause and wait for function
  * ```js
@@ -28,11 +25,16 @@ export type DelayOpts = {
  * console.log(result); // Prints out result after one second
  * ```
  *
+ * If the `interval` option is a number its treated as milliseconds. {@link Interval} can also be used:
+ * ```js
+ * const result = await delay(async () => Math.random(), { mins: 1 });
+ * ```
+ *
  * If `await` is omitted, the function will run after the provided timeout, and code will continue to run.
  *
  * @example Schedule a function without waiting
  * ```js
- * delay(async () => {
+ * await delay(async () => {
  *  console.log(Math.random())
  * }, 1000);
  * // Prints out a random number after 1 second.
@@ -40,13 +42,22 @@ export type DelayOpts = {
  *
  * {@link delay} and {@link sleep} are similar. `delay()` takes a parameter of what code to execute after the timeout, while `sleep()` just resolves after the timeout.
  *
+ * Optionally takes an AbortSignal to cancel delay.
+ * ```js
+ * const ac = new AbortController();
+ * // Super long wait
+ * await delay(someFn, { signal: ac.signal, hours: 1 }}
+ * ...
+ * ac.abort(); // Cancels long delay
+ * ```
  *
+ * It also allows choice of when delay should happen.
  * If you want to be able to cancel or re-run a delayed function, consider using
  * {@link timeout} instead.
  *
  * @template V
- * @see {@link delayIterable} To delay iteration
- * @param callback What to run after `timeoutMs`
+ * @param callback What to run after interval
+ * @param opts Options for delay. By default delay is before `callback` is executed.
  * @return Returns result of `callback`.
  */
 export const delay = async <V>(
@@ -54,8 +65,15 @@ export const delay = async <V>(
   //eslint-disable-next-line functional/prefer-immutable-types
   opts: DelayOpts
 ): Promise<V> => {
-  await sleep(opts);
-  return Promise.resolve(await callback());
+  const delayWhen = opts.delay ?? 'before';
+  if (delayWhen === `before` || delayWhen === `both`) {
+    await sleep(opts);
+  }
+  const r = Promise.resolve(await callback());
+  if (delayWhen === `after` || delayWhen === `both`) {
+    await sleep(opts);
+  }
+  return r;
 };
 
 /**
@@ -78,32 +96,32 @@ export const delay = async <V>(
  * @param iter
  * @param opts
  */
-export async function* delayIterable<V>(
-  iter: AsyncIterable<V> | Iterable<V>,
-  //eslint-disable-next-line functional/prefer-immutable-types
-  opts: DelayOpts
-) {
-  const intervalMs = opts.intervalMs;
-  const delayWhen = opts.delay;
-  const signal = opts.signal;
+// export async function* delayIterable<V>(
+//   iter: AsyncIterable<V> | Iterable<V>,
+//   //eslint-disable-next-line functional/prefer-immutable-types
+//   opts: DelayOpts
+// ) {
+//   const intervalMs = intervalToMs(opts);
+//   const delayWhen = opts.delay;
+//   const signal = opts.signal;
 
-  for await (const v of iter) {
-    // Pre-delay
-    if (delayWhen === 'before' || delayWhen === 'both') {
-      await sleep({ millis: intervalMs, signal });
-      if (signal?.aborted) break;
-    }
+//   for await (const v of iter) {
+//     // Pre-delay
+//     if (delayWhen === 'before' || delayWhen === 'both') {
+//       await sleep({ millis: intervalMs, signal });
+//       if (signal?.aborted) break;
+//     }
 
-    // Yield value
-    yield v;
+//     // Yield value
+//     yield v;
 
-    // Post-delay
-    if (delayWhen === 'after' || delayWhen === 'both') {
-      await sleep({ millis: intervalMs, signal });
-      if (signal?.aborted) break;
-    }
-  }
-}
+//     // Post-delay
+//     if (delayWhen === 'after' || delayWhen === 'both') {
+//       await sleep({ millis: intervalMs, signal });
+//       if (signal?.aborted) break;
+//     }
+//   }
+// }
 
 /**
  * Async generator that loops via `requestAnimationFrame`.
@@ -154,13 +172,19 @@ async function* delayAnimationLoop() {
 }
 
 /**
- * Async generator that loops at a given `timeoutMs`.
+ * Async generator that loops at a given interval.
+ * Alternatives:
+ * * {@link delay} to run a single function after a delay
+ * * {@link sleep} pause execution
+ * * {@link interval} iterate over an iterable with a given delay
+ * * {@link continuously} to start/stop/adjust a constantly running loop
  *
  * @example Loop runs every second
  * ```
  * // Loop forever
  * (async () => {
  *  const loop = delayLoop(1000);
+ *  // or: loop = delayLoop({ secs: 1 });
  *  while (true) {
  *    await loop.next();
  *
@@ -173,15 +197,19 @@ async function* delayAnimationLoop() {
  * @example For Await loop every second
  * ```
  * const loop = delayLoop(1000);
+ * // Or: const loop = delayLoop({ secs: 1 });
  * for await (const o of loop) {
  *  // Do something...
  *  // Warning: loops forever
  * }
  * ```
- * @param timeoutMs Delay. If 0 is given, `requestAnimationFrame` is used over `setTimeout`.
+ * @param timeout Delay. If 0 is given, `requestAnimationFrame` is used over `setTimeout`.
  */
 //eslint-disable-next-line func-style
-export async function* delayLoop(timeoutMs: number) {
+export async function* delayLoop(timeout: Interval) {
+  const timeoutMs = intervalToMs(timeout);
+  if (!timeoutMs) throw new Error(`timeout is undefined`);
+  if (timeoutMs < 0) throw new Error('Timeout is less than zero');
   if (timeoutMs === 0) return yield* delayAnimationLoop();
 
   //eslint-disable-next-line functional/no-let,@typescript-eslint/no-explicit-any
