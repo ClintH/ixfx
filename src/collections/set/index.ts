f03ae4b94c5a5733992fc3144213fd09a9ec4420
CH <clint@thestaticvoid.net>
2023-08-16 21:46:05 +0200

    Update index.ts

--- src/collections/set/index.ts
+++ src/collections/set/index.ts
@@ -1,11 +1,10 @@
-
 export type ValueSetEventMap<V> = {
-  readonly add: {readonly value: V, readonly updated: boolean}
-  readonly clear: boolean
-  readonly delete: V
-}
+  readonly add: { readonly value: V; readonly updated: boolean };
+  readonly clear: boolean;
+  readonly delete: V;
+};
 
-export * from './SetMutable.js';
-export * from './SetImmutable.js';
-export {type ISetMutable} from './ISetMutable.js';
-export {type ISetImmutable} from './ISetImmutable.js';
+export { mutable } from './SetMutable.js';
+export { immutable } from './SetImmutable.js';
+export { type ISetMutable } from './ISetMutable.js';
+export { type ISetImmutable } from './ISetImmutable.js';
