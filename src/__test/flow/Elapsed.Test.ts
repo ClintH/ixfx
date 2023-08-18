e23f5b670fa45d6e770727370a6434f0d38f26ed
CH <clint@thestaticvoid.net>
2023-08-18 11:58:20 +0200

    Create Elapsed.Test.ts

---
+++ src/__test/flow/Elapsed.Test.ts
@@ -0,0 +1,13 @@
+import test from 'ava';
+import { toString, since } from '../../flow/Elapsed.js';
+
+test('toString', (t) => {
+  t.is(toString(100), '100ms');
+  t.is(toString(10 * 1000), '10.0secs');
+
+  const fn = () => 100;
+  t.is(toString(fn), '100ms');
+
+  const elapsed = since();
+  t.is(toString(elapsed), '0ms');
+});
