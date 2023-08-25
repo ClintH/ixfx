17ccc4531907d1811f973300d95c7e5981cb0282
CH <clint@thestaticvoid.net>
2023-08-25 23:18:44 +0200

    Linting

--- src/KeyValue.ts
+++ src/KeyValue.ts
@@ -3,53 +3,58 @@ import { pipe } from 'fp-ts/lib/function.js';
 import * as S from 'fp-ts/lib/string.js';
 import * as N from 'fp-ts/lib/number.js';
 import { reverse as reverseOrd, contramap } from 'fp-ts/lib/Ord.js';
-import {minMaxAvg as arrayMinMaxAg} from './collections/Arrays.js';
+import { minMaxAvg as arrayMinMaxAg } from './collections/Arrays.js';
 
 /// ✔ Sorting functions are unit tested
 
 export type Primitive = string | number;
-export type KeyValue = readonly [key: string, value: Primitive];
+export type KeyValue = readonly [ key: string, value: Primitive ];
 
-const byKey = (reverse:boolean = false) => pipe(
-  reverse ? reverseOrd(S.Ord) : S.Ord, 
-  contramap((v:KeyValue) => v[0])
+const byKey = (reverse = false) => pipe(
+  reverse ? reverseOrd(S.Ord) : S.Ord,
+  contramap((v: KeyValue) => v[ 0 ])
 );
 
-export const byValueString = (reverse:boolean = false) => pipe(
-  reverse ? reverseOrd(S.Ord) : S.Ord, 
-  contramap((v:KeyValue) => v[1] as string)
+export const byValueString = (reverse = false) => pipe(
+  reverse ? reverseOrd(S.Ord) : S.Ord,
+  contramap((v: KeyValue) => v[ 1 ] as string)
 );
 
-const byValueNumber = (reverse:boolean = false) => pipe(
-  reverse ? reverseOrd(N.Ord) : N.Ord, 
-  contramap((v:KeyValue) => v[1] as number)
+const byValueNumber = (reverse = false) => pipe(
+  reverse ? reverseOrd(N.Ord) : N.Ord,
+  contramap((v: KeyValue) => v[ 1 ] as number)
 );
 
-export const sortByKey = (reverse:boolean = false) => sort<KeyValue>(byKey(reverse));
-export const sortByValueString = (reverse:boolean = false) => sort<KeyValue>(byValueString(reverse));
-export const sortByValueNumber = (reverse:boolean = false) => sort<KeyValue>(byValueNumber(reverse));
+export const sortByKey = (reverse = false) => sort<KeyValue>(byKey(reverse));
+export const sortByValueString = (reverse = false) => sort<KeyValue>(byValueString(reverse));
+export const sortByValueNumber = (reverse = false) => sort<KeyValue>(byValueNumber(reverse));
 
-// eslint-disable-next-line functional/prefer-readonly-type
-export type SortingFn = (data:KeyValue[]) => KeyValue[];
+// eslint-disable-next-line functional/prefer-readonly-type,functional/prefer-immutable-types
+export type Sorter = (data: Array<KeyValue>) => Array<KeyValue>;
 
-export const getSorter = (sortStyle:`value` | `valueReverse` | `key` | `keyReverse`) => {
+export const getSorter = (sortStyle: `value` | `valueReverse` | `key` | `keyReverse`) => {
   switch (sortStyle) {
-  case `value`:
-    return sortByValueNumber(false);
-  case `valueReverse`:
-    return sortByValueNumber(true);
-  case `key`:
-    return sortByKey(false);
-  case `keyReverse`:
-    return sortByKey(true);
-  default:
-    throw new Error(`Unknown sorting value '${sortStyle}'. Expecting: value, valueReverse, key or keyReverse`);
+    case `value`: {
+      return sortByValueNumber(false);
+    }
+    case `valueReverse`: {
+      return sortByValueNumber(true);
+    }
+    case `key`: {
+      return sortByKey(false);
+    }
+    case `keyReverse`: {
+      return sortByKey(true);
+    }
+    default: {
+      throw new Error(`Unknown sorting value '${ sortStyle }'. Expecting: value, valueReverse, key or keyReverse`);
+    }
   }
 };
 
 
-export const minMaxAvg = (entries:readonly KeyValue[], conversionFn?:(v:KeyValue) => number) => {
-  if (conversionFn === undefined) conversionFn = (v:KeyValue) => v[1] as number;
-  const values = entries.map<number>(conversionFn);
+export const minMaxAvg = (entries: ReadonlyArray<KeyValue>, conversionFunction?: (v: KeyValue) => number) => {
+  if (conversionFunction === undefined) conversionFunction = (v: KeyValue) => v[ 1 ] as number;
+  const values = entries.map<number>(conversionFunction);
   return arrayMinMaxAg(values);
 };
