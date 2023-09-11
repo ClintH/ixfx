d2b76823f17da89c57c859db09ee4e7cedc1bfc0
CH <clint@thestaticvoid.net>
2023-09-11 18:08:07 +0200

    PriorityQueue

---
+++ src/__test/collections/queue/PriorityMutable.Test.ts
@@ -0,0 +1,21 @@
+import test from 'ava';
+import { PriorityMutable } from '../../../collections/queue/PriorityMutable.js';
+
+test(`pq-basic`, (t) => {
+  const p1 = new PriorityMutable();
+  p1.enqueueWithPriority(`low`, 2);
+  p1.enqueueWithPriority(`high`, 4);
+  p1.enqueueWithPriority(`medium`, 3);
+
+  t.is(p1.peekMax(), `high`);
+  t.is(p1.peekMin(), `low`);
+  t.is(p1.length, 3);
+
+  t.is(p1.dequeueMax(), `high`);
+  t.is(p1.peekMax(), `medium`);
+
+
+  t.is(p1.dequeueMax(), `medium`);
+  t.is(p1.peekMax(), `low`);
+
+});
