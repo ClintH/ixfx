d4a3a20c3f2e1fd830d058d88844e0541154f764
CH <clint@thestaticvoid.net>
2023-09-11 18:07:21 +0200

    Update util.ts

--- src/__test/util.ts
+++ src/__test/util.ts
@@ -1,13 +1,13 @@
 /* eslint-disable */
-import test, {type ExecutionContext} from 'ava';
-import {minMaxAvg} from '../collections/NumericArrays.js';
-import {compareValuesEqual} from '../collections/Arrays.js';
-import {isEqualDefault} from '../Util.js';
+import test, { type ExecutionContext } from 'ava';
+import { minMaxAvg } from '../collections/NumericArrays.js';
+import { compareValuesEqual } from '../collections/Arrays.js';
+import { isEqualDefault } from '../IsEqual.js';
 //test.todo('sf');
 
 export const areIntegers = (t: ExecutionContext, a: Array<number>) => {
   for (let i = 0; i < a.length; i++) {
-    t.is(Math.abs(a[i]) % 1, 0, `Integer ${a[i]}`);
+    t.is(Math.abs(a[ i ]) % 1, 0, `Integer ${ a[ i ] }`);
   }
 };
 
@@ -20,7 +20,7 @@ export const arrayValuesEqual = <V>(
   if (compareValuesEqual(a, b, eq)) {
     t.assert(true);
   } else {
-    t.fail(`Arrays not equal. A: ${JSON.stringify(a)} B: ${JSON.stringify(b)}`);
+    t.fail(`Arrays not equal. A: ${ JSON.stringify(a) } B: ${ JSON.stringify(b) }`);
   }
 };
 
@@ -32,15 +32,15 @@ export const closeTo = <V>(
 ) => {
   const aa = a.toPrecision(precision);
   const bb = b.toPrecision(precision);
-  if (aa !== bb) t.fail(`A is not close enough to B. A: ${a} B: ${b}`);
+  if (aa !== bb) t.fail(`A is not close enough to B. A: ${ a } B: ${ b }`);
   else t.assert(true);
 };
 
 export const isCloseTo = (a: number, b: number, precision: number = 3) => {
   const aa = a.toPrecision(precision);
   const bb = b.toPrecision(precision);
-  if (aa !== bb) return [false, `A is not close enough to B. A: ${a} B: ${b} Precision: ${precision}`];
-  else return [true];
+  if (aa !== bb) return [ false, `A is not close enough to B. A: ${ a } B: ${ b } Precision: ${ precision }` ];
+  else return [ true ];
 }
 
 /**
@@ -58,14 +58,14 @@ export const arrayValueIncludes = <V>(
   for (let i = 0; i < b.length; i++) {
     let found = false;
     for (let x = 0; x < a.length; x++) {
-      if (eq(b[i], a[x])) {
+      if (eq(b[ i ], a[ x ])) {
         found = true;
         break;
       }
     }
     if (!found) {
       t.fail(
-        `Item not found: ${JSON.stringify(b[i])}. A: ${JSON.stringify(a)}`
+        `Item not found: ${ JSON.stringify(b[ i ]) }. A: ${ JSON.stringify(a) }`
       );
       break;
     }
@@ -97,7 +97,7 @@ export const eventPromise = (opts: EventPromiseOpts) => {
       fires--;
       if (fires < 0) {
         done = true;
-        return reject(`Fired too many times: ${opts.fires}`);
+        return reject(`Fired too many times: ${ opts.fires }`);
       }
       if (opts.validateEvent) {
         if (!opts.validateEvent(data)) {
@@ -121,7 +121,7 @@ export const eventPromise = (opts: EventPromiseOpts) => {
     setTimeout(() => {
       if (!done) {
         done = true;
-        reject(`Timeout ${opts.timeoutMs}`);
+        reject(`Timeout ${ opts.timeoutMs }`);
       }
     }, opts.timeoutMs);
   });
@@ -138,15 +138,15 @@ export const equalUnordered = (
   // }
   t.is(a.length, b.length, `Array length`);
 
-  const aa = [...a].sort();
-  const bb = [...b].sort();
+  const aa = [ ...a ].sort();
+  const bb = [ ...b ].sort();
   for (let i = 0; i < aa.length; i++) {
     // if (aa[i] !== bb[i]) {
     //   t.fail(`Index ${i} ${aa[i]}, expected ${bb[i]}`);
 
     //   return;
     // }
-    t.is(aa[i], bb[i], `Contents at ${i}`);
+    t.is(aa[ i ], bb[ i ], `Contents at ${ i }`);
   }
 };
 
@@ -185,7 +185,7 @@ export const someNearness = (
     }
   }
   t.fail(
-    `Target value (${target}) too far. Closest was ${closestValue} with dist ${closestDistance}. Min distance: ${range}. Count: ${count}`
+    `Target value (${ target }) too far. Closest was ${ closestValue } with dist ${ closestDistance }. Min distance: ${ range }. Count: ${ count }`
   );
   return false;
 };
@@ -206,7 +206,7 @@ export const rangeCheck = (
   v: Iterable<number>,
   expected: ExpectedOpts
 ) => {
-  const {min, max} = minMaxAvg(v);
+  const { min, max } = minMaxAvg(v);
   if (expected.lowerExcl !== undefined) {
     if (min < expected.lowerExcl) {
       t.is(min, expected.lowerExcl, 'Lower exclusive');
