14dec87204f01648d0cc4c0a5943e47f16514a2c
CH <clint@thestaticvoid.net>
2023-09-11 18:06:54 +0200

    Update script.js

--- tests/data/rx/script.js
+++ tests/data/rx/script.js
@@ -1,53 +1,19 @@
-import { Pipes } from '../../../dist/data.js';
+import { Pipes, Chains } from '../../../dist/data.js';
 import * as Flow from '../../../dist/flow.js';
 import * as Generators from '../../../dist/generators.js';
 
-// const aa = Pipes.number(10);
-// const bb = Pipes.number(20);
+const rxWindow = Pipes.rxWindow();
+//console.log(`value: ${JSON.stringify(rxWindow.size.value)}`);
 
-// const divide = Pipes.mergeValues((a,b) => a/b, {}, aa, bb);
+rxWindow.size.on(v => {
+  console.log(`1 changed: ${JSON.stringify(v)}`);
+})
 
-// divide.setOutlet(value => {
-//   console.log(`Divide output: ${value}`);
-// }, { prime: true });
+rxWindow.size.on(v => {
+  console.log(`2 changed: ${JSON.stringify(v)}`);
+})
 
-// aa.inlet(20);
-
-
-// const rxWindow = Pipes.rxWindow();
-// rxWindow.size(value => {
-//   console.log(value);
-// })
-
-// const pipes = [];
-// const sync = Pipes.synchronise({}, pipes);
-// for await (const v of Pipes.asAsyncIterable(sync)) {
-//   console.log(`Value: ${v}`);
-// }
-
-const everySecond = Generators.interval(() => `s:${Math.random()}`, 1000);
-const everyThirdSecond = Generators.interval(() => `third:${Math.random()}`, 1000/3);
-
-const sync = Pipes.synchronise({reset:false}, Pipes.fromIterable(everySecond), Pipes.fromIterable(everyThirdSecond));
-
-for await (const v of Pipes.asAsyncIterable(sync)) {
-  console.log(JSON.stringify(v));
-}
-
-// const pipe = Pipes.bidi();
-
-// setInterval(() => pipe.inlet(Math.random()), 1000);
-
-//const windowed = Pipes.slidingWindow(pipe, 3);
-
-// const mma = Pipes.minMaxAvg(pipe);
-
-// for await (const value of Pipes.asAsyncIterable(mma)) {
-//   console.log(`iterable: ${JSON.stringify(value)}`);
-// }
-
-//const throttled = Pipes.throttle(onResizePipe, 1000);
-
-// onResizePipe.setOutlet(v => {
-//   console.log(`Value: ${v}`);
-// }, { initialValue: true });
+setTimeout(() => {
+  console.log(`Disposing`);
+  rxWindow.dispose();
+}, 3000);
