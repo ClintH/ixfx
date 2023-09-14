dcc5432d6b0413c46ce1f18c7c09fbe46761c7e5
CH <clint@thestaticvoid.net>
2023-09-14 19:01:06 +0200

    Data.Table

---
+++ src/__test/data/Table.Test.ts
@@ -0,0 +1,58 @@
+import test from 'ava';
+import { Table } from '../../data/Table.js';
+test(`table`, test => {
+  const t = new Table<number>();
+  t.appendRow(1, 2, 3);
+  test.is(t.rowCount, 1);
+  test.is(t.columnCount, 3);
+
+  test.deepEqual(t.asArray(), [ [ 1, 2, 3 ] ]);
+
+  t.appendRow(4, 5, 6);
+  t.appendRow(7, 8, 9);
+  test.is(t.rowCount, 3);
+  test.is(t.columnCount, 3);
+
+  t.set(1, 1, 0);
+  test.deepEqual(t.asArray(), [
+    [ 1, 2, 3 ],
+    [ 4, 0, 6 ],
+    [ 7, 8, 9 ]
+  ]);
+
+  t.set
+
+});
+
+test(`getRowWithLabelsObject`, test => {
+  const t = new Table<number>();
+  t.appendRow(1, 2, 3);
+  t.appendRow(4, 5, 6);
+  t.appendRow(7, 8, 9);
+  t.labelColumns(`a`, `b`, `c`);
+
+  const r1 = t.getRowWithLabelsArray(1);
+  const r2 = t.getRowWithLabelsObject(1);
+
+  test.deepEqual(r1, [
+    [ `a`, 4 ], [ `b`, 5 ], [ `c`, 6 ]
+  ]);
+
+  test.deepEqual(r2,
+    { a: 4, b: 5, c: 6 }
+  );
+
+  test.is(t.get(0, `b`), 2);
+  test.throws(() => { t.get(0, `d`) });
+
+});
+
+test(`setCols`, test => {
+  const t = new Table<string>();
+  t.setRow(1, 5, `hello`);
+  const arr = t.asArray();
+  test.deepEqual(t.asArray(), [
+    [],
+    [ `hello`, `hello`, `hello`, `hello`, `hello` ]
+  ]);
+})
--- src/data/Table.ts
+++ src/data/Table.ts
@@ -4,12 +4,18 @@ export class Table<V> {
   rowLabels: Array<string> = [];
   colLabels: Array<string> = [];
 
-  labelColumns(labels: Array<string>) {
+  labelColumns(...labels: Array<string>) {
     this.colLabels = labels;
   }
 
   labelColumn(columnNumber: number, label: string) {
     this.colLabels[ columnNumber ] = label;
+  }
+
+  getColumnLabelIndex(label: string): number | undefined {
+    for (const [ index, l ] of this.colLabels.entries()) {
+      if (l === label) return index;
+    }
   }
 
   print() {
@@ -23,17 +29,64 @@ export class Table<V> {
     }
   }
 
+  /**
+   * Return a copy of table as nested array
+   * ```js
+   * const t = new Table();
+   * // add stuff
+   * // ...
+   * const m = t.asArray();
+   * for (const row of m) {
+   *  for (const colValue of row) {
+   *    // iterate over all column values for this row
+   *  }
+   * }
+   * ```
+   * 
+   * Alternative: get value at row Y and column X
+   * ```js
+   * const value = m[y][x];
+   * ```
+   * @returns 
+   */
+  asArray(): Array<Array<V | undefined>> {
+    const r: Array<Array<V | undefined>> = [];
+    for (const row of this.rows) {
+      console.log(`row: ${ JSON.stringify(row) }`);
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (row === undefined) r.push([]);
+      else r.push([ ...row ]);
+    }
+    return r;
+  }
+
+  /**
+   * Return the number of rows
+   */
+  get rowCount() {
+    return this.rows.length;
+  }
+
+  /**
+   * Return the maximum number of columns in any row
+   */
+  get columnCount() {
+    const lengths = this.rows.map(row => row.length);
+    return Math.max(...lengths);
+  }
+
   *rowsWithLabelsObject() {
     for (let index = 0; index < this.rows.length; index++) {
       const labelledRow = this.getRowWithLabelsObject(index);
       yield labelledRow;
     }
   }
-  labelRows(labels: Array<string>) {
+
+  labelRows(...labels: Array<string>) {
     this.rowLabels = labels;
   }
 
-  appendRow(data: Array<V | undefined>) {
+  appendRow(...data: Array<V | undefined>) {
     this.rows.push(data);
   }
 
@@ -43,19 +96,35 @@ export class Table<V> {
     return row.map((value, index) => [ this.colLabels.at(index), value ]);
   }
 
+  /**
+   * Return a row of objects. Keys use the column labels.
+   * 
+   * ```js
+   * const row = table.getRowWithLabelsObject(10);
+   * // eg:
+   * // [{ colour: red, size: 10}, { colour: blue, size: 20 }]
+   * ```
+   * @param rowNumber 
+   * @returns 
+   */
   getRowWithLabelsObject(rowNumber: number): object | undefined {
     const row = this.rows.at(rowNumber);
     if (row === undefined) return undefined;
     const object = {};
     for (let index = 0; index < this.colLabels.length; index++) {
-      const label = this.colLabels.at(index) ?? `-`;
+      const label = this.colLabels.at(index) ?? index.toString();
       // @ts-expect-error
       object[ label ] = row[ index ];
     }
     return object;
   }
 
-  getOrCreateRow(rowNumber: number): Array<V | undefined> {
+  /**
+   * Gets or creates a row at `rowNumber`.
+   * @param rowNumber 
+   * @returns 
+   */
+  private getOrCreateRow(rowNumber: number): Array<V | undefined> {
     let row = this.rows.at(rowNumber);
     if (row === undefined) {
       row = [];
@@ -64,15 +133,40 @@ export class Table<V> {
     return row;
   }
 
+  /**
+   * Gets the values at `rowNumber`
+   * @param rowNumber 
+   * @returns 
+   */
   row(rowNumber: number): Array<V | undefined> | undefined {
     return this.rows.at(rowNumber);
   }
 
+  /**
+   * Set the value of row,column to `value`
+   * @param rowNumber 
+   * @param columnNumber 
+   * @param value 
+   */
   set(rowNumber: number, columnNumber: number, value: V | undefined) {
     const row = this.getOrCreateRow(rowNumber);
     row[ columnNumber ] = value;
   }
 
+  get(rowNumber: number, column: number | string) {
+    const row = this.getOrCreateRow(rowNumber);
+    const index = typeof column === `number` ? column : this.getColumnLabelIndex(column);
+    if (index === undefined) throw new Error(`Column not found: ${ column }`);
+    return row[ index ];
+  }
+
+  /**
+   * For a given row number, set all the columns to `value`.
+   * `cols` gives the number of columns to set
+   * @param rowNumber 
+   * @param cols 
+   * @param value 
+   */
   setRow(rowNumber: number, cols: number, value: V | undefined) {
     const row = this.getOrCreateRow(rowNumber);
     for (let columnNumber = 0; columnNumber < cols; columnNumber++) {
