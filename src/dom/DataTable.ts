ea6ad3cfb71871702c525725d85ecb9ebfb0d473
CH <clint@thestaticvoid.net>
2023-09-17 13:47:55 +0200

    Update DataTable.ts

--- src/dom/DataTable.ts
+++ src/dom/DataTable.ts
@@ -1,16 +1,54 @@
-import { resolveEl } from './Util.js';
+import { round } from 'src/Numbers.js';
+import { resolveEl as resolveElement } from './Util.js';
 import JSON5 from 'json5';
 
-export type DataTableOpts = {
-  readonly formatter?: DataFormatter;
-  readonly precision?: number;
-  readonly roundNumbers?: boolean;
+export type FormattingOptions = {
+  readonly precision?: number
+  readonly roundNumbers?: number
+}
+export type DataTableOpts = FormattingOptions & {
+  readonly formatter?: DataFormatter
+  readonly objectsAsTables?: boolean
+  readonly idPrefix?: string
 };
 
 export type DataTable<V> = {
   update(data: V): void;
   remove(): boolean;
 };
+
+const toHtmlSimple = (v: any, options: FormattingOptions): string => {
+  if (v === null) return `(null)`;
+  if (v === undefined) return `(undefined)`;
+  if (typeof v === `boolean`) return v ? `true` : `false`;
+  if (typeof v === `string`) return `"${ v }"`;
+  if (typeof v === `number`) {
+    let vAsNumber = v;
+    if (options.roundNumbers !== undefined) vAsNumber = round(options.roundNumbers, v)
+    if (options.precision !== undefined) return vAsNumber.toFixed(options.precision);
+    return vAsNumber.toString();
+  }
+  if (typeof v === `object`) return toTableSimple(v, options);
+  return JSON5.stringify(v);
+}
+
+const toTableSimple = (v: object, options: FormattingOptions): string => {
+  let html = `<div style="display:grid; grid-template-columns: repeat(2, 1fr)">`;
+  for (const entry of Object.entries(v)) {
+    const value = toHtmlSimple(entry[ 1 ], options);
+    html += `<div class="label" style="display:table-cell">${ entry[ 0 ] }</div>
+      <div class="data" style="display:table-cell">${ value }</div>`
+  }
+  // for (const entry of Object.entries(v)) {
+  //   const value = toHtmlSimple(entry[ 1 ], options);
+  //   html += `<div style="display:table-row">
+  //     <div class="label" style="display:table-cell">${ entry[ 0 ] }</div>
+  //     <div class="data" style="display:table-cell">${ value }</div>
+  //     </div>`
+  // }
+  html += `</div>`;
+  return html;
+}
 
 /**
  * Creates a table of data points for each object in the map
@@ -24,10 +62,9 @@ export const fromList = (
   //eslint-disable-next-line functional/prefer-readonly-type
   data: Map<string, object>
 ): DataTable<Map<string, object>> => {
-  const parent = resolveEl(parentOrQuery);
-  //eslint-disable-next-line functional/no-let
+  const parent = resolveElement(parentOrQuery);
   let container: HTMLDivElement | undefined = document.createElement(
-    'DIV'
+    `DIV`
   ) as HTMLDivElement;
   parent.append(container);
 
@@ -41,13 +78,13 @@ export const fromList = (
   const update = (data: ReadonlyMap<string, object>) => {
     const seenTables = new Set();
 
-    for (const [key, value] of data) {
-      const tKey = `table-${key}`;
+    for (const [ key, value ] of data) {
+      const tKey = `table-${ key }`;
       seenTables.add(tKey);
-      //eslint-disable-next-line functional/no-let
-      let t = parent.querySelector(`#${tKey}`);
+      let t: HTMLElement | null = parent.querySelector(`#${ tKey }`);
       if (t === null) {
         t = document.createElement(`table`);
+        if (!t) throw new Error(`Could not create table element`);
         //eslint-disable-next-line functional/immutable-data
         t.id = tKey;
         parent.append(t);
@@ -57,12 +94,13 @@ export const fromList = (
     }
 
     // Remove tables that aren't present in map
+    // eslint-disable-next-line unicorn/prefer-spread
     const tables = Array.from(parent.querySelectorAll(`table`));
-    tables.forEach((t) => {
+    for (const t of tables) {
       if (!seenTables.has(t.id)) {
         t.remove();
       }
-    });
+    }
   };
 
   if (data) update(data);
@@ -95,6 +133,8 @@ const updateElement = (
   opts: DataTableOpts = {}
 ) => {
   const precision = opts.precision ?? 2;
+  const idPrefix = opts.idPrefix ?? ``;
+  const objectsAsTables = opts.objectsAsTables ?? false;
 
   if (data === undefined) {
     //eslint-disable-next-line functional/immutable-data
@@ -103,41 +143,37 @@ const updateElement = (
   }
   const seenRows = new Set();
 
-  for (const [key, value] of Object.entries(data)) {
-    const domKey = `row-${key}`;
+  for (const [ key, value ] of Object.entries(data)) {
+    const domKey = `${ idPrefix }-row-${ key }`;
     // Keep track of keys currently in object
     seenRows.add(domKey);
 
     // Is there already a row for this key?
-    //eslint-disable-next-line functional/no-let
-    let rowEl = t.querySelector(`tr[data-key='${domKey}']`);
+    let rowEl = t.querySelector(`tr[data-key='${ domKey }']`);
     if (rowEl === null) {
       // No row, add it
       rowEl = document.createElement(`tr`);
       t.append(rowEl);
-      //eslint-disable-next-line functional/immutable-data
       rowEl.setAttribute(`data-key`, domKey);
 
       const keyEl = document.createElement(`td`);
-      //eslint-disable-next-line functional/immutable-data
-      keyEl.innerText = key;
+      keyEl.textContent = key;
+      keyEl.classList.add(`label`);
       rowEl.append(keyEl);
     }
 
     // Is there already a cell for the value?
-    //eslint-disable-next-line functional/no-let
-    let valEl = rowEl.querySelector(`td[data-key='${domKey}-val']`);
+    let valEl = rowEl.querySelector(`td[data-key='${ domKey }-val']`);
 
     if (valEl === null) {
       // No, create the cell
       valEl = document.createElement(`td`);
-      //eslint-disable-next-line functional/immutable-data
-      valEl.setAttribute('data-key', `${domKey}-val`);
+      valEl.classList.add(`data`);
+      valEl.setAttribute(`data-key`, `${ domKey }-val`);
       rowEl.append(valEl);
     }
 
     // Format the value as HTML
-    //eslint-disable-next-line functional/no-let
     let valueHTML: string | undefined;
     if (opts.formatter) {
       valueHTML = opts.formatter(value, key);
@@ -146,31 +182,29 @@ const updateElement = (
     // If there's no formatter, or not handled...
     if (valueHTML === undefined) {
       if (typeof value === `object`) {
-        valueHTML = JSON5.stringify(value);
+        valueHTML = objectsAsTables ? toTableSimple(value, opts) : JSON5.stringify(value);
       } else if (typeof value === `number`) {
-        if (opts.roundNumbers) {
-          valueHTML = Math.round(value).toString();
-        } else {
-          valueHTML = value.toFixed(precision);
-        }
+        valueHTML = opts.roundNumbers ? Math.round(value).toString() : value.toFixed(precision);
+      } else if (typeof value === `boolean`) {
+        valueHTML = value ? `true` : `false`;
       } else {
-        valueHTML = (value as object).toString();
+        valueHTML = `JSON` + JSON.stringify(value);
       }
     }
 
     // Set the cell value
-    //eslint-disable-next-line functional/immutable-data
     (valEl as HTMLElement).innerHTML = valueHTML;
   }
 
   // Remove rows that aren't present in data
+  // eslint-disable-next-line unicorn/prefer-spread
   const rows = Array.from(t.querySelectorAll(`tr`));
-  rows.forEach((r) => {
-    const key = r.getAttribute('data-key');
+  for (const r of rows) {
+    const key = r.getAttribute(`data-key`);
     if (!seenRows.has(key)) {
       r.remove();
     }
-  });
+  }
 };
 
 /**
@@ -191,12 +225,13 @@ const updateElement = (
  * ```
  */
 export const fromObject = (
-  parentOrQuery: Readonly<HTMLElement | string>,
+  parentOrQuery: HTMLElement | string,
   data?: object,
   opts?: DataTableOpts
 ): DataTable<object> => {
-  const parent = resolveEl(parentOrQuery);
-  //eslint-disable-next-line functional/no-let
+  const parent = resolveElement(parentOrQuery);
+  const idPrefix = opts?.idPrefix ?? Math.floor(Math.random() * 1000).toString();
+
   let t: HTMLTableElement | undefined = document.createElement(`table`);
   parent.append(t);
 
@@ -212,7 +247,7 @@ export const fromObject = (
 
   const update = (d: object) => {
     if (!t) throw new Error(`Table disposed`);
-    updateElement(t, d, opts);
+    updateElement(t, d, { ...opts, idPrefix });
   };
 
   return { remove, update };
