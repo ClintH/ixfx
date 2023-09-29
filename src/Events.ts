3ca8fa0cdaa6cfded714cc8b81116d9fbbc17a1a
CH <clint@thestaticvoid.net>
2023-09-29 09:48:14 +0200

    Linting

--- src/Events.ts
+++ src/Events.ts
@@ -2,57 +2,6 @@
 import type { ISimpleEventEmitter, Listener } from './ISimpleEventEmitter.js';
 import { ofSimpleMutable } from './collections/map/MapOfSimpleMutable.js';
 
-
-// type FlowSource = {
-//   name:string,
-//   dispose():void,
-//   input:FlowSink,
-// };
-
-// type FlowHandler = (args?:any) => void;
-
-// interface FlowSink {
-//  [key:string]: FlowHandler;
-// }
-
-// export type Debouncer = {
-//   reset:()=>void
-//   dispose:()=>void
-// }
-
-// const sinkify = (handler:FlowHandler): FlowSink => ({ '*': handler });
-
-// export const debounceFactory = (sink:FlowSink, opts:{timeoutMs:number}): FlowSource => {
-//   let timer:number|undefined;
-
-//   const input = sinkify(() => {
-//     if (timer) window.clearTimeout(timer);
-//     timer = window.setTimeout(() => { sink[`*`](null); }, opts.timeoutMs);
-//   });
-
-//   const dispose = () => {
-//     if (timer) window.clearTimeout(timer);
-//     timer = undefined;
-//   };
-
-//   return { input, dispose, name:`debounce` };
-// };
-
-// export const debounce = (triggered:()=>void, timeoutMs:number):Debouncer => {
-//   const opts = { timeoutMs: timeoutMs};
-
-//   const sink:FlowSink = {
-//     '*': () => {
-//       triggered();
-//     }
-//   };
-//   const source = debounceFactory(sink, opts);
-//   const reset = () => {
-//     source.input[`*`](null);
-//   };
-//   return {...source, reset};
-// };
-
 export class SimpleEventEmitter<Events> implements ISimpleEventEmitter<Events> {
   readonly #listeners = ofSimpleMutable<Listener<Events>>();
 
