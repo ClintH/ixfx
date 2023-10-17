18c5d808dd0e49c0aa38b5126156484bf64c74ef
CH <clint@thestaticvoid.net>
2023-10-17 20:06:01 +0200

    Create MakeGlobal.ts

---
+++ src/MakeGlobal.ts
@@ -0,0 +1,33 @@
+import * as Util from './Util.js';
+import * as Random from './Random.js';
+import * as Data from './data/index.js';
+import * as Geometry from './geometry/index.js';
+import * as Text from './Text.js';
+import * as Numbers from './Numbers.js';
+import * as Io from './io/index.js';
+import * as Flow from './flow/index.js';
+import * as Generators from './Generators.js';
+import * as Visual from './visual/index.js';
+import * as Dom from './dom/index.js';
+import * as Events from './Events.js';
+import * as Modulation from './modulation/index.js';
+import { Collections } from './index.js';
+export * as Collections from './collections/index.js';
+try {
+  if (typeof window !== `undefined`) {
+    (window as any).Util = Util;
+    (window as any).Random = Random;
+    (window as any).Data = Data;
+    (window as any).Text = Text;
+    (window as any).Numbers = Numbers;
+    (window as any).Io = Io;
+    (window as any).Geometry = Geometry;
+    (window as any).Flow = Flow;
+    (window as any).Generators = Generators;
+    (window as any).Visual = Visual;
+    (window as any).Events = Events;
+    (window as any).Modulation = Modulation;
+    (window as any).Dom = Dom;
+    (window as any).Collections = Collections;
+  }
+} catch { /* no-op */ }
