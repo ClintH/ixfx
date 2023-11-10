d72d6345cc97ce2574a687b16dc4ae3a8008a863
CH <clint@thestaticvoid.net>
2023-11-10 22:25:37 +0100

    Update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -14,6 +14,7 @@ export * from './IntervalTracker.js';
 export * from './PointTracker.js';
 export * from './TrackedValue.js';
 export * from './TrackerBase.js';
+export * from './PrimitiveTracker.js';
 
 export * from './Clamp.js';
 export * from './Scale.js';
