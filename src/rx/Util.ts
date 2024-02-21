6d6c6402cd21fc234511f3f4d5a6ab58c6bc0c39
CH <clint@thestaticvoid.net>
2024-02-21 14:26:30 +0100

    Create Util.ts

---
+++ src/rx/Util.ts
@@ -0,0 +1,43 @@
+import type { Passed, PassedSignal, PassedValue, Reactive, ReactiveDiff, ReactiveDisposable, ReactiveInitial, ReactiveOrSource } from "./Types.js";
+
+export function messageIsSignal<V>(message: Passed<V> | PassedSignal): message is PassedSignal {
+  if (message.value !== undefined) return false;
+  if (`signal` in message && message.signal !== undefined) return true;
+  return false;
+}
+
+export function messageIsDoneSignal<V>(message: Passed<V> | PassedSignal): boolean {
+  if (message.value !== undefined) return false;
+  if (`signal` in message && message.signal === `done`) return true;
+  return false;
+}
+
+/**
+ * Returns _true_ if `v` has a non-undefined value. Note that sometimes
+ * _undefined_ is a legal value to pass
+ * @param v 
+ * @returns 
+ */
+export function messageHasValue<V>(v: Passed<V> | PassedSignal): v is PassedValue<V> {
+  if (v.value !== undefined) return true;
+  return false;
+}
+
+export const hasLast = <V>(rx: Reactive<V> | ReactiveDiff<V>): rx is ReactiveInitial<V> => {
+  if (`last` in rx) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const v = (rx as any).last();
+    if (v !== undefined) return true;
+  }
+  return false;
+}
+
+export const isDisposable = (v: object): v is ReactiveDisposable => {
+  return (`isDisposed` in v && `dispose` in v);
+}
+
+export const opify = <V>(fn: (source: ReactiveOrSource<V>, ...args: Array<any>) => Reactive<V>, ...args: Array<any>) => {
+  return (source: ReactiveOrSource<V>) => {
+    return fn(source, ...args);
+  }
+}
