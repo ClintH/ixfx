2845fd381071e5dcefa75981510e54ab98847953
CH <clint@thestaticvoid.net>
2024-03-15 22:17:35 +0100

    Arrays.insertAt

--- src/collections/arrays/index.ts
+++ src/collections/arrays/index.ts
@@ -477,6 +477,27 @@ export const remove = <V>(
 };
 
 /**
+ * Inserts `values` at position `index`, shuffling remaining
+ * items further down.
+ * @param data 
+ * @param index 
+ * @param values 
+ * @returns 
+ */
+export const insertAt = <V>(
+  //eslint-disable-next-line functional/prefer-readonly-type
+  data: ReadonlyArray<V> | Array<V>,
+  index: number,
+  ...values: Array<V>
+): Array<V> => {
+  if (!Array.isArray(data)) {
+    throw new TypeError(`Param 'data' is not an arry`);
+  }
+  return [ ...data.slice(0, index), ...values, ...data.slice(index + 1) ];
+};
+
+
+/**
  * Groups data by a function `grouper`, returning data as a map with string
  * keys and array values. Multiple values can be assigned to the same group.
  *
