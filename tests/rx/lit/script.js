95eeab772c8d7a367414ec9f4070e207a791a30b
CH <clint@thestaticvoid.net>
2024-03-15 22:19:24 +0100

    Rx visual tests

---
+++ tests/rx/lit/script.js
@@ -0,0 +1,50 @@
+
+import * as Rx from '../../../dist/rx.js';
+/**
+ * @typedef {{
+*  text:string
+*  id:number
+* }} Todo
+*/
+
+let idIndex = 0;
+
+/**
+ * 
+ * @param {*} event 
+ * @returns 
+ */
+const deleteHandler = event => {
+  /** @type HTMLElement */
+  const el = event.target;
+  if (!el) return;
+  const id = Number.parseInt(el.getAttribute(`data-id`) ?? `-1`);
+  if (!id || id < 0) return;
+  todos.deleteWhere(v => v.id === id);
+}
+
+const addHandler = event => {
+  const text = window.prompt(`Todo`, `blah`);
+  if (!text || text.length === 0) return;
+  todos.push(create(text));
+  console.log(todos.last());
+}
+
+/**
+ * Create todo
+ * @param {string} text 
+ * @returns Todo
+ */
+const create = (text) => ({text, id: ++idIndex});
+
+// Responsive array
+const todos = Rx.fromArray([ create(`one`),create(`two`),create(`three`)]);
+
+// Render array
+Rx.lit(`#test`, todos, todos => {
+  return Rx.LitHtml`<h1>Todos</h1>
+  <ol>
+  ${todos?.map(t => Rx.LitHtml`<li >${t.text} <button data-id="${t.id}" @click=${deleteHandler}>✓</button></li>`)}
+  </ol>
+  <button @click=${addHandler}>+</button>`
+});
