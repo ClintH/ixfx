32ec83141cedb6ec2f881393bb7c63d4178b2af9
CH <clint@thestaticvoid.net>
2024-03-15 22:19:46 +0100

    Rx.lit

---
+++ src/rx/Lit.ts
@@ -0,0 +1,38 @@
+import { html, render, type TemplateResult } from 'lit-html';
+import { resolveEl } from '../dom/ResolveEl.js';
+import type { Reactive } from './Types.js';
+import { hasLast } from './index.js';
+
+export { html as LitHtml } from 'lit-html';
+
+/**
+ * Dynamically render a reactive value using [lit-html](https://lit.dev/docs/libraries/standalone-templates/).
+ * 
+ * ```js
+ * // Reactive number
+ * const num = Rx.number(10);
+ * 
+ * // Render value into #test as a H1 tag
+ * Rx.lit(`#test`, num, x => Rx.LitHtml`<h1>${x}</h1>`)
+ *
+ * // For demo purposes, update value.
+ * // Re-rendering will happen automatically.
+ * setInterval(() => {
+ *   num.set(Math.floor(Math.random()*100));
+ * }, 100);
+ * ```
+ * @param elOrQuery 
+ * @param source 
+ * @param gen 
+ */
+export const lit = <T>(elOrQuery: string | HTMLElement, source: Reactive<T>, gen: (s: T | undefined) => TemplateResult<any>) => {
+  const el = resolveEl(elOrQuery);
+  source.value(value => {
+    render(gen(value), el);
+  });
+
+  if (hasLast(source)) {
+    render(gen(source.last()), el);
+  }
+
+}
