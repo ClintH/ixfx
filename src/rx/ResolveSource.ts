b886a05f513f2c87c1ee2c6cd0ed6bbb9caace04
CH <clint@thestaticvoid.net>
2024-04-06 13:46:46 +0200

    Update ResolveSource.ts

--- src/rx/ResolveSource.ts
+++ src/rx/ResolveSource.ts
@@ -1,18 +1,40 @@
-import { generator } from "./FromGenerator.js";
-import type { Reactive, ReactiveOrSource } from "./Types.js";
+import type { Interval } from "../flow/IntervalType.js";
+import { isAsyncIterable, isIterable } from "../iterables/Iterable.js";
+import { fromGenerator } from "./FromGenerator.js";
+import type { FromGeneratorOptions, Reactive, ReactiveOrSource } from "./Types.js";
+import { isReactive } from "./Util.js";
+
+export type ResolveSourceOptions = {
+  /**
+   * Options when creating a reactive from a generator
+   * Default:  `{ lazy: true, interval: 5 }`
+   */
+  generator: FromGeneratorOptions
+}
 
 /**
  * Resolves various kinds of sources into a Reactive.
  * If `source` is an iterable/generator, it gets wrapped via `generator()`.
+ * 
+ * Default options:
+ * * generator: `{ lazy: true, interval: 5 }`
  * @param source 
  * @returns 
  */
-export const resolveSource = <V>(source: ReactiveOrSource<V>): Reactive<V> => {
-  if (`on` in source) return source;
+export const resolveSource = <V>(source: ReactiveOrSource<V>, options: Partial<ResolveSourceOptions> = {}): Reactive<V> => {
+  if (isReactive(source)) return source;
+  const generatorOptions = options.generator ?? { lazy: `initial`, interval: 5 }
+
   // eslint-disable-next-line unicorn/prefer-ternary
   if (Array.isArray(source)) {
-    return generator(source.values(), { lazy: true });
+    return fromGenerator(source.values(), generatorOptions);
   } else {
-    return generator(source, { lazy: true });
+    if (`source` in source && `toArrayOrThrow` in source) {
+      return source.source;
+    }
+    if (isIterable(source) || isAsyncIterable(source)) {
+      return fromGenerator(source, generatorOptions);
+    }
   }
+  throw new Error(`Unable to resolve source. Supports: array, Reactive, Async/Iterable. Got type: ${ typeof source }`);
 }
