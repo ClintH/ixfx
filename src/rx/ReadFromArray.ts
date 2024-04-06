63e7bfd36d114a2f4aed28b9fb186507a5d21a02
CH <clint@thestaticvoid.net>
2024-04-06 13:46:41 +0200

    Update ReadFromArray.ts

--- src/rx/ReadFromArray.ts
+++ src/rx/ReadFromArray.ts
@@ -1,50 +1,60 @@
 import { continuously } from "../flow/Continuously.js";
 import { intervalToMs } from "../flow/IntervalType.js";
 import { initStream } from "./InitStream.js";
-import type { FromArrayOptions, Reactive, ReactiveFinite, ReactiveInitial } from "./Types.js";
+import type { ReadFromArrayOptions, Reactive, ReactiveFinite, ReactiveInitial } from "./Types.js";
 
 /**
  * Reads the contents of `array` into a Reactive, with optional time interval
- * between values.
+ * between values. A copy of the array is used, so changes will not
+ * affect the reactive.
+ * 
+ * See also {@link fromArray} which monitors changes to array values.
+ *
  * @param array 
  * @param options 
  * @returns 
  */
-export const readFromArray = <V>(array: Array<V>, options: Partial<FromArrayOptions> = {}): Reactive<V> & ReactiveFinite & ReactiveInitial<V> => {
-  const lazy = options.lazy ?? false;
-  const idle = options.idle ?? ``;
-  const intervalMs = intervalToMs(options.intervalMs, 5);
+export const readFromArray = <V>(sourceArray: Array<V>, options: Partial<ReadFromArrayOptions> = {}): Reactive<V> & ReactiveFinite & ReactiveInitial<V> => {
+  const lazy = options.lazy ?? `initial`;
+  const signal = options.signal;
+  const whenStopped = options.whenStopped ?? `continue`;
+  const debugLifecycle = options.debugLifecycle ?? false;
+  const array = [ ...sourceArray ];
+
+  const intervalMs = intervalToMs(options.interval, 5);
   let index = 0;
   let lastValue = array[ 0 ];
 
   const s = initStream<V>({
     onFirstSubscribe() {
-      //console.log(`Rx.fromArray onFirstSubscribe. Lazy: ${ lazy } reader state: ${ c.runState }`);
+      if (debugLifecycle) console.log(`Rx.readFromArray:onFirstSubscribe lazy: ${ lazy } runState: '${ c.runState }'`);
       // Start if in lazy mode and not running
-      if (lazy && c.runState === `idle`) c.start();
+      if (lazy !== `never` && c.runState === `idle`) c.start();
     },
     onNoSubscribers() {
-      //console.log(`Rx.fromArray onNoSubscribers. Lazy: ${ lazy } reader state: ${ c.runState } on idle: ${ idle }`);
-      if (lazy) {
-        if (idle === `pause`) {
-          c.cancel();
-        } else if (idle === `reset`) {
-          c.cancel();
+      if (debugLifecycle) console.log(`Rx.readFromArray:onNoSubscribers lazy: ${ lazy } runState: '${ c.runState }' whenStopped: '${ whenStopped }'`);
+      if (lazy === `very`) {
+        c.cancel();
+        if (whenStopped === `reset`) {
           index = 0;
         }
       }
     }
   });
+
   const c = continuously(() => {
     //console.log(`Rx.fromArray loop index ${ index } lazy: ${ lazy }`);
-
+    if (signal?.aborted) {
+      s.dispose(`Signalled (${ signal.reason })`);
+      return false; // stop looping
+    }
     lastValue = array[ index ];
     index++;
 
     s.set(lastValue)
     if (index === array.length) {
-      //console.log(`Rx.fromArray exiting continuously`);
-      return false;
+      s.dispose(`Source array complete`);
+      return false; // stop loop
     }
   }, intervalMs);
 
