c2ad56ae885031ce89924b4029819c45ca2e04fc
CH <clint@thestaticvoid.net>
2024-04-06 13:46:57 +0200

    Update Types.ts

--- src/rx/Types.ts
+++ src/rx/Types.ts
@@ -1,34 +1,68 @@
+import type { Primitive } from 'src/KeyValue.js';
 import type { ChangeRecord } from '../Compare.js';
 import type { Interval } from '../flow/IntervalType.js';
 import * as Immutable from '../Immutable.js';
+
+
+export type MergeOptions = {
+  /**
+   * How to handle when a source ends.
+   * * 'allow': continue mergeAsArrayStream, last value for done stream will kept
+   * * 'break': stop mergeAsArrayStream
+   * 
+   * Default: 'break'
+   */
+  onSourceDone: `allow` | `break`
+}
+
+export type SyncOptions = {
+
+  /**
+   * How to handle when a source completes.
+   * * 'allow' means we continue synchronising with remaining alive sources. Use 'finalValue' option to control what data is returned for completed sources
+   * * 'break' means we stop the stream, because synchronisation across all sources is no longer possible.
+   * 
+   * Default: 'break'.
+   */
+  onSourceDone: `allow` | `break`,
+  /**
+   * Maximum time to wait for synchronisation to happen.
+   * If interval is exceeded, stream closes.
+   * Default: 2s
+   */
+  maximumWait: Interval
+  /**
+   * If we continue synchronisation when a source is done (via `onSourceDone:'allow'`),
+   * what source should be returned for a completed source?
+   * * 'undefined': _undefined_
+   * * 'last': the last received value, or _undefined_
+   * 
+   * Default: 'undefined'
+   */
+  finalValue: `undefined` | `last`
+}
+
 
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 
 export const symbol = Symbol(`Rx`);
 
-export type SignalKinds = `done`;
+export type SignalKinds = `done` | `warn`;
 export type Passed<V> = {
   value: V | undefined
   signal?: SignalKinds
   context?: string
 }
 
-export type GeneratorOptions = {
-  /**
-   * By default (true) only accesses the generator if there is a subscriber.
-   */
-  lazy: boolean
-}
-
 export type PassedSignal = Passed<any> & {
   value: undefined
-  signal: `done`
+  signal: SignalKinds
   context: string
 }
 
 export type EventOptions<V> = {
-  process: (args?: Event | undefined) => V
-  lazy?: boolean
+  transform: (args?: Event | undefined) => V
+  lazy?: Lazy
   /**
    * If true, log messages are emitted
    * when event handlers are added/removed
@@ -45,9 +79,8 @@ export type PassedValue<V> = Passed<V> & {
   value: V
 }
 
-
 export type UpstreamOptions<In> = {
-  lazy: boolean
+  lazy: Lazy
   /**
    * If _true_ (default), we dispose the underlying stream if the upstream closes. This happens after onStop() is called.
    */
@@ -63,8 +96,9 @@ export type UpstreamOptions<In> = {
    * @returns
    */
   onStop: () => void
-}
 
+  onDispose: () => void
+}
 
 /**
  * Wrapped Reactive for object-oriented access
@@ -139,11 +173,15 @@ export type Wrapped<TIn> = {
   debounce: (options: Partial<DebounceOptions>) => Wrapped<TIn>
 
   /**
-   * Emits values when this stream and any additional streams produce a value.
-   * Outputted values captures the last value from each stream.
+   * Emits values when this stream and any additional streams produce a value. The resulting stream is
+   * thus an array of values, each source at a given index.
+   * Waits to output a value until each stream has produced a value. Thus, the pace is determined by
+   * the slowest stream.
    * @returns 
    */
-  synchronise: (...additionalSources: Array<ReactiveOrSource<TIn> | Wrapped<TIn>>) => Wrapped<Array<TIn | undefined>>
+  synchronise: <const T extends ReadonlyArray<ReactiveOrSource<any>>>(reactiveSources: T, options?: Partial<SyncOptions>) => Wrapped<[ TIn, ...RxValueTypes<T> ]>
+
+  //synchronise: (...additionalSources: Array<ReactiveOrSource<TIn> | Wrapped<TIn>>) => Wrapped<Array<TIn | undefined>>
   /**
    * Creates new streams for each case, sending values to the stream if they match the filter predicate
    * @param cases 
@@ -187,24 +225,112 @@ export type ToArrayOptions<V> = {
   fillValue: V
 }
 
-export type FromArrayOptions = {
+/**
+ * Laziness
+ * * start: only begins on first subscriber. Keeps running even when there are no subscribers
+ * * very: only begins on first subscriber. Stops looping if there are no subscribers
+ * * never: begins calling function when initalised and doesn't stop until Reactive is disposed
+ */
+export type Lazy = `initial` | `never` | `very`
+export type InitLazyStreamOptions = Partial<InitStreamOptions> & {
+  lazy: Lazy
+  onStart: () => void
+  onStop: () => void
+};
+
+export type CountOptions = { lazy: Lazy, amount: number, offset: number, interval: Interval, signal: AbortSignal }
+export type PingedFunctionOptions = {
   /**
-   * Interval between each item being read. 5ms by default.
+   * If _true_, stream closes if function throws an error.
+   * If _false_, errors are emitted as signals, but stream is not closed.
+   * Default: _true_
    */
-  intervalMs: Interval
+  closeOnError: boolean
   /**
-   * If _true_, only starts after first subscriber. _False_ by default.
+   * Laziness
+   * * start: only begins on first subscriber. Keeps running even when there are no subscribers
+   * * very: only begins on first subscriber. Stops looping if there are no subscribers
+   * * never: begins calling function when initalised and doesn't stop until Reactive is disposed
    */
-  lazy: boolean
+  lazy: Lazy,
   /**
-   * Governs behaviour if all subscribers are removed AND lazy=true. By default continues
-   * iteration.
-   * 
-   * * pause: stop at last array index
-   * * reset: go back to 0
-   * * empty: continue, despite there being no listeners (default)
+ * If specified, a time before invoking function.
+ * If `repeat` is used, this is in addition to `interval` time.
+ */
+  predelay: Interval,
+  /***
+* If specified, signal is checked to prevent function execution.
+* Also used for aborting a looped fromFunction.
+*/
+  signal: AbortSignal
+}
+
+export type FromFunctionOptions = {
+  /**
+   * If _true_, stream closes if function throws an error.
+   * If _false_, errors are emitted as signals, but stream is not closed.
+   * Default: _true_
    */
-  idle: `` | `pause` | `reset`
+  closeOnError: boolean
+  /**
+   * Laziness
+   * * start: only begins on first subscriber. Keeps running even when there are no subscribers
+   * * very: only begins on first subscriber. Stops looping if there are no subscribers
+   * * never: begins calling function when initalised and doesn't stop until Reactive is disposed
+   */
+  lazy: Lazy
+  /**
+   * If specified, sets an upper limit of how many times we loop
+   * (if this is also enabled)
+   */
+  maximumRepeats: number
+  /**
+   * If specified, function is called repeatedly with this delay
+   */
+  interval: Interval
+  /**
+   * If specified, a time before invoking function.
+   * If `repeat` is used, this is in addition to `interval` time.
+   */
+  predelay: Interval,
+  /***
+   * If specified, signal is checked to prevent function execution.
+   * Also used for aborting a looped fromFunction.
+   */
+  signal: AbortSignal
+}
+
+
+export type ReadFromArrayOptions = {
+  /**
+   * Interval between each item being read. Default: 5ms.
+   */
+  interval: Interval
+
+  lazy: Lazy
+  /**
+   * Behaviour when reactive stops, for example due to having no subscribers
+   * * continue: iteration continues through array where it left off
+   * * reset: iteration begins from start of array
+   */
+  whenStopped: `continue` | `reset`
+  debugLifecycle: boolean
+  signal: AbortSignal
+}
+
+export type FromGeneratorOptions = {
+  /**
+   * Minimum time interval between reads from generator
+   * Default: 5ms
+   */
+  interval: Interval
+  /**
+   * If _true_, only accesses the generator if there is a subscriber.
+   * Default: true
+   */
+  lazy: Lazy,
+  signal: AbortSignal
+
 }
 
 /**
@@ -218,7 +344,7 @@ export type SwitcherOptions = {
   match: `first` | `all`
 }
 
-export type ReactiveOrSource<V> = Reactive<V> | IterableIterator<V> | AsyncIterableIterator<V> | Generator<V> | AsyncGenerator<V> | Array<V>
+export type ReactiveOrSource<V> = Wrapped<V> | Reactive<V> | IterableIterator<V> | AsyncIterableIterator<V> | Generator<V> | AsyncGenerator<V> | Array<V>
 
 export type BindUpdateOpts<V> = {
   initial: (v: V, el: HTMLElement) => void,
@@ -244,9 +370,11 @@ export type Reactive<V> = {
    * ```
    * @param handler 
    */
-  on(handler: (value: Passed<V>) => void): () => void
-  value(handler: (value: V) => void): () => void
+  on(handler: (value: Passed<V>) => void): Unsubscriber
+  value(handler: (value: V) => void): Unsubscriber
 }
+
+export type Unsubscriber = () => void;
 
 export type ReactiveNonInitial<V> = Reactive<V> & {
   last(): V | undefined
@@ -407,6 +535,7 @@ export type PipeSet<In, Out> = [
 export type InitStreamOptions = {
   onFirstSubscribe: () => void
   onNoSubscribers: () => void
+  onDispose: () => void
 }
 
 export type ThrottleOptions = InitStreamOptions & {
@@ -433,15 +562,42 @@ export type PipeDomBinding = {
   remove(deleteElements: boolean): void
 }
 
+/**
+ * Transform options
+ */
 export type TransformOpts = InitStreamOptions;
+/**
+ * Cached stream options
+ */
+export type CacheOpts<V> = InitStreamOptions & {
+  /**
+   * Initial value
+   */
+  initialValue: V,
+  /**
+   * Laziness
+   */
+  lazy: Lazy
+}
 
 export type BatchOptions = InitStreamOptions & {
   /**
    * If _true_ (default) remaining results are yielded
-   * if source closes. If _false_, only 'complete' batches are yielded.
+   * if source closes. If _false_, only batches that meet
+   * `elapsed` or `quantity` are emitted.
    */
   returnRemainder: boolean
+  /**
+   * Amount of time to gather results for a batch.
+   * 'elapsed' and 'quantity' is ORed. Meaning a batch will the minimum of
+   * 'elapsed' and 'quantity'
+   */
   elapsed: Interval
+  /**
+   * Number of items to gather for a batch.
+   * 'elapsed' and 'quantity' is ORed. Meaning a batch will the minimum of
+   * 'elapsed' and 'quantity'
+   */
   quantity: number
 }
 
@@ -491,14 +647,8 @@ export type ResolveOptions = {
    * Delay before value
    */
   interval: Interval
-  /**
-   * If _true_, resolution only starts after first subscriber. Looping, if active,
-   * stops if there are no subscribers.
-   * 
-   * _False_ by default.
-   * 
-   */
-  lazy: boolean
+
+  lazy: Lazy
 }
 
 export type ReactiveOpInit<TIn, TOut, TOpts> = (options: Partial<TOpts>) => ReactiveOp<TIn, TOut>
@@ -516,3 +666,47 @@ export type DebounceOptions = InitStreamOptions & {
    */
   elapsed: Interval
 }
+
+export type RxValueTypes<T extends ReadonlyArray<ReactiveOrSource<any>>> =
+  { [ K in keyof T ]: T[ K ] extends Reactive<infer V> ? V | undefined :
+    T[ K ] extends Wrapped<infer V> ? V | undefined :
+    T[ K ] extends Generator<infer V> ? V | undefined :
+    T[ K ] extends AsyncGenerator<infer V> ? V | undefined :
+    T[ K ] extends IterableIterator<infer V> ? V | undefined :
+    T[ K ] extends AsyncIterableIterator<infer V> ? V | undefined :
+    T[ K ] extends Array<infer V> ? V | undefined :
+    never };
+
+export type RxValueTypeObject<T extends Record<string, ReactiveOrSource<any>>> =
+  { [ K in keyof T ]: T[ K ] extends Reactive<infer V> ? V | undefined :
+    T[ K ] extends Wrapped<infer V> ? V | undefined :
+    T[ K ] extends Generator<infer V> ? V | undefined :
+    T[ K ] extends AsyncGenerator<infer V> ? V | undefined :
+    T[ K ] extends IterableIterator<infer V> ? V | undefined :
+    T[ K ] extends AsyncIterableIterator<infer V> ? V | undefined :
+    T[ K ] extends Array<infer V> ? V | undefined :
+    never };
+
+
+export type PrimitiveValueTypeObject<T extends Record<string, Primitive>> =
+  { [ K in keyof T ]:
+    T[ K ] extends number ? number | undefined :
+    T[ K ] extends string ? string | undefined :
+    T[ K ] extends boolean ? boolean | undefined :
+    T[ K ] extends bigint ? bigint | undefined :
+    never };
+
+export type RxPrimitiveValueTypeObject<T extends Record<string, Primitive | ReactiveOrSource<any>>> =
+  { [ K in keyof T ]:
+    T[ K ] extends number ? number | undefined :
+    T[ K ] extends string ? string | undefined :
+    T[ K ] extends boolean ? boolean | undefined :
+    T[ K ] extends bigint ? bigint | undefined :
+    T[ K ] extends Reactive<infer V> ? V | undefined :
+    T[ K ] extends Wrapped<infer V> ? V | undefined :
+    T[ K ] extends Generator<infer V> ? V | undefined :
+    T[ K ] extends AsyncGenerator<infer V> ? V | undefined :
+    T[ K ] extends IterableIterator<infer V> ? V | undefined :
+    T[ K ] extends AsyncIterableIterator<infer V> ? V | undefined :
+    T[ K ] extends Array<infer V> ? V | undefined :
+    never };
