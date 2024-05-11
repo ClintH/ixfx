492330dd4345e8155b9c8dbab41bdd56dfb20478
CH <clint@thestaticvoid.net>
2024-05-11 20:31:09 +0200

    Update Rect.Test.ts

--- src/__test/geometry/Rect.Test.ts
+++ src/__test/geometry/Rect.Test.ts
@@ -7,7 +7,8 @@ test(`corners`, t => {
   const r2 = Rects.corners({ width: 20, height: 5 }, { x: 10, y: 10 });
   t.deepEqual(r1, r2);
   t.deepEqual(r1, [
-    { x: 10, y: 10 }, { x: 30, y: 10 }, { x: 15, y: 10 }, { x: 30, y: 15 }
+    // ne, nw, sw, se
+    { x: 10, y: 10 }, { x: 30, y: 10 }, { x: 30, y: 15 }, { x: 10, y: 15 }
   ])
 });
 
