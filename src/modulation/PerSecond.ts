2dab105c1c7dcdcfe254128daedb26cf24268b94
CH <clint@thestaticvoid.net>
2024-05-11 20:25:29 +0200

    Update PerSecond.ts

--- src/modulation/PerSecond.ts
+++ src/modulation/PerSecond.ts
@@ -1,13 +1,17 @@
+import { relativeTimer } from "src/flow/index.js"
+
 /**
  * Returns a proportion of `amount` depending on elapsed time.
  * ```js
+ * // Calculate a proportion of 0.1 every second
  * const x = perSecond(0.1);
  * x();
  * ```
  * 
  * The faster `x()` is called, the smaller the chunks of `amount` are returned.
+ * Values accumulate. If, for example `x()` isn't called for two seconds, 2*amount is returned.
  * 
- * @example Usage example
+ * @example Usage
  * ```js
  * const settings = Object.freeze({
  *  ageMod: perSecond(0.1);
@@ -29,16 +33,26 @@
  *  }
  * });
  * ```
+ * 
+ * Options:
+ * * max: if specified, the max return value
+ * * min: if specified, the min return value
  * @param amount
  * @returns 
  */
-export const perSecond = (amount: number) => {
+export const perSecond = (amount: number, options: Partial<{ max: number, min: number }> = {}) => {
   const perMilli = amount / 1000;
+  const min = options.min ?? Number.MIN_SAFE_INTEGER;
+  const max = options.max ?? Number.MAX_SAFE_INTEGER;
   let called = performance.now();
 
   return () => {
-    const elapsed = performance.now() - called;
-    called = performance.now();
-    return perMilli * elapsed;
+    const now = performance.now();
+    const elapsed = now - called;
+    called = now;
+    const x = perMilli * elapsed;
+    if (x > max) return max;
+    if (x < min) return min;
+    return x;
   }
 }
