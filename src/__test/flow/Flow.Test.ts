99872bfc40f3db6662fbfd9230257acf25df64fd
CH <clint@thestaticvoid.net>
2024-06-07 11:13:02 +0200

    SyncWait: simple synchronisation object that waits until a signal happens

--- src/__test/flow/Flow.Test.ts
+++ src/__test/flow/Flow.Test.ts
@@ -3,6 +3,42 @@ import test from 'ava';
 import { continuously } from '../../flow/Continuously.js';
 import { JSDOM } from 'jsdom';
 import { sleep } from '../../flow/Sleep.js';
+import { SyncWait } from '../../flow/SyncWait.js';
+
+test(`syncWait`, async t => {
+  const sw = new SyncWait();
+  let signalled = false;
+  setTimeout(() => {
+    signalled = true;
+    sw.signal();
+  }, 100);
+
+  await sw.forSignal();
+  t.true(signalled);
+
+  signalled = false;
+  setTimeout(() => {
+    signalled = true;
+    sw.signal();
+  }, 100);
+
+  await sw.forSignal();
+  t.true(signalled);
+
+  // Test signal before wait
+  const sw2 = new SyncWait();
+  sw2.signal();
+  await sw2.forSignal();
+
+  // Test elapsed
+  const sw3 = new SyncWait();
+  try {
+    await sw3.forSignal(100);
+    t.fail(`Exception not thrown`);
+  } catch (error) {
+    t.pass(`Exception thrown`);
+  }
+});
 
 test(`sleep`, async t => {
   const ac = new AbortController();
@@ -32,7 +68,7 @@ test('continuously', async (t) => {
     return loops > 0;
   };
 
-  const c = continuously(fn, 200);
+  const c = continuously(fn, 50);
   t.is(c.startCount, 0);
   t.is(c.runState, `idle`);
   c.start();
@@ -40,7 +76,7 @@ test('continuously', async (t) => {
 
   //return new Promise(async (resolve, reject) => {
   await sleep(expectedElapsed + 50);
-  t.true(c.startCount > 0);
+  t.true(c.startCountTotal > 0, `startCount: ${ c.startCountTotal }`);
   const elapsed = Date.now() - startedAt;
   t.true(Math.abs(elapsed - expectedElapsed) < 100, `Elapsed time too long: ${ elapsed }, expected: ${ expectedElapsed }`);
 
