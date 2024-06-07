f31a3b4d1d1667f2ce1b2c3a8f97cd8eee3a93c0
CH <clint@thestaticvoid.net>
2024-06-07 09:57:48 +0200

    Use Color.js instead of d3, resolveToString

--- src/visual/Colour.ts
+++ src/visual/Colour.ts
@@ -1,59 +1,127 @@
-import * as d3Colour from 'd3-color';
-import * as d3Interpolate from 'd3-interpolate';
+import Color, { type Range } from 'colorjs.io';
+
 import { defaultRandom, type RandomSource } from '../random/Types.js';
 import { throwNumberTest } from '../Guards.js';
+import { piecewise } from '../collections/arrays/index.js';
+import { scale as scaleNumber } from '../data/Scale.js';
+import { clamp } from '../data/Clamp.js';
 
 export type Hsl = { h: number; s: number; l: number; opacity?: number };
+export type Hsla = Hsl & { opacity: number };
 export type Rgb = { r: number; g: number; b: number; opacity?: number };
-export type Spaces = `hsl` | `rgb` | `lab` | `hcl` | `cubehelix`;
-/**
- * @private
- */
-export type Colour = d3Colour.RGBColor | d3Colour.HSLColor;
+export type Rgba = Rgb & { opacity: number };
+export type Spaces = `hsl` | `hsluv` | `rgb` | `srgb` | `lch` | `oklch` | `oklab` | `okhsl` | `p3` | `lab` | `hcl` | `cubehelix`;
+
 
 /**
  * A representation of colour. Eg: `blue`, `rgb(255,0,0)`, `hsl(20,100%,50%)`
  */
-export type Colourish = string | d3Colour.ColorCommonInstance;
+export type Colourish = Color | Hsl | Hsla | Rgb | Rgba | string;
 
 /**
  * Options for interpolation
  */
 export type InterpolationOpts = {
-  /**
-   * Gamma correction. Eg 4 brightens values. Only applies to rgb and cubehelix
-   * [Read more](https://github.com/d3/d3-interpolate#interpolate_gamma)
-   */
-  gamma?: number;
-  /**
-   * Colour space
-   */
-  space?: Spaces;
-  /**
-   * If true, interpolation happens the longer distance. Only applies to hsl, hcl and cubehelix
-   */
-  long?: boolean;
+  space: Spaces,
+  hue: `longer` | `shorter` | `increasing` | `decreasing` | `raw`
 };
 
 /**
- * Parses colour to `{ h, s, l }`. `opacity` field is added if it exists on source.
+ * Parses colour to `{ h, s, l }`, each field being on 0..1 scale.
+ * 
+ * Note that some colours will return NaN for h,s or l. This is because they have
+ * indeterminate hue. For example white, black and transparent.
  * @param colour
  * @returns
  */
 export const toHsl = (colour: Colourish): Hsl => {
-  const c = resolveColour(colour);
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  if (c === null) throw new Error(`Could not resolve colour ${ colour }`);
-
-  if (isHsl(c)) return c;
-  if (isRgb(c)) {
-    const asHsl = d3Colour.hsl(c);
-    if (c.opacity) return { ...asHsl, opacity: c.opacity };
-    return asHsl;
+  const c = resolve(colour);
+  const hsl = c.hsl;
+  const parsedHsl = {
+    h: hsl[ 0 ] / 360,
+    s: hsl[ 1 ] / 100,
+    l: hsl[ 2 ] / 100
   }
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  throw new Error(`Could not resolve colour ${ colour }`);
+  if (c.alpha !== 1) return { ...parsedHsl, opacity: c.alpha / 100 };
+  return parsedHsl;
+
+  // if (isHsl(colour)) return colour;
+  // const c = resolveColour(colour);
+  // // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  // if (c === null) throw new Error(`Could not resolve colour ${ colour }`);
+
+  // if (isHsl(c)) {
+  //   if (Number.isNaN(c.h) && Number.isNaN(c.s)) {
+  //     return { h: Number.NaN, s: Number.NaN, l: c.l, opacity: c.opacity }
+  //   }
+  //   return c;
+  // }
+  // if (isRgb(c)) {
+  //   const asHsl = d3Colour.hsl(c);
+  //   if (c.opacity) return { ...asHsl, opacity: c.opacity };
+  //   return asHsl;
+  // }
+  // // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  // throw new Error(`Could not resolve colour ${ colour }`);
 };
+
+const hslToColorJs = (hsl: Hsl): Color => {
+  const coords: [ number, number, number ] = [
+    hsl.h * 360,
+    hsl.s * 100,
+    hsl.l * 100
+  ];
+  return `opacity` in hsl ? new Color(`hsl`, coords, hsl.opacity) : new Color(`hsl`, coords);
+}
+
+const rgbToColorJs = (rgb: Rgb): Color => {
+  const coords: [ number, number, number ] = [
+    rgb.r,
+    rgb.g,
+    rgb.b
+  ];
+  return `opacity` in rgb ?
+    new Color(`srgb`, coords, rgb.opacity) :
+    new Color(`srgb`, coords);
+}
+
+/**
+ * Returns a colour in the best-possible CSS colour form.
+ * The return value can be used setting colours in the canvas or DOM manipulations.
+ * 
+ * See also: {@link toHslaString} 
+ * @param colour 
+ */
+export const toString = (colour: Colourish): string => {
+  const c = resolve(colour);
+  return c.display();
+}
+
+/**
+ * Returns a colour in hsla(0-360,100%,1) form.
+ * See also: {@link toString}
+ * @param colour 
+ * @returns 
+ */
+export const toHslaString = (colour: Colourish): string => {
+  const c = resolve(colour);
+  return c.toString({ format: `hsla` });
+
+  // if (typeof colour === `string`) {
+  //   if (colour.startsWith(`hsla(`)) return colour;
+  //   if (colour.startsWith(`hsl(`)) return colour;
+  // }
+  // // eslint-disable-next-line prefer-const
+  // let { h, s, l, opacity } = toHsl(colour);
+  // if (Number.isNaN(h)) h = 0;
+  // if (Number.isNaN(s)) s = 0;
+  // if (Number.isNaN(l)) l = 0;
+
+  // if (opacity !== undefined) {
+  //   return `hsla(${ Math.floor(h * 360) },${ Math.floor(s * 100) }%,${ Math.floor(l * 100) }%,${ opacity })`
+  // }
+  // return `hsla(${ Math.floor(h * 360) },${ Math.floor(s * 100) }%,${ Math.floor(l * 100) }%,1)`
+}
 
 /**
  * Returns a full HSL colour string (eg `hsl(20,50%,75%)`) based on a index.
@@ -104,29 +172,72 @@ export const randomHue = (rand: RandomSource = defaultRandom): number => {
 };
 
 /**
- * Parses colour to `{ r, g, b }`. `opacity` field is added if it exists on source.
+ * Parses colour to `{ r, g, b }` where each field is on 0..1 scale.
+ * `opacity` field is added if opacity is not 1.
  * [Named colours](https://html-color-codes.info/color-names/)
  * @param colour
  * @returns
  */
 export const toRgb = (colour: Colourish): Rgb => {
-  const c = resolveColour(colour);
-  const rgb = c.rgb();
-  return c.opacity < 1 ? { r: rgb.r, g: rgb.g, b: rgb.b, opacity: c.opacity } : { r: rgb.r, g: rgb.g, b: rgb.b };
+  const c = resolve(colour);
+  const rgb = c.srgb;
+  return c.alpha < 1 ?
+    { r: rgb.r, g: rgb.g, b: rgb.b, opacity: c.alpha } :
+    { r: rgb.r, g: rgb.g, b: rgb.b };
 };
 
-const resolveColour = (c: Colourish): Colour => {
-  if (typeof c === `string`) {
-    const css = d3Colour.color(c);
-    if (css !== null) return css;
-    const error = c.startsWith(`hsl`) && c.indexOf(`%`) <= 0 ? new Error(`Could not resolve CSS colour ${ c }. HSL values should be in the form: hsl(0, 50%, 50%)`) : new Error(`Could not resolve CSS colour ${ c }`);
-    throw error;
+/**
+ * Parses a string representation of colour, or a Rgb/Hsl object.
+ * If the string starts with '--' it's assumed to be a CSS variable
+ * 
+ * See also {@link resolveToString} to resolve to a CSS colour string.
+ * @param colour Colour to resolve
+ * @returns Color.js Color object
+ */
+export const resolve = (colour: Colourish): Color => {
+  if (typeof colour === `string`) {
+    if (colour.startsWith(`--`)) {
+      // Resolve CSS variable
+      colour = getComputedStyle(document.body).getPropertyValue(colour);
+      console.log(`CSS colour: ${ colour }`);
+    }
+    return new Color(colour);
   } else {
-    if (isHsl(c)) return d3Colour.hsl(c.h, c.s, c.l);
-    if (isRgb(c)) return d3Colour.rgb(c.r, c.g, c.b);
+    if (isHsl(colour)) return new Color(hslToColorJs(colour));
+    if (isRgb(colour)) return new Color(rgbToColorJs(colour));
   }
-  throw new Error(`Could not resolve colour ${ JSON.stringify(c) }`);
+  return colour;
 };
+
+/**
+ * Like {@link resolve}, but returns a CSS-ready string
+ * representation.
+ * ```js
+ * element.style.backgroundColor = resolveToString(`red`);
+ * ```
+ * 
+ * Tries each parameter in turn, returning the value
+ * for the first that resolves. This can be useful for
+ * having fallback values.
+ * 
+ * ```js
+ * // Try a CSS variable, a object property or finally fallback to red.
+ * element.style.backgroundColor = resolveToString('--some-var', opts.background, `red`);
+ * ```
+ * @param colour 
+ * @returns 
+ */
+export const resolveToString = (...colours: Array<Colourish | undefined>): string => {
+  for (const colour of colours) {
+    if (colour === undefined) continue;
+    if (colour === null) continue;
+    const c = resolve(colour);
+    const hsl = c.hsl;
+    console.log(`hsl:`, hsl);
+    return c.display();
+  }
+  return `rebeccapurple`;
+}
 
 /**
  * Returns a colour in hex format `#000000`. 
@@ -134,12 +245,14 @@ const resolveColour = (c: Colourish): Colour => {
  * colour (eg. "hsl(0, 50%, 50%)")", an object {h,s,l} or {r,g,b}.
  * Note that
  * '#' is included as a prefix.
+ * 
+ * Transparent colour is returned as #00000000
  * @param colour
  * @returns Hex format, including #
  */
 export const toHex = (colour: Colourish): string => {
-  const c = resolveColour(colour);
-  return c.formatHex();
+  const c = resolve(colour);
+  return c.toString({ format: `hex` });
 };
 
 /**
@@ -161,8 +274,8 @@ export const toHex = (colour: Colourish): string => {
  * @returns String representation of colour
  */
 export const opacity = (colour: Colourish, amt: number): string => {
-  const c = resolveColour(colour);
-  c.opacity *= amt;
+  const c = resolve(colour);
+  c.alpha *= amt;
   return c.toString();
 };
 
@@ -207,180 +320,119 @@ export const getCssVariable = (
  * @param optsOrSpace Options for interpolation, or string name for colour space, eg `hsl`.
  * @returns String representation of colour, eg. `rgb(r,g,b)`
  */
-export const interpolate = (
-  amount: number,
-  from: Colourish,
-  to: Colourish,
-  optsOrSpace?: string | InterpolationOpts
-): string => {
-  throwNumberTest(amount, `percentage`, `amount`);
-  if (typeof from !== `string`)
-    throw new Error(`Expected string for 'from' param`);
-  if (typeof to !== `string`) throw new Error(`Expected string for 'to' param`);
+// export const interpolate = (
+//   amount: number,
+//   from: Colourish,
+//   to: Colourish,
+//   optsOrSpace?: string | InterpolationOpts
+// ): string => {
+//   throwNumberTest(amount, `percentage`, `amount`);
+//   if (typeof from !== `string`)
+//     throw new Error(`Expected string for 'from' param`);
+//   if (typeof to !== `string`) throw new Error(`Expected string for 'to' param`);
 
-  let opts: InterpolationOpts;
-  if (typeof optsOrSpace === `undefined`) opts = {};
-  else if (typeof optsOrSpace === `string`)
-    opts = { space: optsOrSpace as Spaces };
-  else opts = optsOrSpace;
+//   let opts: InterpolationOpts;
+//   if (typeof optsOrSpace === `undefined`) opts = {};
+//   else if (typeof optsOrSpace === `string`)
+//     opts = { space: optsOrSpace as Spaces };
+//   else opts = optsOrSpace;
 
-  const inter = getInterpolator(opts, [ from, to ]);
-  if (inter === undefined) throw new Error(`Could not handle colour/space`);
-  return inter(amount);
-};
+//   const inter = getInterpolator(opts, [ from, to ]);
+//   if (inter === undefined) throw new Error(`Could not handle colour/space`);
+//   return inter(amount);
+// };
 
-const getInterpolator = (
-  optsOrSpace: InterpolationOpts | string,
-  colours: Array<Colourish>
-): ((t: number) => string) | undefined => {
-  if (!Array.isArray(colours))
-    throw new Error(`Expected one or more colours as parameters`);
+export const interpolator = (colours: Array<Colourish>, opts: Partial<InterpolationOpts> = {}) => {
+  const space = opts.space ?? `lch`;
+  const hue = opts.hue ?? `shorter`;
+  const pieces = interpolatorInit(colours);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+  const ranges = pieces.map(piece => (piece[ 0 ] as any).range(piece[ 1 ], { space, hue })) as Array<Range>;
 
-  let opts: InterpolationOpts;
-  if (typeof optsOrSpace === `undefined`) opts = {};
-  else if (typeof optsOrSpace === `string`)
-    opts = { space: optsOrSpace as Spaces };
-  else opts = optsOrSpace;
+  return (amt: number) => {
+    amt = clamp(amt);
 
-  if (!Array.isArray(colours))
-    throw new Error(`Expected array for colours parameter`);
-  if (colours.length < 2)
-    throw new Error(`Interpolation expects at least two colours`);
-  const { space = `rgb`, long = false } = opts;
-  let inter;
-
-  switch (space) {
-    case `lab`: {
-      inter = d3Interpolate.interpolateLab;
-      break;
-    }
-    case `hsl`: {
-      inter = long
-        ? d3Interpolate.interpolateHslLong
-        : d3Interpolate.interpolateHsl;
-      break;
-    }
-    case `hcl`: {
-      inter = long
-        ? d3Interpolate.interpolateHclLong
-        : d3Interpolate.interpolateHcl;
-      break;
-    }
-    case `cubehelix`: {
-      inter = long
-        ? d3Interpolate.interpolateCubehelixLong
-        : d3Interpolate.interpolateCubehelix;
-      break;
-    }
-    case `rgb`: {
-      inter = d3Interpolate.interpolateRgb;
-    }
-    default: {
-      inter = d3Interpolate.interpolateRgb;
-    }
+    // Scale to 0..1 to 0...ranges.length
+    const s = scaleNumber(amt, 0, 1, 0, ranges.length);
+    const index = Math.floor(s);
+    const amtAdjusted = s - index;
+    //console.log(`Colour.interpolator: amt: ${ amt } s: ${ s } index: ${ index } amtAdjusted: ${ amtAdjusted } ranges: ${ ranges.length } steps: ${ steps }`);
+    return ranges[ index ](amtAdjusted);
   }
+}
 
-  if (opts.gamma && (space === `rgb` || space === `cubehelix`)) {
-    inter = (inter as d3Interpolate.ColorGammaInterpolationFactory).gamma(
-      opts.gamma
-    );
-  }
-
-  return colours.length > 2 ? d3Interpolate.piecewise(inter, colours) : inter(colours[ 0 ], colours[ 1 ]);
-};
+const interpolatorInit = (colours: Array<Colourish>) => {
+  if (!Array.isArray(colours)) throw new Error(`Param 'colours' is not an array as expected. Got: ${ typeof colours }`);
+  if (colours.length < 2) throw new Error(`Param 'colours' should be at least two in length. Got: ${ colours.length }`);
+  const c = colours.map(colour => resolve(colour));
+  const pieces = [ ...piecewise(c) ];
+  return pieces;
+}
 
 /**
- * Produces a scale of colours as a string array
- *
- * @example
+ * Produces a stepped scale of colours.
+ * 
+ * Return result is an array of Color.js 'Colour' objects.
  * ```js
- * // Yields array of 5 colour strings
- * const s = scale(5, {space:`hcl`}, `blue`, `red`);
- * // Produces scale between three colours
- * const s = scale(5, {space:`hcl`}, `blue`, `yellow`, `red`);
+ * import { Colour } from 'ixfx/visual'
+ * const steps = Colour.scale(['red','green'], 10);
+ * for (const step of steps) {
+ *  // Get a 'hsla(...)' string representation of colour
+ *  // This can be used with the canvas, setting DOM properties etc.
+ *  const css = Colour.toString(step);
+ * }
  * ```
- * @param steps Number of colours
- * @param opts Options for interpolation, or string colour space eg `hsl`
- * @param colours From/end colours (or more)
- * @returns
+ * 
+ * {@link cssLinearGradient} can produce a smooth gradient in CSS on the basis
+ * of the stepped colours.
+ * @param colours 
+ * @param numberOfSteps 
+ * @param opts 
+ * @returns 
  */
-export const scale = (
-  steps: number,
-  opts: InterpolationOpts | string,
-  ...colours: Array<Colourish>
-): Array<string> => {
-  throwNumberTest(steps, `aboveZero`, `steps`);
-  if (!Array.isArray(colours))
-    throw new Error(`Expected one or more colours as parameters`);
-  const inter = getInterpolator(opts, colours);
-  if (inter === undefined) throw new Error(`Could not handle colour/space`);
+export const scale = (colours: Array<Colourish>, numberOfSteps: number, opts: Partial<InterpolationOpts> = {}) => {
+  const space = opts.space ?? `lch`;
+  const hue = opts.hue ?? `shorter`;
+  const pieces = interpolatorInit(colours);
+  const stepsPerPair = Math.floor(numberOfSteps / pieces.length);
 
-  const perStep = 1 / (steps - 1);
-  const r = [];
-  //eslint-disable-next-line functional/no-let
-  let amt = 0;
-  //eslint-disable-next-line functional/no-let
-  for (let index = 0; index < steps; index++) {
-    //eslint-disable-next-line functional/immutable-data
-    r.push(inter(amt));
-    amt += perStep;
-    if (amt > 1) amt = 1;
-  }
-  return r;
-};
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+  const steps = pieces.map(piece => (piece[ 0 ] as any).steps(piece[ 1 ],
+    { space, hue, steps: stepsPerPair, outputSpace: `srgb` }
+  )) as Array<Color>;
 
-const isHsl = (p: Colour | d3Colour.ColorCommonInstance | Hsl): p is Hsl => {
+  return steps.flat();
+}
+
+/**
+ * Returns a CSS `linear-gradient` with stops corresponding to the given list of `colours`.
+ * ```js
+ * element.style.background = Colour.cssLinearGradient(['red','green','blue']);
+ * ```
+ * 
+ *
+ * @param colours 
+ * @returns 
+ */
+export const cssLinearGradient = (colours: Array<Colourish>) => {
+  const c = colours.map(c => resolve(c));
+  return `linear-gradient(to right, ${ c.map(v => v.display()).join(`, `) })`;
+}
+
+const isHsl = (p: Colourish): p is Hsl => {
+  if (p === undefined || p === null) return false;
+  if (typeof p !== `object`) return false;
   if ((p as Hsl).h === undefined) return false;
   if ((p as Hsl).s === undefined) return false;
   if ((p as Hsl).l === undefined) return false;
   return true;
 };
 
-const isRgb = (p: Colour | d3Colour.ColorCommonInstance | Rgb): p is Rgb => {
+const isRgb = (p: Colourish): p is Rgb => {
+  if (p === undefined || p === null) return false;
+  if (typeof p !== `object`) return false;
   if ((p as Rgb).r === undefined) return false;
   if ((p as Rgb).g === undefined) return false;
   if ((p as Rgb).b === undefined) return false;
   return true;
 };
-
-// const rgbToHsl = (r: number, g: number, b: number): Hsl => {
-//   r /= 255;
-//   g /= 255;
-//   b /= 255;
-
-//   var min = Math.min(r, g, b),
-//     max = Math.max(r, g, b),
-//     delta = max - min,
-//     h,
-//     s,
-//     l;
-
-//   h = 0;
-//   if (max === min) {
-//     h = 0;
-//   } else if (r === max) {
-//     h = (g - b) / delta;
-//   } else if (g === max) {
-//     h = 2 + (b - r) / delta;
-//   } else if (b === max) {
-//     h = 4 + (r - g) / delta;
-//   }
-
-//   h = Math.min(h * 60, 360);
-
-//   if (h < 0) {
-//     h += 360;
-//   }
-
-//   l = (min + max) / 2;
-
-//   if (max === min) {
-//     s = 0;
-//   } else if (l <= 0.5) {
-//     s = delta / (max + min);
-//   } else {
-//     s = delta / (2 - max - min);
-//   }
-
-//   return {h, s, l};
-// };
