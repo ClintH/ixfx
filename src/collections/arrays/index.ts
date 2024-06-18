4b6cb370774544b2d5f0ec4e395dab6fd910f345
CH <clint@thestaticvoid.net>
2024-06-18 17:20:37 +0200

    Update index.ts

--- src/collections/arrays/index.ts
+++ src/collections/arrays/index.ts
@@ -24,11 +24,13 @@ export * from '../ArrayCycle.js';
 export * from '../FilterBetween.js';
 export * from '../GuardArray.js';
 export * from '../GuardIndex.js';
+export * from './ContainsDuplicateInstances.js';
 export * from './AverageWeighted.js';
 export * from './NumericArrays.js';
 export * from './Zip.js';
 export * from './ValuesEqual.js';
 export * from './SortByNumericProperty.js';
+export * from './Without.js';
 export { compareValues, compareValuesEqual } from '../Iterables.js';
 
 /**
@@ -350,71 +352,6 @@ export const shuffle = <V>(
   return array;
 };
 
-
-
-/**
- * Returns an array with value(s) omitted. If value is not found, result will be a copy of input.
- * Value checking is completed via the provided `comparer` function.
- * By default checking whether `a === b`. To compare based on value, use the `isEqualValueDefault` comparer.
- *
- * @example
- * ```js
- * import { without } from 'https://unpkg.com/ixfx/dist/arrays.js';
- *
- * const data = [100, 20, 40];
- * const filtered = without(data, 20); // [100, 40]
- * ```
- *
- * @example Using value-based comparison
- * ```js
- * import { without } from 'https://unpkg.com/ixfx/dist/arrays.js';
- *
- * const data = [{name: `Alice`}, {name:`Sam`}];
- *
- * // This wouldn't work as expected, because the default comparer uses instance,
- * // not value:
- * without(data, {name: `Alice`});
- *
- * // So instead we can use a value comparer:
- * without(data, {name:`Alice`}, isEqualValueDefault);
- * ```
- *
- * @example Use a function
- * ```js
- * import { without } from 'https://unpkg.com/ixfx/dist/arrays.js';
- *
- * const data = [{name: `Alice`}, {name:`Sam`}];
- * without(data, {name:`ALICE`}, (a, b) => {
- *  return (a.name.toLowerCase() === b.name.toLowerCase());
- * });
- * ```
- *
- * Consider {@link remove} to remove an item by index.
- *
- * @template V Type of array items
- * @param sourceArray Source array
- * @param toRemove Value(s) to remove
- * @param comparer Comparison function. If not provided `Util.isEqualDefault` is used, which compares using `===`
- * @return Copy of array without value.
- */
-export const without = <V>(
-  //eslint-disable-next-line functional/prefer-readonly-type
-  sourceArray: ReadonlyArray<V> | Array<V>,
-  toRemove: V | Array<V>,
-  comparer: IsEqual<V> = isEqualDefault
-): Array<V> => {
-  if (Array.isArray(toRemove)) {
-    const returnArray = []
-    for (const source of sourceArray) {
-      if (!toRemove.some(v => comparer(source, v))) {
-        returnArray.push(source);
-      }
-    }
-    return returnArray;
-  } else {
-    return sourceArray.filter((v) => !comparer(v, toRemove));
-  }
-}
 
 export const withoutUndefined = <V>(data: ReadonlyArray<V> | Array<V>): Array<V> => {
   return data.filter(v => v !== undefined);
@@ -864,22 +801,7 @@ export const containsDuplicateValues = <V>(
   return false;
 };
 
-/**
- * Returns _true_ if array contains duplicate instances.
- * Use {@link containsDuplicateValues} if you'd rather compare by value.
- * @param array 
- * @returns 
- */
-export const containsDuplicateInstances = <V>(array: Array<V> | ReadonlyArray<V>): boolean => {
-  if (!Array.isArray(array)) throw new Error(`Parameter needs to be an array`);
-  for (let index = 0; index < array.length; index++) {
-    for (let x = 0; x < array.length; x++) {
-      if (index === x) continue;
-      if (array[ index ] === array[ x ]) return true;
-    }
-  }
-  return false;
-}
+
 
 /**
  * Returns _true_ if the two arrays have the same items at same indexes.
