d407dc1eb5b5b60fa75a8adb4b4feaf8eb3660d6
CH <clint@thestaticvoid.net>
2024-06-18 17:17:36 +0200

    Create DefaultKeyer.ts

---
+++ src/DefaultKeyer.ts
@@ -0,0 +1,9 @@
+/**
+ * If values are strings, uses that as the key.
+ * Otherwise uses `JSON.stringify`.
+ * @param a
+ * @returns
+ */
+export const defaultKeyer = <V>(a: V) => {
+  return typeof a === `string` ? a : JSON.stringify(a);
+};
