f2b90c21d302c807f4070e4c6134088fc0999310
CH <clint@thestaticvoid.net>
2024-06-18 17:13:03 +0200

    Added ops

--- src/rx/index.ts
+++ src/rx/index.ts
@@ -1,18 +1,22 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 //#region imports
-import type { Reactive, ReactiveOrSource, ReactiveWritable, ReactiveOp, InitStreamOptions, WithValueOptions, CombineLatestOptions, RxValueTypes, RxValueTypeObject, PipeSet, ReactiveInitial, ReactiveDisposable } from "./Types.js";
-
-import { isDisposable, messageHasValue, messageIsDoneSignal, opify } from "./Util.js";
+import type { Reactive, ReactiveOrSource, ReactiveWritable, ReactiveOp, InitStreamOptions, WithValueOptions, CombineLatestOptions, RxValueTypes, RxValueTypeObject, PipeSet } from "./Types.js";
+import { messageHasValue, messageIsDoneSignal, opify } from "./Util.js";
 import * as OpFns from './ops/index.js';
 import { initStream } from "./InitStream.js";
 import { type Interval, intervalToMs } from '../flow/IntervalType.js';
 import { resolveSource } from './ResolveSource.js';
 import type { BatchOptions, DebounceOptions, FieldOptions, SingleFromArrayOptions, SplitOptions, FilterPredicate, SwitcherOptions, SyncOptions, ThrottleOptions } from "./ops/Types.js";
 import type { TimeoutTriggerOptions } from "./sources/Types.js";
+import * as SinkFns from './sinks/index.js';
+import type { RankFunction, RankOptions } from "src/data/BasicProcessors.js";
+export * from './Chain.js';
+
 //#endregion
 
 //#region exports
 export * from './ops/index.js';
+export * from './sinks/index.js';
 export * from './Graph.js';
 export * from './Types.js';
 export * from './ToArray.js';
@@ -32,33 +36,59 @@ export * as From from './sources/index.js';
 export function manual<V>(options: Partial<InitStreamOptions> = {}): Reactive<V> & ReactiveWritable<V> {
   const events = initStream<V>(options);
   return {
+    dispose: events.dispose,
+    isDisposed: events.isDisposed,
     set(value: V) {
       events.set(value);
     },
     on: events.on,
-    value: events.value
+    onValue: events.onValue
   };
+}
+
+export const Sinks = {
+  setHtmlText: (options: SinkFns.SetHtmlOptions) => {
+    return (source: ReactiveOrSource<string>) => {
+      SinkFns.setHtmlText(source, options);
+    }
+  }
 }
 
 export const Ops = {
   /**
- * Annotates values.
+ * Annotates values with the result of a function.
+ * The input value needs to be an object.
  * 
- * For every value `input` emits, run it through `transformer`, which should
+ * For every value `input` emits, run it through `annotator`, which should
  * return the original value with additional fields.
  * 
  * Conceptually the same as `transform`, just with typing to enforce result
  * values are V & TAnnotation
- * @param transformer 
+ * @param annotator 
  * @returns 
  */
-  annotate: <V, TAnnotation>(transformer: (input: V) => V & TAnnotation) => opify(OpFns.annotate, transformer),
+  annotate: <V, TAnnotation>(annotator: (input: V) => V & TAnnotation) => opify(OpFns.annotate, annotator),
   /**
-   * Annotates all values with the elapsed time since the last value
+   * Annotates the input stream using {@link ReactiveOp} as the source of annotations.
+   * The output values will have the shape of `{ value: TIn, annotation: TAnnotation }`.
+   * Meaning that the original value is stored under `.value`, and the annotation under `.annotation`.
+   * 
+   * ```js
+   * // Emit values from an array
+   * const r1 = Rx.run(
+   *  Rx.From.array([ 1, 2, 3 ]),
+   *  Rx.Ops.annotateWithOp(
+   *    // Add the 'max' operator to emit the largest-seen value
+   *    Rx.Ops.sum()
+   *  )
+   * );
+   * const data = await Rx.toArray(r1);
+   * // Data =  [ { value: 1, annotation: 1 }, { value: 2, annotation: 3 }, { value: 3, annotation: 6 } ]
+   * ```
+   * @param annotatorOp 
    * @returns 
    */
-  annotateElapsed: <V>() => opify<V>(OpFns.annotateElapsed),
-
+  annotateWithOp: <TIn, TAnnotation>(annotatorOp: ReactiveOp<TIn, TAnnotation>) => opify(OpFns.annotateWithOp, annotatorOp),
   /**
    * Takes a stream of values and batches them up (by quantity or time elapsed),
    * emitting them as an array.
@@ -112,6 +142,7 @@ export const Ops = {
       return OpFns.debounce(source, options);
     }
   },
+  elapsed: <V>(): ReactiveOp<V, number> => opify(OpFns.elapsed),
   /**
    * Yields the value of a field from an input stream of values.
    * Eg if the source reactive emits `{ colour: string, size: number }`,
@@ -121,8 +152,8 @@ export const Ops = {
    * @param options 
    * @returns 
    */
-  field: <V extends object>(fieldName: keyof V, options: FieldOptions<V>) => {
-    return (source: ReactiveOrSource<V>) => {
+  field: <TSource extends object, TFieldType>(fieldName: keyof TSource, options: FieldOptions<TSource, TFieldType>) => {
+    return (source: ReactiveOrSource<TSource>) => {
       return OpFns.field(source, fieldName, options);
     }
   },
@@ -132,6 +163,22 @@ export const Ops = {
    * @returns 
    */
   filter: <V>(predicate: (value: V) => boolean) => opify(OpFns.filter, predicate),
+  /**
+ * Outputs the minimum numerical value of the stream.
+ * A value is only emitted when minimum decreases.
+ * @returns 
+ */
+  min: <TIn = number>(options?: OpFns.OpMathOptions) => opify<TIn, number>(OpFns.min, options),
+  /**
+   * Outputs the maxium numerical value of the stream.
+   * A value is only emitted when maximum increases.
+   * @returns 
+   */
+  max: <TIn = number>(options?: OpFns.OpMathOptions) => opify<TIn, number>(OpFns.max, options),
+  sum: <TIn = number>(options?: OpFns.OpMathOptions) => opify<TIn, number>(OpFns.sum, options),
+  average: <TIn = number>(options?: OpFns.OpMathOptions) => opify<TIn, number>(OpFns.average, options),
+  tally: <TIn>(options?: OpFns.TallyOptions) => opify<TIn, number>(OpFns.tally, options),
+  rank: <TIn>(rank: RankFunction<TIn>, options?: RankOptions & OpFns.OpMathOptions) => opify<TIn>(OpFns.rank, rank, options),
 
   pipe: <TInput, TOutput>(...streams: Array<Reactive<any> & ReactiveWritable<any>>) => {
     return (source: ReactiveOrSource<TInput>) => {
@@ -172,6 +219,22 @@ export const Ops = {
       return OpFns.syncToObject(reactiveSources, options);
     }
   },
+  tapProcess: <In>(processor: ((value: In) => any)): ReactiveOp<In, In> => {
+    return (source: ReactiveOrSource<In>) => {
+      return OpFns.tapProcess(source, processor);
+    }
+  },
+  tapStream: <In>(divergedStream: ReactiveWritable<In>): ReactiveOp<In, In> => {
+    return (source: ReactiveOrSource<In>) => {
+      return OpFns.tapStream(source, divergedStream);
+    }
+  },
+  tapOps: <In, Out>(...ops: Array<ReactiveOp<In, Out>>) => {
+    return (source: ReactiveOrSource<In>) => {
+      return OpFns.tapOps(source, ...ops);
+    }
+  },
+
   /**
  * Throttle values from the stream.
  * Only emits a value if some minimum time has elapsed.
@@ -190,9 +253,9 @@ export const Ops = {
       return OpFns.timeoutTrigger<V, TTriggerValue>(source, options);
     }
   },
-  transform: <In, Out>(transformer: ((value: In) => Out)): ReactiveOp<In, Out> => {
+  transform: <In, Out>(transformer: ((value: In) => Out), options: Partial<OpFns.TransformOpts> = {}): ReactiveOp<In, Out> => {
     return (source: ReactiveOrSource<In>) => {
-      return OpFns.transform(source, transformer);
+      return OpFns.transform(source, transformer, options);
     }
   },
 
@@ -212,35 +275,37 @@ export const Ops = {
  * @param ops 
  * @returns 
  */
-const prepareOps = <TIn, TOut>(...ops: Array<ReactiveOp<TIn, TOut>>) => {
-  return (source: ReactiveOrSource<TIn>) => {
-    for (const op of ops) {
-      // @ts-expect-error
-      source = op(source);
-    }
-    return source as any as Reactive<TOut>;
-  }
-}
+// export const chain = <TIn, TOut>(...ops: Array<ReactiveOp<TIn, TOut>>) => {
+//   return (source: ReactiveOrSource<TIn>) => {
+//     for (const op of ops) {
+//       // @ts-expect-error
+//       source = op(source);
+//     }
+//     return source as any as Reactive<TOut>;
+//   }
+// }
+
+// export const chainStream = <TIn, TOut>(...ops: Array<ReactiveOp<TIn, TOut>>): ReactiveStream<TIn, TOut> => {
+//   const stream = manual<TIn>();
+//   const c = chain(...ops);
+//   const x = c(stream);
+//   return x;
+// }
 
 
-/**
- * Connects `source` to serially-connected set of ops. Values thus
- * flow from `source` to each op in turn.
- * 
- * Returned result is the final reactive.
- * 
- * @param source 
- * @param ops 
- * @returns 
- */
-export function run<TIn, TOut>(source: ReactiveOrSource<TIn>, ...ops: Array<ReactiveOp<any, any>>) {
-  const raw = prepareOps<TIn, TOut>(...ops);
-  return raw(source);
-}
+
+// function chainx<TIn, TOut>(...ops: Array<ReactiveOp<any, any>>) {
+//   return (source: ReactiveOrSource<TIn>) => {
+//     for (const op of ops) {
+//       source = op(source);
+//     }
+//     return source as any as Reactive<TOut>;
+//   }
+// }
 
 export function cache<T>(r: Reactive<T>, initialValue: T) {
-  let lastValue: T | undefined = initialValue;
-  r.value(value => {
+  let lastValue: T = initialValue;
+  r.onValue(value => {
     lastValue = value;
   });
   return {
@@ -249,10 +314,10 @@ export function cache<T>(r: Reactive<T>, initialValue: T) {
       return lastValue
     },
     reset() {
+      // @ts-expect-error
       lastValue = undefined;
     }
   }
-
 }
 
 // export function runWithInitial<TIn, TOut>(initial: TOut, source: ReactiveOrSource<TIn>, ...ops: Array<ReactiveOp<any, any>>): ReactiveInitial<TOut> & ReactiveDisposable<TOut> {
@@ -261,7 +326,7 @@ export function cache<T>(r: Reactive<T>, initialValue: T) {
 //   const r = raw(source);
 //   let disposed = false;
 
-//   r.value(value => {
+//   r.onValue(value => {
 //     lastValue = value;
 //   });
 
@@ -342,11 +407,7 @@ export const to = <TA, TB>(a: Reactive<TA>, b: ReactiveWritable<TB>, transform: 
     } else if (messageIsDoneSignal(message)) {
       unsub();
       if (closeBonA) {
-        if (isDisposable<any>(b)) {
-          b.dispose(`Source closed (${ message.context ?? `` })`);
-        } else {
-          console.warn(`Reactive.to cannot close 'b' reactive since it is not disposable`);
-        }
+        b.dispose(`Source closed (${ message.context ?? `` })`);
       }
     } else {
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
