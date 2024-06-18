18764197e5d714a0d19fdf55a083b3a45f737c91
CH <clint@thestaticvoid.net>
2024-06-18 17:10:35 +0200

    Update Transform.ts

--- src/rx/ops/Transform.ts
+++ src/rx/ops/Transform.ts
@@ -1,5 +1,5 @@
 import { initUpstream } from "../InitStream.js";
-import type { ReactiveOrSource, Reactive } from "../Types.js";
+import type { Reactive, ReactiveOrSource } from "../Types.js";
 import { toReadable } from "../ToReadable.js";
 import type { TransformOpts } from "./Types.js";
 
@@ -9,14 +9,25 @@ import type { TransformOpts } from "./Types.js";
  * @returns 
  */
 export function transform<In, Out>(input: ReactiveOrSource<In>, transformer: (value: In) => Out, options: Partial<TransformOpts> = {}): Reactive<Out> {
+  const traceInput = options.traceInput ?? false;
+  const traceOutput = options.traceOutput ?? false;
+
   const upstream = initUpstream<In, Out>(input, {
     lazy: `initial`,
     ...options,
     onValue(value) {
       const t = transformer(value);
+      if (traceInput && traceOutput) {
+        console.log(`Rx.Ops.transform input: ${ JSON.stringify(value) } output: ${ JSON.stringify(t) }`);
+      } else if (traceInput) {
+        console.log(`Rx.Ops.transform input: ${ JSON.stringify(value) }`);
+      } else if (traceOutput) {
+        console.log(`Rx.Ops.transform output: ${ JSON.stringify(t) }`);
+      }
+
       upstream.set(t);
     },
   })
-
   return toReadable(upstream);
 }
+
