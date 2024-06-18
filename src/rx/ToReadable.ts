f9b7150ae48b4c6c81dfe6a17639442562e9d211
CH <clint@thestaticvoid.net>
2024-06-18 17:09:11 +0200

    Update ToReadable.ts

--- src/rx/ToReadable.ts
+++ src/rx/ToReadable.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/unbound-method */
-import type { ReactiveDisposable, ReactiveStream } from "./Types.js";
+import type { Reactive, ReactiveStream } from "./Types.js";
 
-export const toReadable = <V>(upstream: ReactiveStream<V>): ReactiveDisposable<V> => ({
-  on: upstream.on,
-  dispose: upstream.dispose,
-  isDisposed: upstream.isDisposed,
-  value: upstream.value
+/***
+ * Returns a read-only version of `stream`
+ */
+export const toReadable = <V>(stream: ReactiveStream<V>): Reactive<V> => ({
+  on: stream.on,
+  dispose: stream.dispose,
+  isDisposed: stream.isDisposed,
+  onValue: stream.onValue
 });
