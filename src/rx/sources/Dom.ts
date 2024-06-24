4c73ba77483533d156c6a2fd3ac6a781c0898751
CH <clint@thestaticvoid.net>
2024-06-24 13:39:05 +0200

    Allow for an upstream source and transform function for its values

--- src/rx/sources/Dom.ts
+++ src/rx/sources/Dom.ts
@@ -1,139 +1,104 @@
 import * as Colour from '../../visual/Colour.js';
-import { scale } from "../../data/Scale.js";
-import type { ReactiveInitial, ReactiveWritable, ReactiveStream, Reactive } from "../Types.js";
-import { eventField, event, eventTrigger } from "./Event.js";
-import type { DomValueOptions } from "./Types.js";
+import type { ReactiveInitial, ReactiveWritable, Reactive } from "../Types.js";
+import { eventTrigger } from "./Event.js";
+import type { DomNumberInputValueOptions, DomValueOptions } from "./Types.js";
 import { resolveEl } from '../../dom/ResolveEl.js';
 import { transform } from '../ops/Transform.js';
+import { hasLast } from '../Util.js';
+import { Visual } from 'src/index.js';
 
 /**
- * Fires when the value of a HTMLInputElement changes.
- * This function expects numeric values.
+ * Reactive getting/setting of values to a HTML INPUT element.
  * 
- * ```js
- * const r = domValueAsNumber(el);
- * ```
+ * Sets the 'type' attribute of element to 'range'.
  * 
  * Options:
- * * makeRelative: if _true_, uses min/max attributes to make value relative (default: false)
- * * when: if 'change' (default), fires when value has been changed. Use 'progress' to emit in-progress values.
+ * - relative: if _true_, sets min/max values of attribute. Default: _false_
+ * - inverted:  if _true_ flips min/max. Default: _false_
  * @param targetOrQuery 
  * @param options 
  * @returns 
  */
-// export function domValueAsNumber(targetOrQuery: HTMLInputElement | string, options: Partial<DomValueAsNumberOptions> = {}): ReactiveInitial<number> & ReactiveDisposable<number> & ReactiveWritable<number> {
-//   const target: HTMLInputElement | null = (typeof targetOrQuery === `string` ? document.querySelector(targetOrQuery) : targetOrQuery);
-//   if (target === null && typeof targetOrQuery === `string`) throw new Error(`Element query could not be resolved '${ targetOrQuery }`);
-//   if (target === null) throw new Error(`targetOrQuery is null`)
+export function domNumberInputValue(targetOrQuery: HTMLInputElement | string, options: Partial<DomNumberInputValueOptions> = {}): ReactiveInitial<number> & ReactiveWritable<number> {
+  const input = domInputValue(targetOrQuery, options);
+  const el = input.el;
+  const relative = options.relative ?? false;
+  const inverted = options.inverted ?? false;
 
-//   const when = options.when ?? `changed`;
-//   const eventName = when === `changed` ? `change` : `input`;
-//   const makeRelative = options.makeRelative ?? false;
-//   const invert = options.invert ?? false;
-//   const initialValue = options.initialValue;
-//   const postTransform = options.transform;
+  const rx = transform(input, v => {
+    return Number.parseFloat(v);
+  });
 
-//   const setToDom = (value: number) => {
-//     target.valueAsNumber = value;
-//     target.setAttribute(`value`, value.toString())
-//   }
+  if (relative) {
+    //el.setAttribute(`max`, inverted ? "0" : "1");
+    el.max = inverted ? "0" : "1";
+    //el.setAttribute(`min`, inverted ? "1" : "0");
+    el.min = inverted ? "1" : "0";
+    if (!el.hasAttribute(`step`)) {
+      //el.setAttribute(`step`, "0.1");
+      el.step = "0.1";
+    }
+  }
+  //el.setAttribute(`type`, `range`);
+  el.type = `range`;
 
-//   if (initialValue !== undefined) {
-//     setToDom(initialValue);
-//   }
+  const set = (value: number) => {
+    input.set(value.toString());
+  }
 
-//   const p = (value: string | null) => {
-//     // eslint-disable-next-line unicorn/no-null
-//     if (value === null) return null;
-//     return Number.parseFloat(value);
-//   }
+  return {
+    ...rx,
+    last() {
+      return Number.parseFloat(input.last())
+    },
+    set
+  };
+}
 
-//   const max = p(target.getAttribute(`max`));
-//   const min = p(target.getAttribute(`min`));
-//   if (makeRelative && max === null) throw new Error(`Cannot make a relative value if 'max' attribute missing from target element.`);
-//   if (makeRelative && min === null) throw new Error(`Cannot make a relative value if 'min' attribute missing from target element.`);
+export function domHslInputValue(targetOrQuery: HTMLInputElement | string, options: Partial<DomValueOptions> = {}): ReactiveInitial<Colour.Hsl> & Reactive<Colour.Hsl> & ReactiveWritable<Colour.Hsl> {
 
-//   const transform = (value?: number) => {
-//     if (value === undefined) return 0;
-//     if (makeRelative) {
-//       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-//       value = scale(value, min!, max!);
-//       if (invert) value = 1 - value;
-//     } else if (invert && max !== null && min !== null) {
-//       value = scale(value, min, max, max, min);
-//     }
-
-//     if (postTransform) value = postTransform(value);
-//     return value;
-//   }
-
-//   options = {
-//     ...options,
-//     transform
-//   }
-//   const stream = eventPluckedField<number>(target, eventName, `valueAsNumber`, options);
-//   return {
-//     ...stream,
-//     set: setToDom,
-//   };
-// }
-
-// export function domValueAsHsl(targetOrQuery: HTMLInputElement | string, options: Partial<DomValueOptions<Colour.Hsl>> = {}): ReactiveInitial<Colour.Hsl> & ReactiveDisposable<Colour.Hsl> {
-//   const target: HTMLInputElement | null = (typeof targetOrQuery === `string` ? document.querySelector(targetOrQuery) : targetOrQuery);
-//   if (target === null && typeof targetOrQuery === `string`) throw new Error(`Element query could not be resolved '${ targetOrQuery }`);
-//   if (target === null) throw new Error(`targetOrQuery is null`)
-
-//   const postTransform = options.transform;
-//   const when = options.when ?? `changed`;
-//   const eventName = when === `changed` ? `change` : `input`;
-//   const value = Colour.resolve(options.initialValue ?? target.value);
-
-//   if (options.initialValue) {
-//     const hex = Colour.toHex(value);
-//     target.value = hex;
-//     target.setAttribute(`value`, hex);
-//   }
-
-//   const transform = (value?: string | Colour.Hsl): Colour.Hsl => {
-//     if (value === undefined) return Colour.toHsl(`white`);
-//     let colour = Colour.toHsl(value);
-//     if (postTransform) colour = postTransform(colour);
-//     return colour;
-//   }
-
-//   options = {
-//     ...options,
-//     transform,
-//     initialValue: transform(value)
-//   }
-//   const stream = eventPluckedField<Colour.Hsl>(target, eventName, `value`, options);
-//   return stream;
-// }
+  const input = domInputValue(targetOrQuery, {
+    ...options,
+    upstreamFilter(value) {
+      return (typeof value === `object`) ? Visual.Colour.toHex(value) : value;
+    },
+  });
+  const rx = transform(input, v => {
+    return Colour.toHsl(v);
+  });
+  return {
+    ...rx,
+    last() {
+      return Colour.toHsl(input.last())
+    },
+    set(value) {
+      input.set(Colour.toHex(value));
+    },
+  };
+}
 
 /**
  * A stream of values when the a HTMLInputElement changes. Eg a <input type="range">
  * ```js
- * const r = Rx.From.domValue(`#myEl`);
+ * const r = Rx.From.domInputValue(`#myEl`);
  * r.onValue(value => {
  *  // value will be string
  * });
  * ```
  * 
  * Options:
- * * when: Events can fire when the value is settled, or live
- * * initialValue: Sets a value on the output stream when starting. It also sets the HTML element
- * * domToValue: function to convert from DOM field (usually a string) to the destination type
- * * valueToDom: function to convert from stream value type to something assignable to DOM (usually a string)
- * 
- * The conversion functions for example are useful for <input type="color">. It needs a hex-encoded colour, but that's
- * not necessarily 
+ * * emitInitialValue: If _true_ emits the HTML value of element (default: false)
+ * * attributeName: If set, this is the HTML attribute value is set to when writing to stream (default: 'value')
+ * * fieldName: If set, this is the DOM object field set when writing to stream (default: 'value')
+ * * when: 'changed'|'changing' when values are emitted. (default: 'changed')
+ * * fallbackValue:  Fallback value to use if field/attribute cannot be read (default: '')
  * @param targetOrQuery 
  * @param options 
  * @returns 
  */
-export function domInputValue(targetOrQuery: HTMLInputElement | string, options: Partial<DomValueOptions> = {}): ReactiveInitial<string> & Reactive<string> & ReactiveWritable<string> {
+export function domInputValue(targetOrQuery: HTMLInputElement | string, options: Partial<DomValueOptions> = {}): { el: HTMLInputElement } & ReactiveInitial<string> & ReactiveWritable<string> {
   const target: HTMLInputElement | null = (typeof targetOrQuery === `string` ? document.querySelector(targetOrQuery) : targetOrQuery);
-  if (target === null && typeof targetOrQuery === `string`) throw new Error(`Element query could not be resolved '${ targetOrQuery }`);
+  if (target === null && typeof targetOrQuery === `string`) throw new Error(`Element query could not be resolved '${ targetOrQuery }'`);
   if (target === null) throw new Error(`targetOrQuery is null`)
 
   const el = resolveEl(targetOrQuery);
@@ -141,6 +106,8 @@ export function domInputValue(targetOrQuery: HTMLInputElement | string, options:
   const eventName = when === `changed` ? `change` : `input`;
   const emitInitialValue = options.emitInitialValue ?? false;
   const fallbackValue = options.fallbackValue ?? ``;
+  const upstreamSource = options.upstreamSource;
+  let upstreamSourceUnsub = () => {}
 
   let attribName = options.attributeName;
   let fieldName = options.fieldName;
@@ -154,10 +121,11 @@ export function domInputValue(targetOrQuery: HTMLInputElement | string, options:
     if (attribName) {
       value = el.getAttribute(attribName);
     }
-    if (value === null && fieldName) {
+    if (fieldName) {
       value = (el as any)[ fieldName ]
     }
     if (value === undefined || value === null) value = fallbackValue;
+    //console.log(`domInputValue readValue: ${ value }`);
     return value;
   }
 
@@ -170,42 +138,40 @@ export function domInputValue(targetOrQuery: HTMLInputElement | string, options:
     }
   }
 
+  const setUpstream = (v: any) => {
+    v = options.upstreamFilter ? options.upstreamFilter(v) : v;
+    setValue(v);
+  }
+  if (upstreamSource) {
+    upstreamSourceUnsub = upstreamSource.onValue(setUpstream);
+    if (hasLast(upstreamSource)) {
+      setUpstream(upstreamSource.last());
+    }
+  }
+
   // Input element change event stream
   const rxEvents = eventTrigger(el, eventName, {
     fireInitial: emitInitialValue,
     debugFiring: options.debugFiring ?? false,
-    debugLifecycle: options.debugLifecycle ?? false
+    debugLifecycle: options.debugLifecycle ?? false,
   });
 
   // Transform to get values
-  const rxValues = transform(rxEvents, _trigger => {
-    // Event has fired
-    return readValue();
-  });
+  const rxValues = transform(rxEvents, _trigger => readValue());
 
   return {
     ...rxValues,
+    el,
     last() {
       return readValue()
     },
     set(value) {
       setValue(value);
     },
+    dispose(reason) {
+      upstreamSourceUnsub();
+      rxValues.dispose(reason);
+      rxEvents.dispose(reason);
+    },
   }
-  // if (options.initialValue) {
-  //   const domValue = options.valueToDom ? options.valueToDom(options.initialValue) : options.initialValue.toString()
-  //   target.value = domValue;
-  //   target.setAttribute(`value`, domValue);
-  // }
-
-  // const transform = (value?: string): string => {
-  //   if (postTransform) value = postTransform(value);
-  //   return value ?? ``;
-  // }
-
-  // options = {
-  //   ...options
-  // }
-  // const stream = eventPluckedField(target, eventName, `value`, options);
-  // return stream;
 }
