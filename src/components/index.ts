964b99322f5e776b86cffbac25688f8037b52770
CH <clint@thestaticvoid.net>
2024-07-26 21:18:44 +0200

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,16 @@
-export { HistogramVis } from './HistogramVis.js';
-export { FrequencyHistogramPlot } from './FrequencyHistogramPlot.js';
-export { PlotElement, PlotSeries } from './PlotElement.js';
+export * from './FrequencyHistogramPlot.js';
+export * from './HistogramVis.js';
+import { PlotElement, PlotSeries } from './PlotElement.js';
+export * from './PlotElement.js';
+
+export function init() {
+  try {
+    customElements.define(`plot-element`, PlotElement);
+  } catch {}
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "plot-element": PlotElement;
+  }
+}
