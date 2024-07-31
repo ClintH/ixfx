414a713d3c03dd910629456b67a812d2cb75e04b
CH <clint@thestaticvoid.net>
2024-08-01 00:56:43 +0200

    Update Easing.Test.ts

--- src/__test/modulation/Easing.Test.ts
+++ src/__test/modulation/Easing.Test.ts
@@ -5,7 +5,7 @@ import { repeatSync } from '../../flow/Repeat.js';
 test(`tick`, async t => {
   const e1 = Easings.tickEasing(`sineIn`, 10)
   t.false(e1.isDone);
-  const e1D = Array.fromAsync(repeatSync(e1.compute, { count: 11 }));
+  const e1D = Array.from(repeatSync(e1.compute, { count: 11 }));
   console.log(e1D);
   t.true(e1.isDone);
 
