b9d04d80a8672d436d34d5f35f539e0b7d78f0cc
CH <clint@thestaticvoid.net>
2024-08-19 15:57:13 +0200

    Rect.Multiply use common Apply

--- src/geometry/rect/Multiply.ts
+++ src/geometry/rect/Multiply.ts
@@ -1,136 +1,107 @@
-import { guard } from "./Guard.js";
 import type { RectPositioned, Rect } from "./RectTypes.js";
-import { isRect, isRectPositioned, isPositioned } from "./Guard.js";
+import { apply, applyDim, applyScalar } from "./Apply.js";
+
+const multiplyOp = (a: number, b: number) => a * b;
+
 
 /**
- * Multiplies `a` by rectangle or width/height. Useful for denormalising a value.
+ * Multiplies positioned `rect` by width/height. Useful for denormalising a value.
  *
  * ```js
  * import { Rects } from "https://unpkg.com/ixfx/dist/geometry.js";
  *
- * // Normalised rectangle of width 50%, height 50%
- * const r = {width: 0.5, height: 0.5};
+ * // Normalised rectangle
+ * const r = { x:0.5, y:0.5, width: 0.5, height: 0.5};
  *
  * // Map to window:
  * const rr = Rects.multiply(r, window.innerWidth, window.innerHeight);
  * ```
  *
- * ```js
- * // Returns {width: someRect.width * someOtherRect.width ...}
- * Rects.multiply(someRect, someOtherRect);
- *
- * // Returns {width: someRect.width * 100, height: someRect.height * 200}
- * Rects.multiply(someRect, 100, 200);
- * ```
- *
- * Multiplication applies to the first parameter's x/y fields, if present.
+ * Multiplication applies to the first parameter's x/y fields.
  */
 export function multiply(
-  a: RectPositioned,
-  b: Rect | number,
-  c?: number
+  rect: RectPositioned,
+  width: number,
+  height?: number
 ): RectPositioned;
 
 /**
- * Multiplies `a` by rectangle or width/height. Useful for denormalising a value.
+ * Multiplies `rect` by width/height. Useful for denormalising a value.
  *
  * ```js
  * import { Rects } from "https://unpkg.com/ixfx/dist/geometry.js";
  *
  * // Normalised rectangle of width 50%, height 50%
- * const r = {width: 0.5, height: 0.5};
+ * const r = { width: 0.5, height: 0.5 };
  *
  * // Map to window:
  * const rr = Rects.multiply(r, window.innerWidth, window.innerHeight);
  * ```
  *
+ * Multiplication applies to the first parameter's x/y fields, if present.
+ */
+export function multiply(rect: Rect, width: number, height: number): Rect;
+
+/**
+ * Multiplies positioned rect `a` by width and height of rect `b`.
  * ```js
  * // Returns {width: someRect.width * someOtherRect.width ...}
  * Rects.multiply(someRect, someOtherRect);
- *
- * // Returns {width: someRect.width * 100, height: someRect.height * 200}
- * Rects.multiply(someRect, 100, 200);
  * ```
  *
- * Multiplication applies to the first parameter's x/y fields, if present.
+ * @param a 
+ * @param b 
  */
-export function multiply(a: Rect, b: Rect | number, c?: number): Rect;
 export function multiply(a: RectPositioned, b: Rect): RectPositioned;
 
 /**
- * Multiplies `a` by rectangle or width/height. Useful for denormalising a value.
- *
- * ```js
- * import { Rects } from "https://unpkg.com/ixfx/dist/geometry.js";
- *
- * // Normalised rectangle of width 50%, height 50%
- * const r = {width: 0.5, height: 0.5};
- *
- * // Map to window:
- * const rr = Rects.multiply(r, window.innerWidth, window.innerHeight);
- * ```
- *
+ * Multiplies rect `a` by width and height of rect `b`.
+ * 
  * ```js
  * // Returns {width: someRect.width * someOtherRect.width ...}
  * Rects.multiply(someRect, someOtherRect);
- *
- * // Returns {width: someRect.width * 100, height: someRect.height * 200}
- * Rects.multiply(someRect, 100, 200);
  * ```
  *
- * Multiplication applies to the first parameter's x/y fields, if present.
+ * @param a 
+ * @param b 
  */
-//eslint-disable-next-line func-style
+export function multiply(a: Rect, b: Rect): Rect;
+
+/**
+ * @internal
+ * @param a 
+ * @param b 
+ * @param c 
+ * @returns 
+ */
 export function multiply(
   a: RectPositioned | Rect,
   b: Rect | number,
   c?: number
 ): RectPositioned | Rect {
-  guard(a, `a`);
-  if (isRect(b)) {
-    return isRectPositioned(a) ? Object.freeze({
-      ...a,
-      x: a.x * b.width,
-      y: a.y * b.height,
-      width: a.width * b.width,
-      height: a.height * b.height,
-    }) : Object.freeze({
-      ...a,
-      width: a.width * b.width,
-      height: a.height * b.height,
-    });
-  } else {
-    if (typeof b !== `number`) {
-      throw new TypeError(
-        `Expected second parameter of type Rect or number. Got ${ JSON.stringify(
-          b
-        ) }`
-      );
-    }
-    if (c === undefined) c = b;
-
-    return isRectPositioned(a) ? Object.freeze({
-      ...a,
-      x: a.x * b,
-      y: a.y * c,
-      width: a.width * b,
-      height: a.height * c,
-    }) : Object.freeze({
-      ...a,
-      width: a.width * b,
-      height: a.height * c,
-    });
-  }
+  // @ts-ignore
+  return apply(multiplyOp, a, b, c) as RectPositioned | Rect;
 }
 
+
 /**
- * Multiplies all components of `rect` by `amount`
+ * Multiplies all components of `rect` by `amount`.
+ * ```js
+ * multiplyScalar({ width:10, height:20 }, 2); // { width:20, height: 40 }
+ * ```
  * @param rect
  * @param amount
  */
 export function multiplyScalar(rect: Rect, amount: number): Rect;
+
 /**
- * Multiplies all components of `rect` by `amount`
+ * Multiplies all components of `rect` by `amount`.
+ * This includes x,y if present.
+ * 
+ * ```js
+ * multiplyScalar({ width:10, height:20 }, 2); // { width:20, height: 40 }
+ * multiplyScalar({ x: 1, y: 2, width:10, height:20 }, 2); // { x: 2, y: 4, width:20, height: 40 }
+ * ```
  * @param rect
  * @param amount
  */
@@ -138,8 +109,17 @@ export function multiplyScalar(
   rect: RectPositioned,
   amount: number
 ): RectPositioned;
+
 /**
- * Multiplies all components of `rect` by `amount`
+ * Multiplies all components of `rect` by `amount`.
+ * This includes x,y if present.
+ * 
+ * ```js
+ * multiplyScalar({ width:10, height:20 }, 2); // { width:20, height: 40 }
+ * multiplyScalar({ x: 1, y: 2, width:10, height:20 }, 2); // { x: 2, y: 4, width:20, height: 40 }
+ * ```
+ * 
+ * Use {@link multiplyDim} to only multiply width & height.
  * @param rect
  * @param amount
  */
@@ -147,15 +127,47 @@ export function multiplyScalar(
   rect: Rect | RectPositioned,
   amount: number
 ): Rect | RectPositioned {
-  return isPositioned(rect) ? Object.freeze({
-    ...rect,
-    x: rect.x * amount,
-    y: rect.y * amount,
-    width: rect.width * amount,
-    height: rect.height * amount,
-  }) : Object.freeze({
-    ...rect,
-    width: rect.width * amount,
-    height: rect.height * amount,
-  });
+  return applyScalar(multiplyOp, rect, amount);
+  // return isPositioned(rect) ? Object.freeze({
+  //   ...rect,
+  //   x: rect.x * amount,
+  //   y: rect.y * amount,
+  //   width: rect.width * amount,
+  //   height: rect.height * amount,
+  // }) : Object.freeze({
+  //   ...rect,
+  //   width: rect.width * amount,
+  //   height: rect.height * amount,
+  // });
+}
+
+
+/**
+ * Multiplies only the width/height of `rect`, leaving `x` and `y` as they are.
+ * ```js
+ * multiplyDim({ x:1,y:2,width:3,height:4 }, 2);
+ * // Yields: { x:1, y:2, width:6, height: 8 }
+ * ```
+ * 
+ * In comparison, {@link multiply} will also include x & y.
+ * @param rect Rectangle
+ * @param amount Amount to multiply by
+ * @returns 
+ */
+export function multiplyDim(
+  rect: Rect | RectPositioned,
+  amount: number
+): Rect | RectPositioned {
+  return applyDim(multiplyOp, rect, amount);
+  // return isPositioned(rect) ? Object.freeze({
+  //   ...rect,
+  //   x: rect.x * amount,
+  //   y: rect.y * amount,
+  //   width: rect.width * amount,
+  //   height: rect.height * amount,
+  // }) : Object.freeze({
+  //   ...rect,
+  //   width: rect.width * amount,
+  //   height: rect.height * amount,
+  // });
 }
