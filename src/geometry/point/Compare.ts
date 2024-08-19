bbdecf2a4414b4c548e7154a47a099f1df933f5a
CH <clint@thestaticvoid.net>
2024-08-19 15:58:09 +0200

    compareByY

--- src/geometry/point/Compare.ts
+++ src/geometry/point/Compare.ts
@@ -22,18 +22,47 @@ export const compare = (a: Point, b: Point): number => {
 };
 
 /**
- * Compares points based on x value.
- * Returns above 0 if a.x > b.x (to the right)
- * Returns 0 if a.x === b.x
- * Returns below 0 if a.x < b.x (to the left)
+ * Compares points based on x value. Y value is ignored.
+ * 
+ * Return values:
+ * * 0: If a.x === b.x
+ * * 1: a is to the right of b (ie. a.x > b.x)
+ * * -1: a is to the left of b (ie. a.x < b.x)
  *
  * @example Sorting by x
  * ```js
  * arrayOfPoints.sort(Points.compareByX);
  * ```
+ * 
  * @param a
  * @param b
  * @returns
  */
-export const compareByX = (a: Point, b: Point): number =>
-  a.x - b.x || a.y - b.y;
+export const compareByX = (a: Point, b: Point): number => {
+  if (a.x === b.x) return 0;
+  if (a.x < b.x) return -1;
+  return 1;
+
+  // a.x - b.x || a.y - b.y;
+}
+
+/**
+ * Compares points based on Y value. X value is ignored.
+ * Returns values:
+ * * 0: If a.y === b.y
+ * * 1: A is below B (ie. a.y > b.y)
+ * * -1: A is above B (ie. a.y < b.y)
+ *
+ * @example Sorting by Y
+ * ```js
+ * arrayOfPoints.sort(Points.compareByY);
+ * ```
+ * @param a
+ * @param b
+ * @returns
+ */
+export const compareByY = (a: Point, b: Point): number => {
+  if (a.y === b.y) return 0;
+  if (a.y < b.y) return -1;
+  return 1;
+}
