5d890d645a25e2a019be8d0da7927103a8df6465
CH <clint@thestaticvoid.net>
2024-08-23 12:24:59 +0200

    Update Cache.ts

--- src/rx/Cache.ts
+++ src/rx/Cache.ts
@@ -9,11 +9,6 @@ export type CacheStreamInitial<T> = CacheStream<T> & {
   last: () => T
 }
 
-// export function cache<T>(r: ReactiveWritable<T>, initialValue: T): CacheStream & ReactiveWritable<T> & ReactiveInitial<T>;
-// export function cache<T>(r: ReactiveWritable<T>, initialValue?: T | undefined): CacheStream & ReactiveWritable<T> & ReactiveNonInitial<T>;
-
-// export function cache<T>(r: ReactiveWritable<T>, initialValue: T | undefined): CacheStream & ReactiveWritable<T> & ReactiveInitial<T>;
-// export function cache<T>(r: Reactive<T>, initialValue: T | undefined): CacheStream & Reactive<T> & ReactiveInitial<T>;
 export function cache<TValue, RT extends Reactive<TValue>>(r: RT, initialValue: TValue): CacheStreamInitial<TValue> & RT;
 
 export function cache<TValue, RT extends Reactive<TValue>>(r: RT, initialValue: TValue | undefined): CacheStream<TValue> & RT {
