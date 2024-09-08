6b2cc638d1d017ea4f99aa943970130fd74a109b
CH <clint@thestaticvoid.net>
2024-09-08 22:05:13 +0200

    Points.Unit, Points.Unit3d, Points.Empty3d

--- src/geometry/point/Empty.ts
+++ src/geometry/point/Empty.ts
@@ -6,6 +6,10 @@
  */
 export const Empty = { x: 0, y: 0 } as const;
 
+/**
+ * Returns { x:1, y:1 }
+ */
+export const Unit = { x: 1, y: 1 } as const;
 
 /**
  * An empty Point of `{ x: 0, y: 0, z: 0}`
@@ -13,3 +17,8 @@ export const Empty = { x: 0, y: 0 } as const;
  * Use `Empty` to get an empty point without `z`.
  */
 export const Empty3d = { x: 0, y: 0, z: 0 } as const;
+
+/**
+ * Returns { x:1,y:1,z:1 }
+ */
+export const Unit3d = { x: 1, y: 1, z: 1 } as const;
