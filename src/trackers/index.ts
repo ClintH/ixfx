ce2aa3b574e78022afd3a5b8d764e5591c3c0b09
CH <clint@thestaticvoid.net>
2024-09-10 15:30:22 +0200

    Added Trackers.rate for tracking events/second etc

--- src/trackers/index.ts
+++ src/trackers/index.ts
@@ -4,6 +4,7 @@ export * from './NumberTracker.js';
 export * from './ObjectTracker.js';
 export * from './PointTracker.js';
 export * from './PrimitiveTracker.js';
+export * from './RateTracker.js';
 export * from './TrackedValue.js';
 export * from './TrackerBase.js';
 export * from './TrackUnique.js';
