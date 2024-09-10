75a6b76e1ae7cb280fc6477198e6975c407809f1
CH <clint@thestaticvoid.net>
2024-09-10 13:39:30 +0200

    Update Time.ts

--- src/modulation/source/Time.ts
+++ src/modulation/source/Time.ts
@@ -3,9 +3,11 @@ import { throwNumberTest } from "../../util/GuardNumbers.js";
 import type { ModSettableOptions, ModSettable, ModSettableFeedback } from "../Types.js";
 
 /**
- * Returns an elapsed number of milliseconds up to `interval`.
- * If `oneShot` is _false_ (default), it will loop, resetting to 0 when `interval` is reached.
- * If `oneShot` is _true_, once `interval` is reached, this value will be returned.
+ * Returns the percentage of time toward `interval`. See also: {@link bpm}, {@link hertz} which are the same but
+ * using different units for time.
+ * 
+ * By default, it continues forever, cycling from 0..1 repeatedly for each interval. Use
+ * `cycleLimit` to restrict this. A value of 1 means it won't loop. 
  * 
  * The starting 'position' is `performance.now()`. If `startAt` option is provided, this will be used instead.
  * It probably should be an offset of `performance.now()`, eg: `{ startAt: performance.now() - 500 }` to shift
@@ -13,9 +15,10 @@ import type { ModSettableOptions, ModSettable, ModSettableFeedback } from "../Ty
  * 
  * When using `startAtRelative`, the starting position will be set backward by the relative amount. A value
  * of 0.5, for example, will set the timer back 50% of the interval, meaning the cycle will start half way through.
+ * 
  * @param interval 
  * @param options 
- * @returns 
+ * @returns
  */
 export function elapsed(interval: Interval, options: Partial<ModSettableOptions> = {}): ModSettable {
   const cycleLimit = options.cycleLimit ?? Number.MAX_SAFE_INTEGER;
@@ -56,23 +59,25 @@ export function elapsed(interval: Interval, options: Partial<ModSettableOptions>
 }
 
 /**
- * Counts beats based on a BPM
+ * Counts beats based on a BPM.
+ * Uses {@link elapsed} internally.
  * @param bpm 
  * @param options 
  * @returns 
  */
-export function bpm(bpm: number, options: Partial<ModSettableOptions>): ModSettable {
+export function bpm(bpm: number, options: Partial<ModSettableOptions> = {}): ModSettable {
   const interval = (60 * 1000) / bpm; // milliseconds between beats.
   return elapsed(interval, options);
 }
 
 /**
- * Counts based on hertz (oscillations per second)
+ * Counts based on hertz (oscillations per second).
+ * Uses {@link elapsed} internally.
  * @param hz 
  * @param options 
  * @returns 
  */
-export function hertz(hz: number, options: Partial<ModSettableOptions>): ModSettable {
+export function hertz(hz: number, options: Partial<ModSettableOptions> = {}): ModSettable {
   const interval = 1000 / hz;
   return elapsed(interval, options);
 }
