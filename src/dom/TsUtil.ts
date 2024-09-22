a0fd72dfbad8c0695752df068283087a9a181de8
CH <clint@thestaticvoid.net>
2024-09-22 12:08:47 +0200

    Create TsUtil.ts

---
+++ src/dom/TsUtil.ts
@@ -0,0 +1,6 @@
+export function isHtmlElement(o: any): o is HTMLElement {
+  return (
+    typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+      o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName === "string"
+  );
+}
