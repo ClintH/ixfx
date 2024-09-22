7d461718bb4eef422893d89a5847a9da1d40cb45
CH <clint@thestaticvoid.net>
2024-09-22 12:08:30 +0200

    radiansSum,degreesSum,radianArc,degreeArc
    + radiansSum/degreesSum: add angles with wrap-around logic
    + radianArc/degreeArc: calculate span of angles with wrap-around logic

--- src/__test/geometry/Angles.Test.ts
+++ src/__test/geometry/Angles.Test.ts
@@ -1,6 +1,66 @@
 import test from 'ava';
-import { degreeToRadian, radianToDegree } from '../../geometry/Angles.js';
-/* eslint-disable */
+import { degreeToRadian, radianToDegree, degreeArc, degreesSum, radiansSum } from '../../geometry/Angles.js';
+
+
+test(`degreeArc`, t => {
+  // From zero CW
+  t.is(degreeArc(0, 90, true), 270);
+  t.is(degreeArc(0, 180, true), 180);
+  t.is(degreeArc(0, 270, true), 90);
+  t.is(degreeArc(0, 360, true), 0);
+
+  // From zero CCW
+  t.is(degreeArc(0, 90, false), 90);
+  t.is(degreeArc(0, 180, false), 180);
+  t.is(degreeArc(0, 270, false), 270);
+  t.is(degreeArc(0, 360, false), 0);
+
+  // Cross over pre zero
+  t.is(degreeArc(45, 315, true), 90);
+  t.is(degreeArc(45, 225, true), 180);
+  t.is(degreeArc(45, 135, true), 270);
+  t.is(degreeArc(45, 45, true), 0);
+
+  // Cross over post zero
+  t.is(degreeArc(315, 315, false), 0);
+  t.is(degreeArc(315, 225, false), 270);
+  t.is(degreeArc(315, 135, false), 180);
+  t.is(degreeArc(315, 45, false), 90);
+
+});
+
+test(`radiansSum`, t => {
+  t.is(radiansSum(3.14, 1.57, true), 1.57);
+
+
+  // From zero CW
+  t.is(degreesSum(0, 90, true), 270);
+  t.is(degreesSum(0, 180, true), 180);
+  t.is(degreesSum(0, 270, true), 90);
+  t.is(degreesSum(0, 360, true), 0);
+
+  // From zero CCW
+  t.is(degreesSum(0, 90, false), 90);
+  t.is(degreesSum(0, 180, false), 180);
+  t.is(degreesSum(0, 270, false), 270);
+  t.is(degreesSum(0, 360, false), 0);
+
+  // Pre zero CW
+  t.is(degreesSum(45, 45, true), 0);
+  t.is(degreesSum(45, 90, true), 315);
+  t.is(degreesSum(45, 180, true), 225);
+  t.is(degreesSum(45, 270, true), 135);
+  t.is(degreesSum(45, 360, true), 45);
+
+  // Post zero CCW
+  t.is(degreesSum(315, 45, false), 0);
+  t.is(degreesSum(315, 90, false), 45);
+  t.is(Math.ceil(degreesSum(315, 180, false)), 135);
+  t.is(Math.floor(degreesSum(315, 270, false)), 225);
+  t.is(Math.ceil(degreesSum(315, 360, false)), 315);
+
+
+});
 
 test(`degreeToRadian`, (t) => {
   t.is(degreeToRadian(30).toPrecision(4), '0.5236');
--- src/geometry/Angles.ts
+++ src/geometry/Angles.ts
@@ -1,3 +1,4 @@
+import { piPi } from '../data/index.js';
 import type { Point } from './point/PointType.js';
 /**
  * Convert angle in degrees to angle in radians.
@@ -46,3 +47,142 @@ export function radianToDegree(angleInRadians: number | ReadonlyArray<number>): 
  * @returns 
  */
 export const radiansFromAxisX = (point: Point): number => Math.atan2(point.x, point.y);
+
+/**
+ * Sum angles together, accounting for the 'wrap around'.
+ * 
+ * `clockwise` of _true_ (default) means angles are added in clockwise direction
+ * 
+ * ```js
+ * // From 180deg, add 90deg in the clockwise direction
+ * radiansSum(Math.PI, Math.PI/2, true);
+ * ```
+ * 
+ * Orientation of angles is as follows:
+ * ```
+ *       90deg
+ *       Pi/2
+ *        |
+ * Pi  ---+--- 0
+ * 180    |
+ *       3PI/2
+ *       270deg
+ * ```
+ * {@link degreesSum} is the same, but uses degrees (0..360)
+ * @param start Starting angle, in radian
+ * @param amount Angle to add, in radian
+ * @param clockwise Add in clockwise direction (default: _true_)
+ * @returns Sum result, in radians
+ */
+export const radiansSum = (start: number, amount: number, clockwise = true) => {
+  if (clockwise) {
+    const x = start - amount;
+    if (x < 0) {
+      return piPi + x;
+    }
+    return x;
+  } else {
+    let x = start + amount;
+    if (x >= piPi) x = x % piPi;
+    return x;
+  }
+}
+
+/**
+ * Sum angles together, accounting for the 'wrap around'.
+ * 
+ * `clockwise` of _true_ (default) means angles are added in clockwise direction
+ * 
+ * ```js
+ * // From 180deg, add 90deg in the clockwise direction
+ * radiansSum(180, 90, true);
+ * ```
+ * 
+ * {@link radiansSum} is the same, but uses radians (0..2 Pi)
+ * 
+ * Orientation of angles is as follows:
+ * ```
+ *       90
+ *        |
+ * 180 ---+--- 0
+ *        |
+ *       270
+ * ```
+ * @param start Starting angle, in degrees
+ * @param amount Angle to add, in degrees
+ * @param clockwise Add in clockwise direction (default: _true_)
+ * @returns Sum result, in degrees
+ */
+export const degreesSum = (start: number, amount: number, clockwise = true) => radianToDegree(radiansSum(degreeToRadian(start), degreeToRadian(amount), clockwise));
+
+/**
+ * Computes the angle arc between a start and end angle,
+ * given in radians. It properly accounts for the wrap-around
+ * values.
+ * 
+ * ```js
+ * // Between 0-90deg in clockwise direction
+ * radianArc(0, Math.PI/2, true); // Yields: 3Pi/2 (270 deg)
+ * 
+ * // In counter-clockwise direction
+ * radianArc(0, Math.PI/2, false); // Yields: Math.PI/2 (90deg)
+ * ```
+ * 
+ * See {@link degreeArc} to operate in degrees.
+ * 
+ * Orientation of angles is as follows:
+ * ```
+ *       90deg
+ *       Pi/2
+ *        |
+ * Pi  ---+--- 0
+ * 180    |
+ *       3PI/2
+ *       270deg
+ * ```
+ * @param start Start angle, in radians
+ * @param end End angle, in radians
+ * @param clockwise Calculate in clockwise direction (default: _true_)
+ * @returns Angle of arc, in radians.
+ */
+export const radianArc = (start: number, end: number, clockwise = true) => {
+  let s = start;
+  if (end < s) {
+    s = 0;
+    end = piPi - start + end;
+  }
+  let d = end - s;
+  if (clockwise) d = piPi - d;
+  if (d >= piPi) return d % piPi;
+  return d;
+}
+
+/**
+ * Computes the angle arc between a start and end angle,
+ * given in degrees. It properly accounts for the wrap-around
+ * values.
+ * 
+ * ```js
+ * // Between 0-90 in clockwise direction
+ * degreeArc(0, 90, true); // Yields: 270
+ * 
+ * // In counter-clockwise direction
+ * degreeArc(0, 90, false); // Yields: 90
+ * ```
+ * 
+ * See {@link radianArc} to operate in radians.
+ * 
+ * Orientation of angles is as follows:
+ * ```
+ *       90
+ *        |
+ * 180 ---+--- 0
+ *        |
+ *       270
+ * ```
+ * @param start Start angle, in degrees
+ * @param end End angle, in degrees
+ * @param clockwise Calculate in clockwise direction (default: _true_)
+ * @returns Angle of arc, in degrees.
+ */
+export const degreeArc = (start: number, end: number, clockwise = true) => radianToDegree(radianArc(degreeToRadian(start), degreeToRadian(end), clockwise));
