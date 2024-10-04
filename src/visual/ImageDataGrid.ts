7aa7bfc014f0757e14ede53b6027818a3aa275ee
CH <clint@thestaticvoid.net>
2024-10-04 22:18:15 +0200

    Update ImageDataGrid.ts

--- src/visual/ImageDataGrid.ts
+++ src/visual/ImageDataGrid.ts
@@ -1,17 +1,31 @@
-import * as Grids from '../geometry/Grid.js';
-import type { Rgb } from './Colour.js';
+import * as Grids from '../geometry/grid/index.js';
+import type { Rgb, Rgb8Bit } from './Colour.js';
+
+
+export const grid = (image: ImageData): Grids.Grid => {
+  const g = { rows: image.width, cols: image.height };
+  return g;
+}
+
+export const wrap = (image: ImageData): Grids.GridWritable<Rgb8Bit> & Grids.GridReadable<Rgb8Bit> => {
+  return {
+    ...grid(image),
+    get: accessor(image),
+    set: setter(image)
+  }
+}
 
 /**
- * Returns a function that accesses pixel values by x,y
+ * Returns a function to access pixel values by x,y
  * @param image 
  * @returns 
  */
-export const accessor = (image: ImageData): Grids.CellAccessor<Rgb> => {
-  const grid = { rows: image.width, cols: image.height };
+export const accessor = (image: ImageData): Grids.GridCellAccessor<Rgb8Bit> => {
+  const g = grid(image);
   const data = image.data;
 
-  const fn: Grids.CellAccessor<Rgb> = (cell: Grids.Cell, bounds) => {
-    const index = Grids.indexFromCell(grid, cell, bounds);
+  const fn: Grids.GridCellAccessor<Rgb8Bit> = (cell: Grids.GridCell, bounds = `undefined`) => {
+    const index = Grids.indexFromCell(g, cell, bounds);
     if (index === undefined) {
       return undefined;
     }
@@ -21,12 +35,43 @@ export const accessor = (image: ImageData): Grids.CellAccessor<Rgb> => {
       g: data[ pxIndex + 1 ],
       b: data[ pxIndex + 2 ],
       opacity: data[ pxIndex + 3 ],
+      unit: `8bit`,
       space: `srgb`
     };
   };
   return fn;
 };
 
+/**
+ * Returns a function that sets pixel values
+ * @param image 
+ * @returns 
+ */
+export const setter = (image: ImageData): Grids.GridCellSetter<Rgb8Bit> => {
+  const g = grid(image);
+  const data = image.data;
+
+  const fn: Grids.GridCellSetter<Rgb8Bit> = (value: Rgb8Bit, cell: Grids.GridCell, bounds = `undefined`) => {
+    const index = Grids.indexFromCell(g, cell, bounds);
+    if (index === undefined) {
+      return undefined;
+    }
+    const pxIndex = index * 4;
+    data[ pxIndex ] = value.r;
+    data[ pxIndex + 1 ] = value.g;
+    data[ pxIndex + 2 ] = value.b;
+    data[ pxIndex + 3 ] = value.opacity;
+  };
+  return fn;
+}
+
+// export function* visit(image: ImageData, visitor: Grids.VisitGenerator) {
+//   const a = accessor(image);
+
+//   for (const cell of visitor) {
+//     yield a(cell, `undefined`);
+//   }
+// }
 
 /**
  * Yields pixels of an image row by row
@@ -34,15 +79,12 @@ export const accessor = (image: ImageData): Grids.CellAccessor<Rgb> => {
  */
 export function* byRow(image: ImageData) {
   const a = accessor(image);
-  const grid = { rows: image.width, cols: image.height };
+  const g = grid(image);
 
-  for (let y = 0; y < grid.rows; y++) {
-    let row: Rgb[] = [];
-    for (let x = 0; x < grid.cols; x++) {
-      const p = a({ x, y }, `undefined`);
-      if (p) row.push(p);
-    }
-    yield row;
+  const v = Grids.As.rows(g, { x: 0, y: 0 });
+  for (const row of v) {
+    let pixels = row.map(p => a(p, `undefined`));
+    yield pixels
   }
 }
 
@@ -52,11 +94,12 @@ export function* byRow(image: ImageData) {
  */
 export function* byColumn(image: ImageData) {
   const a = accessor(image);
-  const grid = { rows: image.width, cols: image.height };
+  const g = grid(image);
 
-  for (let x = 0; x < grid.cols; x++) {
-    let col: Rgb[] = [];
-    for (let y = 0; y < grid.rows; y++) {
+  //Grids.visitorRow
+  for (let x = 0; x < g.cols; x++) {
+    let col: Rgb8Bit[] = [];
+    for (let y = 0; y < g.rows; y++) {
       const p = a({ x, y }, `undefined`);
       if (p) col.push(p);
     }
