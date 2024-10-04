5990b6527fcae143d73ab153456985b778871578
CH <clint@thestaticvoid.net>
2024-10-04 22:25:28 +0200

    Update Util.Test.ts

--- src/__test/Util.Test.ts
+++ src/__test/Util.Test.ts
@@ -1,13 +1,5 @@
 import test from 'ava';
-import { relativeDifference } from '../numbers/Difference.js';
 import { toStringDefault } from '../util/ToString.js';
-test('relativeDifference', (t) => {
-  const rel = relativeDifference(100);
-  t.is(rel(100), 1);
-  t.is(rel(150), 1.5);
-  t.is(rel(50), 0.5);
-});
-
 
 test(`toStringDefault`, (t) => {
   const a = {
