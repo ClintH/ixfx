73ae2cd45c5a8b770dec1040f7c37c546376b148
CH <clint@thestaticvoid.net>
2024-10-17 17:52:25 +0200

    canvas-region

--- tests/dom/canvas-region/script.js
+++ tests/dom/canvas-region/script.js
@@ -1,9 +1,28 @@
 import { CanvasRegion, CanvasSource } from '../../../dist/visual.js';
+import {ElementSizer,resolveEl }from '../../../dist/dom.js';
 
-const s = new CanvasSource(`canvas`);
-const r = new CanvasRegion(s, {
-  x: 100, y: 0, width: 100, height: 300
+
+const canvasEl = resolveEl(`canvas`);
+const source = new CanvasSource(canvasEl);
+const r = source.createRegion({
+  match:`#targetEl`
+});
+ElementSizer.canvasViewport(canvasEl, {
+  onSetSize: (size, _el) => {
+    source.setLogicalSize(size);
+    draw();
+  }
 });
 
-r.fill(`pink`);
-r.drawBounds(`blue`);
+
+// const r = new CanvasRegion(s, {
+//   x: 100, y: 0, width: 100, height: 300
+// });
+
+
+
+function draw() {
+  if (typeof r === `undefined`) return;
+  r.fill(`pink`);
+  r.drawBounds(`blue`);
+}
