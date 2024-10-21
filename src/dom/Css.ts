1bc04f26f11cba5434c6fc27803e2969251fd24c
CH <clint@thestaticvoid.net>
2024-10-21 10:59:59 +0200

    getComputedPixels: returns a parsed pixel values for a set of CSS properties via getComputedStyle

--- src/dom/Css.ts
+++ src/dom/Css.ts
@@ -1,4 +1,68 @@
-import { resolveEls, type QueryOrElements } from "./ResolveEl.js";
+
+import type { RectPositioned } from "../geometry/Types.js";
+import { resolveEl, resolveEls, type QueryOrElements } from "./ResolveEl.js";
+
+// const tw = getComputedStyle(this.#el).borderTopWidth;
+// const bw = getComputedStyle(this.#el).borderBottomWidth;
+// const leftW = getComputedStyle(this.#el).borderLeftWidth;
+// const rightW = getComputedStyle(this.#el).borderRightWidth;
+
+type ComputedPixelsMap<T extends ReadonlyArray<keyof CSSStyleDeclaration>> = {
+  [Key in T[number]]: number
+}
+
+/**
+ * Returns the value of `getBoundingClientRect` plus the width of all the borders
+ * @param elOrQuery 
+ * @returns 
+ */
+export const getBoundingClientRectWithBorder = (elOrQuery:SVGElement|HTMLElement|string):RectPositioned => {
+  let el = resolveEl(elOrQuery);
+  const size = el.getBoundingClientRect();
+  if (el instanceof SVGElement) {
+    el = el.parentElement as HTMLElement;
+  }
+  const border = getComputedPixels(el, `borderTopWidth`, `borderLeftWidth`, `borderRightWidth`, `borderBottomWidth`);
+
+  return {
+    x: size.x,
+    y: size.y,
+    width: size.width +border.borderLeftWidth+border.borderRightWidth,
+    height: size.height+border.borderTopWidth+border.borderBottomWidth
+  }
+}
+
+/**
+ * Returns the computed measurements of CSS properties via [getComputedStyle](https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle).
+ * ```js
+ * const v = getComputedPixels(`#some-el`, `borderTopWidth`, `borderLeftWidth`);
+ * v.borderTopWidth;  // number
+ * b.borderLeftWidth; // number
+ * ```
+ * 
+ * Throws an error if value from `getComputedStyle` is not a string or does not end in 'px'.
+ * @param elOrQuery 
+ * @param properties 
+ * @returns 
+ */
+export const getComputedPixels = <T extends ReadonlyArray<keyof CSSStyleDeclaration>>(elOrQuery:HTMLElement|string, ...properties:T):ComputedPixelsMap<T> => {
+  const s = getComputedStyle(resolveEl(elOrQuery));
+  const returnValue = {};
+  for (const property of properties) {
+    const v = s[property];
+    if (typeof v === `string`) {
+      if (v.endsWith(`px`)) {
+        (returnValue as any)[property] = Number.parseFloat(v.substring(0,v.length-2));
+      } else {
+        throw new Error(`Property '${String(property)}' does not end in 'px'. Value: ${v}`);
+      }
+    } else {
+      throw new Error(`Property '${String(property)}' is not type string. Got: ${typeof v} Value: ${v}`);
+    }
+  }
+  return returnValue as ComputedPixelsMap<T>;
+}
+
 
 /**
  * Adds `cssClass` to element(s) if `value` is true.
