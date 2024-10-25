351be7acc79aee767d911d2e3938009f7253b32f
CH <clint@thestaticvoid.net>
2024-10-25 21:32:01 +0200

    With with Point3d too

--- src/geometry/point/FindMinimum.ts
+++ src/geometry/point/FindMinimum.ts
@@ -1,4 +1,15 @@
-import type { Point } from "./PointType.js";
+import { isPoint3d } from "./Guard.js";
+import type { Point, Point3d } from "./PointType.js";
+
+export function findMinimum(
+  comparer: (a: Point, b: Point) => Point,
+  ...points: ReadonlyArray<Point>
+): Point;
+
+export function findMinimum(
+  comparer: (a: Point3d, b: Point3d) => Point3d,
+  ...points: ReadonlyArray<Point3d>
+): Point3d;
 
 /**
  * Returns the 'minimum' point from an array of points, using a comparison function.
@@ -19,14 +30,18 @@ import type { Point } from "./PointType.js";
  * @param points
  * @returns
  */
-export const findMinimum = (
-  comparer: (a: Point, b: Point) => Point,
-  ...points: ReadonlyArray<Point>
-): Point => {
+export function findMinimum(
+  comparer: ((a: Point, b: Point) => Point)|((a: Point3d, b: Point3d) => Point3d),
+  ...points: ReadonlyArray<Point|Point3d>
+): Point|Point3d  {
   if (points.length === 0) throw new Error(`No points provided`);
   let min = points[ 0 ];
   for (const p of points) {
-    min = comparer(min, p);
+    if (isPoint3d(min) && isPoint3d(p)) {
+      min = comparer(min, p);
+    } else {
+      min = comparer(min as any, p as any);
+    }
   }
   return min;
 };
