2449709f83fe34523942dad89417f4c1ca338845
CH <clint@thestaticvoid.net>
2025-02-13 13:19:12 +0100

    Delete Cells.ts

--- src/geometry/grid/visitors/Cells.ts
+++
@@ -1,7 +0,0 @@
-import type { GridVisitorOpts, Grid, GridCreateVisitor } from "../Types.js";
-
-// export const cells = (grid: Grid, opts: VisitorOpts):CreateVisitor => {
-//   return () => {
-
-//   }
-// }
