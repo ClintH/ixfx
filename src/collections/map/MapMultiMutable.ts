cf47efc2b30a98494e99e3c67a27505f7a3fb7cd
CH <clint@thestaticvoid.net>
2025-02-13 13:20:15 +0100

    Delete MapMultiMutable.ts

--- src/collections/map/MapMultiMutable.ts
+++
@@ -1,7 +0,0 @@
-// // ✔ UNIT TESTED
-// import { SimpleEventEmitter } from "../Events.js";
-// import { ToString, toStringDefault, isEqualDefault } from "../Util.js";
-
-// import { without } from './Arrays.js';
-// import { circularArray } from './CircularArray.js';
-// import { CircularArray,  MapArrayOpts, MapCircularOpts,    MapSetOpts, MultiValue } from "./Interfaces.js";
