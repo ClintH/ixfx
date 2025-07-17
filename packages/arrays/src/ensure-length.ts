baf9f48f20bdfb583d6b684c6a2ac67a12fe46c5
CH <clint@thestaticvoid.net>
2025-07-17 13:41:52 +0200

    Update ensure-length.ts

--- packages/arrays/src/ensure-length.ts
+++ packages/arrays/src/ensure-length.ts
@@ -1,33 +1,47 @@
+
+export function ensureLength<V>(
+  data: readonly V[] | V[],
+  length: number,
+  expand: `repeat` | `first` | `last`
+): (V)[];
+
+export function ensureLength<V>(
+  data: readonly V[] | V[],
+  length: number,
+  expand?: `undefined`
+): (V | undefined)[];
+
 /**
- * Returns a copy of `data` with specified length.
- * If the input array is too long, it is truncated.
+ * Returns a copy of an array with specified length - padded or truncated as needed.
  *
  * If the input array is too short, it will be expanded based on the `expand` strategy:
- *  - 'undefined': fill with `undefined`
+ *  - 'undefined': fill with _undefined_ (default)
  *  - 'repeat': repeat array elements, starting from position 0
  *  - 'first': repeat with first element from `data`
  *  - 'last': repeat with last element from `data`
  *
+ * Truncate:
  * ```js
- * import { Arrays } from 'https://unpkg.com/ixfx/dist/data.js';
- *
- * Arrays.ensureLength([1,2,3], 2); // [1,2]
- * Arrays.ensureLength([1,2,3], 5, `undefined`); // [1,2,3,undefined,undefined]
- * Arrays.ensureLength([1,2,3], 5, `repeat`);    // [1,2,3,1,2]
- * Arrays.ensureLength([1,2,3], 5, `first`);     // [1,2,3,1,1]
- * Arrays.ensureLength([1,2,3], 5, `last`);      // [1,2,3,3,3]
+ * ensureLength([1,2,3], 2); // [1,2]
+ * ```
+ * 
+ * Padded:
+ * ```js
+ * ensureLength([1,2,3], 5, `undefined`); // [1,2,3,undefined,undefined]
+ * ensureLength([1,2,3], 5, `repeat`);    // [1,2,3,1,2]
+ * ensureLength([1,2,3], 5, `first`);     // [1,2,3,1,1]
+ * ensureLength([1,2,3], 5, `last`);      // [1,2,3,3,3]
  * ```
  * @param data Input array to expand
  * @param length Desired length
  * @param expand Expand strategy
  * @typeParam V Type of array
  */
-export const ensureLength = <V>(
-  data: ReadonlyArray<V> | Array<V>,
+export function ensureLength<V>(
+  data: readonly V[] | V[],
   length: number,
   expand: `undefined` | `repeat` | `first` | `last` = `undefined`
-): Array<V> => {
-  // Unit tested
+): (V | undefined)[] {
   if (data === undefined) throw new Error(`Data undefined`);
   if (!Array.isArray(data)) throw new Error(`data is not an array`);
   if (data.length === length) return [ ...data ];
@@ -37,12 +51,10 @@ export const ensureLength = <V>(
   const d = [ ...data ];
   const add = length - d.length;
 
-  //eslint-disable-next-line functional/no-let
   for (let index = 0; index < add; index++) {
-    //eslint-disable-next-line functional/immutable-data
     switch (expand) {
       case `undefined`: {
-        // @ts-expect-error
+        // @ts-expect-error all fine .....
         d.push(undefined);
         break;
       }
@@ -55,7 +67,7 @@ export const ensureLength = <V>(
         break;
       }
       case `last`: {
-        // @ts-expect-error
+        // @ts-expect-error all fine .....
         d.push(data.at(-1));
         break;
       }
