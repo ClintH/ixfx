333cab09d45bc68cff721dccc910f0ec4d34481d
CH <clint@thestaticvoid.net>
2025-07-29 15:19:16 +0200

    Export sub-modules

--- packages/modulation/src/index.ts
+++ packages/modulation/src/index.ts
@@ -1,17 +1,18 @@
 export * as Modulation from './envelope/index.js';
 export * as Sources from './source/index.js';
+export * as Oscillators from './oscillator.js';
+export * as Easings from './easing/index.js'
+export * as Envelopes from './envelope/index.js';
+export * as Forces from './forces.js';
 export * from './cubic-bezier.js'
 export * from './drift.js';
-export * as Easings from './easing/index.js'
 export type * from './easing/types.js';
-export * as Forces from './forces.js';
 export * from './gaussian.js';
 export * from './interpolate.js';
 export * from './jitter.js';
 export * from './mix.js';
 export * from './modulator-timed.js';
 export * from './no-op.js';
-export * as Oscillators from './oscillator.js';
 export * from './ping-pong.js';
 export * from './spring.js';
 export * from './timing-source-factory.js';
