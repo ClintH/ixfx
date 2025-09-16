52abc543c2226c81b78be533e3dc25b8acbb62d3
CH <clint@thestaticvoid.net>
2025-09-16 08:43:31 +0200

    Resolvable functions given spread arguments

--- packages/core/src/resolve-core.ts
+++ packages/core/src/resolve-core.ts
@@ -9,16 +9,20 @@ import { hasLast, isReactive } from "./reactive-core.js";
 /**
  * Something that can resolve to a value
  */
-export type ResolveToValueSync<V> = BasicType | ReactiveNonInitial<V> | Generator<V> | IterableIterator<V> | ((args: any) => V)
-export type ResolveToValueAsync<V> = AsyncGenerator<V> | AsyncIterableIterator<V> | Promise<V> | ((args: any) => Promise<V>);
+export type ResolveToValueSync<V> = BasicType | ReactiveNonInitial<V> | Generator<V> | IterableIterator<V> | ((...args: unknown[]) => V)
+export type ResolveToValueAsync<V> = AsyncGenerator<V> | AsyncIterableIterator<V> | Promise<V> | ((...args: unknown[]) => Promise<V>);
 export type ResolveToValue<V> = ResolveToValueAsync<V> | ResolveToValueSync<V>;
 
 /**
- * Resolves `r` to a value, where `r` is:
- * * primitive value
+ * Resolves the input to a concrete value. 
+ * 
+ * The input can be:
+ * * primitive value (string, boolean, number, object)
  * * a/sync function
  * * a/sync generator/iterator
  * * ReactiveNonInitial
+ * 
+ * Examples:
  * ```js
  * await resolve(10);       // 10
  * await resolve(() => 10); // 10
@@ -28,58 +32,60 @@ export type ResolveToValue<V> = ResolveToValueAsync<V> | ResolveToValueSync<V>;
  * });                // 10
  * ```
  * 
- * To resolve an object's properties, use {@link resolveFields}.
+ * If the input is a function, any arguments given to `resolve` are passed to it as a spread.
  * 
- * Resolve is not recursive. So if `r` is an object, it will be returned, even
- * though its properties may be resolvable.
- * @param r 
- * @param args 
+ * Resolve is not recursive. If the input is an object, it will be returned, even
+ * though its properties may be resolvable. Use {@link resolveFields} if you want to handle this case.
+ * @param resolvable Input to resolve
+ * @param args Additional arguments to pass to function-resolvables.
  * @returns 
  */
-export async function resolve<V extends BasicType>(r: ResolveToValue<V>, ...args: any): Promise<V> {
-  if (typeof r === `object`) {
-    if (`next` in r) {
-      const tag = (r as any)[ Symbol.toStringTag ];
+export async function resolve<V extends BasicType>(resolvable: ResolveToValue<V>, ...args: unknown[]): Promise<V> {
+  if (typeof resolvable === `object`) {
+    if (`next` in resolvable) {
+      const tag = (resolvable as any)[ Symbol.toStringTag ];
       if (tag === `Generator` || tag == `Array Iterator`) {
-        const v = r.next();
+        const v = resolvable.next();
         if (`done` in v && `value` in v) return v.value as V;
         return v as V;
       } else if (tag === `AsyncGenerator`) {
-        const v = await r.next();
+        const v = await resolvable.next();
         //console.log(`  hasDone: ${ `done` in v } value:`, v);
         if (`done` in v && `value` in v) return v.value as V;
         return v as V;
       } else {
         throw new Error(`Object has 'next' prop, but does not have 'AsyncGenerator', 'Generator' or 'Array Iterator' string tag symbol. Got: '${ tag }'`);
       }
-    } else if (isReactive<V>(r)) {
-      if (hasLast(r)) return r.last()!;
+    } else if (isReactive<V>(resolvable)) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      if (hasLast(resolvable)) return resolvable.last()!;
       throw new Error(`Reactive does not have last value`);
     } else {
       // Some regular object
-      return r as V;
+      return resolvable as V;
     }
-  } else if (typeof r === `function`) {
-    const v = await r(args) as V;
+  } else if (typeof resolvable === `function`) {
+
+    const v = await resolvable(...args) as V;
     return v;
   } else {
     // Primitive value?
-    return r as V;
+    return resolvable as V;
   }
 }
 
 /**
- * For a given input `r`, attempts to 'resolve' it. See {@link resolve} for details.
- * @param r 
+ * For the given input, attempts to 'resolve' it. See {@link resolve} for details and asynchronous version.
+ * @param resolvable 
  * @param args 
  * @returns 
  */
-export function resolveSync<V extends BasicType>(r: ResolveToValueSync<V>, ...args: any): V {
-  if (typeof r === `object`) {
-    if (`next` in r) {
-      const tag = (r as any)[ Symbol.toStringTag ];
+export function resolveSync<V extends BasicType>(resolvable: ResolveToValueSync<V>, ...args: unknown[]): V {
+  if (typeof resolvable === `object`) {
+    if (`next` in resolvable) {
+      const tag = (resolvable as any)[ Symbol.toStringTag ];
       if (tag === `Generator` || tag == `Array Iterator`) {
-        const v = r.next();
+        const v = resolvable.next();
         if (`done` in v && `value` in v) return v.value as V;
         return v as V;
       } else if (tag === `AsyncGenerator`) {
@@ -87,25 +93,26 @@ export function resolveSync<V extends BasicType>(r: ResolveToValueSync<V>, ...ar
       } else {
         throw new Error(`Object has 'next' prop, but does not have 'Generator' or 'Array Iterator' string tag symbol. Got: '${ tag }'`);
       }
-    } else if (isReactive<V>(r)) {
-      if (hasLast(r)) return r.last()!;
+    } else if (isReactive<V>(resolvable)) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      if (hasLast(resolvable)) return resolvable.last()!;
       throw new Error(`Reactive does not have last value`);
     } else {
       // Some regular object
-      return r as V;
+      return resolvable as V;
     }
-  } else if (typeof r === `function`) {
-    return r(args);
+  } else if (typeof resolvable === `function`) {
+    return resolvable(...args);
   } else {
     // Primitive value?
-    return r as V;
+    return resolvable as V;
   }
 }
 
 /**
- * Resolves a value as per {@link resolve}, however
- * If an error is thrown or the resolution results in _undefined_ 
- * or NaN, `fallbackValue` is returned instead.
+ * Resolves a value as per {@link resolve}, however f an error is thrown 
+ * or the resolution results in _undefined_ 
+ * or NaN, the fallback value is returned instead.
  * 
  * `null` is an allowed return value.
  * 
@@ -118,16 +125,18 @@ export function resolveSync<V extends BasicType>(r: ResolveToValueSync<V>, ...ar
  * const r = resolveWithFallback(fn, 1);
  * const value = r(); // Always 0 or 1
  * ```
+ * 
+ * See also {@link resolveWithFallbackSync}
  * @param p Thing to resolve
- * @param fallback Fallback value if an error happens, undefined or NaN
+ * @param options Fallback value if an error happens, undefined or NaN
  * @param args 
  * @returns 
  */
-export async function resolveWithFallback<T extends BasicType>(p: ResolveToValue<T>, fallback: ResolveFallbackOpts<T>, ...args: any) {
+export async function resolveWithFallback<T extends BasicType>(p: ResolveToValue<T>, options: ResolveFallbackOptions<T>, ...args: unknown[]) {
   let errored = false;
-  let fallbackValue = fallback.value;
-  const overrideWithLast = fallback.overrideWithLast ?? false;
-  if (fallbackValue === undefined) throw new Error(`Needs a fallback value`);
+  let fallbackValue = options.value;
+  const overrideWithLast = options.overrideWithLast ?? false;
+  if (fallbackValue === undefined) throw new Error(`Param 'options.value' is undefined`);
 
   try {
     const r = await resolve(p, ...args);
@@ -144,11 +153,24 @@ export async function resolveWithFallback<T extends BasicType>(p: ResolveToValue
   }
 }
 
-export function resolveWithFallbackSync<T extends BasicType>(p: ResolveToValueSync<T>, fallback: ResolveFallbackOpts<T>, ...args: any) {
+/**
+ * Resolves a 'resolvable', using a fallback value if it results to _undefined_ or _NaN_. _null_ is allowed.
+ * 
+ * See also {@link resolveWithFallback} for the asynchronous version.
+ * 
+ * Options:
+ * * value: Fallback value
+ * * overrideWithLast: If true, uses the previously-valid value as the replacement fallback (default: false)
+ * @param p 
+ * @param options 
+ * @param args 
+ * @returns 
+ */
+export function resolveWithFallbackSync<T extends BasicType>(p: ResolveToValueSync<T>, options: ResolveFallbackOptions<T>, ...args: unknown[]) {
   let errored = false;
-  let fallbackValue = fallback.value;
-  const overrideWithLast = fallback.overrideWithLast ?? false;
-  if (fallbackValue === undefined) throw new Error(`Needs a fallback value`);
+  let fallbackValue = options.value;
+  const overrideWithLast = options.overrideWithLast ?? false;
+  if (fallbackValue === undefined) throw new Error(`Param 'options.value' is undefined`);
 
   try {
     const r = resolveSync(p, ...args);
@@ -165,4 +187,17 @@ export function resolveWithFallbackSync<T extends BasicType>(p: ResolveToValueSy
   }
 }
 
-export type ResolveFallbackOpts<T> = { value: T, overrideWithLast?: boolean }
+/**
+ * Options for {@link resolveWithFallbackSync}
+ */
+export type ResolveFallbackOptions<T> = {
+  /**
+   * Fallback value
+   */
+  value: T,
+  /**
+   * If _true_, will use the last valid value as a replacement fallback
+   * Default: false
+   */
+  overrideWithLast?: boolean
+}
