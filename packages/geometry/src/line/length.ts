b4fd888cc8fd727d28867ec2d87a8b56a22dcb4d
CH <clint@thestaticvoid.net>
2025-10-12 20:38:35 +0200

    Line.length added 2d calculation option

--- packages/geometry/src/line/length.ts
+++ packages/geometry/src/line/length.ts
@@ -3,6 +3,7 @@ import { getPointParameter } from "./get-points-parameter.js";
 import { isPolyLine } from "./guard.js";
 import type { Line, PolyLine } from "./line-type.js";
 
+
 /**
  * Returns the length between two points
  * ```js
@@ -12,7 +13,7 @@ import type { Line, PolyLine } from "./line-type.js";
  * @param b Second point
  * @returns 
  */
-export function length(a: Point, b: Point): number;
+export function length(a: Point, b: Point, force2d?: boolean): number;
 
 /**
  * Returns length of line. If a polyline (array of lines) is provided,
@@ -24,7 +25,7 @@ export function length(a: Point, b: Point): number;
  * ```
  * @param line Line
  */
-export function length(line: Line | PolyLine): number;
+export function length(line: Line | PolyLine, force2d?: boolean): number;
 
 /**
  * Returns length of line, polyline or between two points
@@ -34,16 +35,19 @@ export function length(line: Line | PolyLine): number;
  * @returns Length (total accumulated length for arrays)
  */
 
-export function length(aOrLine: Point | Line | PolyLine, pointB?: Point): number {
+export function length(aOrLine: Point | Line | PolyLine, pointBOrForce2d?: Point | boolean, force2d?: boolean): number {
   if (isPolyLine(aOrLine)) {
-    const sum = aOrLine.reduce((accumulator, v) => length(v) + accumulator, 0);
+    const _force2d = typeof pointBOrForce2d === `boolean` ? pointBOrForce2d : false;
+    const sum = aOrLine.reduce((accumulator, v) => length(v, _force2d) + accumulator, 0);
     return sum;
   }
   if (aOrLine === undefined) throw new TypeError(`Parameter 'aOrLine' is undefined`);
-  const [ a, b ] = getPointParameter(aOrLine, pointB);
+  const [ a, b ] = typeof pointBOrForce2d === `object` ? getPointParameter(aOrLine, pointBOrForce2d) : getPointParameter(aOrLine);
   const x = b.x - a.x;
   const y = b.y - a.y;
-  if (a.z !== undefined && b.z !== undefined) {
+  const _force2d = typeof pointBOrForce2d === `boolean` ? pointBOrForce2d :
+    typeof force2d === `boolean` ? force2d : false;
+  if (!_force2d && a.z !== undefined && b.z !== undefined) {
     const z = b.z - a.z;
     return Math.hypot(x, y, z);
   } else {
