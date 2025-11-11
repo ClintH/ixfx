dd4ad8d83b7bc39eeed577d3b52f2e7b2bb40406
CH <clint@thestaticvoid.net>
2025-11-11 11:58:46 +0100

    prefixProperties

---
+++ packages/core/src/records/prefix.ts
@@ -0,0 +1,26 @@
+/**
+ * Returns a new object based on `data` but with all
+ * properties prefixed by `prefix`.
+ * 
+ * ```js
+ * prefixProperties({ name: `x`, size: 10 }, `test-`);
+ * 
+ * // Yields:
+ * // { test-name: `x`, test-size: 10 }
+ * ```
+ * @param data 
+ * @param prefix 
+ * @returns 
+ */
+export function prefixProperties(data: Record<string | number, any>[], prefix: string) {
+  const changed: Record<string, unknown>[] = [];
+  for (const d of data) {
+    const entries = Object.entries(d);
+    const changedEntries: [ key: string, value: any ][] = [];
+    for (const [ key, value ] of entries) {
+      changedEntries.push([ prefix + key, value ]);
+    }
+    changed.push(Object.fromEntries(changedEntries));
+  }
+  return changed;
+}
