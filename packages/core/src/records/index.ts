4dc661da43c22c7b6a086e2309078f87a184cd85
CH <clint@thestaticvoid.net>
2025-11-11 11:58:53 +0100

    Core.Records.prefixProperties, .enumerateNumericalValues, .zipRecords

--- packages/core/src/records/index.ts
+++ packages/core/src/records/index.ts
@@ -1,9 +1,13 @@
-export * from './compare.js';
+export * from './circular.js';
 export * from './clone-from-fields.js';
-export * from './map-object.js';
-export * from './map-object-keys.js';
-export * from './traverse.js';
-export * from './merge.js';
+export * from './compare.js';
 export * from './keys-to-numbers.js';
+export * from './map-object-keys.js';
+export * from './map-object.js';
+export * from './merge.js';
+export * from './prefix.js';
+export * from './traverse.js';
+export * from './values.js';
+export * from './zip.js';
 export type * from '../types-compare.js';
 
