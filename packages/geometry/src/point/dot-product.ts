b4526f9e809bf9ee3e0edc8201880f0b97458ab2
CH <clint@thestaticvoid.net>
2026-01-08 12:33:56 +0100

    Added crossProductRaw

--- packages/geometry/src/point/dot-product.ts
+++ packages/geometry/src/point/dot-product.ts
@@ -6,3 +6,31 @@ export const dotProduct = (...pts: readonly Point[]): number => {
   const a = pts.map(p => toArray(p));
   return ArraysDotProduct(a);
 };
+
+/**
+ * Returns the cross-product:
+ * ```
+ * ax * by - ay * bx
+ * ```
+ * @param a 
+ * @param b
+ * @returns 
+ */
+export function cross(a: Point, b: Point): number {
+  return a.x * b.y - a.y * b.x;
+}
+
+/**
+ * Returns the cross-product:
+ * ```
+ * ax * by - ay * bx
+ * ```
+ * @param ax 
+ * @param ay 
+ * @param bx 
+ * @param by 
+ * @returns 
+ */
+export function crossProductRaw(ax: number, ay: number, bx: number, by: number): number {
+  return ax * by - ay * bx;
+}
