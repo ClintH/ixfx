4ea62033e5bce4b447424904c2bc66e825c6e511
CH <clint@thestaticvoid.net>
2026-03-12 16:13:47 +0100

    Update labelled.ts

--- packages/collections/src/tree/labelled.ts
+++ packages/collections/src/tree/labelled.ts
@@ -1,11 +1,12 @@
-import { LabelledSingleValue, LabelledValue, LabelledValues } from "./types.js";
+import type { LabelledSingleValue, LabelledValue, LabelledValues } from "./types.js";
 
-export const isSingleValue = <T>(v:LabelledValue<T>):  v is LabelledSingleValue<T> => {
-  if (`value` in v) return true;
+export function isSingleValue<T>(v: LabelledValue<T>): v is LabelledSingleValue<T> {
+  if (`value` in v)
+    return true;
   return false;
 }
 
-export const isMultiValue = <T>(v:LabelledValue<T>):  v is LabelledValues<T> => {
+export function isMultiValue<T>(v: LabelledValue<T>): v is LabelledValues<T> {
   if (`values` in v) {
     return Array.isArray(v.values);
   }
