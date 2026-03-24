2a5f42ebd531941c0edd0c5941e00b68d9df9693
CH <clint@thestaticvoid.net>
2026-03-24 16:28:54 +0100

    FrequencyByGroup: expanded frequency tracker

--- packages/arrays/tests/frequency.test.ts
+++ packages/arrays/tests/frequency.test.ts
@@ -1,54 +1,53 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
-import { test, expect, describe } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { FrequencyByGroup } from '../src/frequency.js';
 import * as Arrays from '../src/index.js';
 
 describe(`frequency`, () => {
-  test(`string-key`, () => {
-    const list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  it(`value`, () => {
+    const groupBy = (v: number) => v.toString();
+    const sub = FrequencyByGroup.entriesFromArray([1, 2, 3, 1, 2, 3, 0, 1, 1, 1, 4], groupBy);
+    expect([...sub.entries()]).toStrictEqual([[`1`, 5], [`2`, 2], [`3`, 2], [`0`, 1], [`4`, 1]]);
+  });
+
+  it(`string-key`, () => {
     const groupBy = (v: number) => v % 2 === 0 ? `even` : `odd`;
-    const sub = Arrays.frequencyByGroup(groupBy, list);
+    const sub = FrequencyByGroup.fromArray(list, groupBy);
 
     expect(sub.get(`even`)).toEqual(5);
     expect(sub.get(`odd`)).toEqual(5);
-    expect([ ...sub.keys() ].length).toEqual(2);
+    expect([...sub.keys()].length).toEqual(2);
   });
 
-  test(`number-key`, () => {
-    const list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+  it(`number-key`, () => {
     const groupBy = (v: number) => v % 2 === 0 ? 1 : 2;
-    const sub = Arrays.frequencyByGroup(groupBy, list);
+    const sub = FrequencyByGroup.fromArray(list, groupBy);
 
     expect(sub.get(1)).toEqual(5);
     expect(sub.get(2)).toEqual(5);
-    expect([ ...sub.keys() ].length).toEqual(2);
+    expect([...sub.keys()].length).toEqual(2);
   });
 
-  test(`params`, () => {
+  it(`params`, () => {
     const groupByString = (v: number) => v % 2 === 0 ? `even` : `odd`;
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const groupByBadVoid = (v: number) => {};
-    const groupByBadNonString = (v: number) => null;
-
-    const data = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+    const groupByBadVoid = (_v: number) => {};
+    const groupByBadNonString = (_v: number) => null;
 
     // Validity of groupBy
-    // @ts-expect-error
-    expect(() => Arrays.frequencyByGroup(null, data)).toThrow();
-    // @ts-expect-error
-    expect(() => Arrays.frequencyByGroup(undefined, data)).toThrow();
-    // @ts-expect-error
-    expect(() => Arrays.frequencyByGroup(groupByBadVoid, data)).toThrow();
-    // @ts-expect-error
-    expect(() => Arrays.frequencyByGroup(groupByBadNonString, data)).toThrow();
+    // @ts-expect-error error checking
+    expect(() => FrequencyByGroup.fromArray(null, list)).toThrow();
+    // @ts-expect-error error checking
+    expect(() => FrequencyByGroup.fromArray(undefined, list)).toThrow();
+    // @ts-expect-error error checking
+    expect(() => FrequencyByGroup.fromArray(groupByBadVoid, list)).toThrow();
+    // @ts-expect-error error checking
+    expect(() => FrequencyByGroup.fromArray(groupByBadNonString, list)).toThrow();
 
     // Validity of data
-    // @ts-expect-error
-    expect(() => Arrays.frequencyByGroup(groupByString, undefined)).toThrow();
-    // @ts-expect-error
-    expect(() => Arrays.frequencyByGroup(groupByString, `hello`)).toThrow();
-
+    // @ts-expect-error error checking
+    expect(() => FrequencyByGroup.fromArray(groupByString, undefined)).toThrow();
+    // @ts-expect-error error checking
+    expect(() => FrequencyByGroup.fromArray(groupByString, `hello`)).toThrow();
   });
-
-})
+});
