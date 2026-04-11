8e55b8a27eb9948c13301254d1d32124b74d6840
CH <clint@thestaticvoid.net>
2026-04-11 16:46:34 +0200

    sorted-arrays: Many new & improved functions for working with sorted arrays using a binary search algorithm

--- packages/collections/src/sorted-array.ts
+++ packages/collections/src/sorted-array.ts
@@ -1,11 +1,11 @@
-import { defaultComparer, type Comparer } from "@ixfx/core";
+import type { Comparer } from "@ixfx/core";
+import { defaultComparer } from "@ixfx/core";
 
 /**
  * Returns an immutable wrapper around an array, initially unsorted.
  * Sorts data and passes to {@link wrapSorted}.
- * @param unsortedData 
- * @param comparer 
- * @returns 
+ * @param unsortedData
+ * @param comparer
  */
 export const wrapUnsorted = <T>(unsortedData: T[], comparer: Comparer<T> = defaultComparer): WrapSortedArray<T> => wrapSorted(unsortedData.toSorted(comparer));
 
@@ -13,76 +13,76 @@ export type WrapSortedArray<T> = {
   /**
    * Returns index of an item, or -1 if not found
    * @param sought Item to find
-   * @param start 
-   * @param end 
-   * @returns 
+   * @returns
    */
-  indexOf: (sought: T, start?: number, end?: number) => number
+  indexOf: (sought: T) => number;
+
+  matchingRange: (sought: T) => { startIndex: number; endIndex: number } | undefined;
 
   /**
    * Returns the index at which the value would be inserted
-   * @param toInsert 
-   * @returns 
+   * @param toInsert
+   * @returns
    */
-  insertionIndex: (toInsert: T) => number
+  insertionIndex: (toInsert: T) => number;
 
   /**
-  * Inserts an item, returning a new wrapper
-  * @param toInsert 
-  * @returns 
-  */
-  insert: (toInsert: T) => WrapSortedArray<T>
+   * Inserts an item, returning a new wrapper
+   * @param toInsert
+   * @returns
+   */
+  insert: (toInsert: T) => WrapSortedArray<T>;
 
   /**
    * Removes an item, returning a new wrapper
-   * @param toRemove 
-   * @returns 
+   * @param toRemove
+   * @returns
    */
-  remove: (toRemove: T) => WrapSortedArray<T>
+  remove: (toRemove: T) => WrapSortedArray<T>;
 
   /**
-  * Gets item at a specified offset
-  * @param offset 
-  * @returns 
-  */
-  at: (offset: number) => T | undefined
+   * Gets item at a specified offset
+   * @param offset
+   * @returns
+   */
+  at: (offset: number) => T | undefined;
 
   /**
    * Gets length
    */
-  get length(): number
+  get length(): number;
 
   /**
-  * Gets underlying array.
-  * Be careful not to mutate. Use `toArray` to get a copy
-  * that can be modified.
-  */
-  get data(): T[]
+   * Gets underlying array.
+   * Be careful not to mutate. Use `toArray` to get a copy
+   * that can be modified.
+   */
+  get data(): T[];
 
   /**
-  * Returns a copy of data which is safe to modify.
-  * @returns 
-  */
-  toArray: () => T[]
-}
+   * Returns a copy of data which is safe to modify.
+   * @returns
+   */
+  toArray: () => T[];
+};
 /**
  * Returns an immutable wrapper around a sorted array.
  * Use {@link wrapUnsorted} if not yet sorted.
- * 
+ *
  * Functions that change contents return a new wrapped instance.
- * 
+ *
  * ```js
  * let w = wrapSorted([ 1, 2, 10 ]);
  * w.indexOf(1);    // 0
  * w = w.insert(9); // Inserts 9, returning a new wrapper
  * w = w.remove(9); // Removes 9, returning a new wrapper
  * ```
- * 
+ *
  * You can access the underyling sorted array with the `data` property.
  * It's important this is not modified since the wrapper assumes its immutable
  * and stays sorted. Use `toArray()` to get a copy of the array which can be
  * changed.
- * 
+ *
  * ```js
  * // A few basic array-like features supported
  * w.length;    // Get length of array
@@ -90,24 +90,28 @@ export type WrapSortedArray<T> = {
  * w.data;      // Get underlying array
  * w.toArray(); // Get a copy of the underyling array
  * ```
- * @param sortedData 
- * @param comparer 
- * @returns 
+ * @param sortedData
+ * @param comparer
+ * @returns Wrapped, sorted array
  */
-export const wrapSorted = <T>(sortedData: T[], comparer: Comparer<T> = defaultComparer): WrapSortedArray<T> => {
-  const store = [ ...sortedData ];
+export function wrapSorted<T>(sortedData: T[], comparer: Comparer<T> = defaultComparer): WrapSortedArray<T> {
+  const store = [...sortedData];
   return {
 
-    indexOf: (sought: T, start = 0, end = store.length) => {
-      return indexOf(store, sought, start, end, comparer);
+    indexOf: (sought: T) => {
+      // return indexOf(store, sought, start, end, comparer);
+      return indexOf(store, sought, comparer);
     },
     insertionIndex: (toInsert: T) => {
-      return insertionIndex(store, toInsert, 0, store.length, comparer);
+      // return insertionIndex(store, toInsert, 0, store.length, comparer);
+      return insertionIndex(store, toInsert, comparer);
     },
     insert: (toInsert: T) => {
       return wrapSorted(insert(store, toInsert, comparer), comparer);
     },
-
+    matchingRange: (sought: T): { startIndex: number; endIndex: number } | undefined => {
+      return matchingRange(store, sought, comparer);
+    },
     remove: (toRemove: T) => {
       return wrapSorted(remove(store, toRemove, comparer));
     },
@@ -123,82 +127,152 @@ export const wrapSorted = <T>(sortedData: T[], comparer: Comparer<T> = defaultCo
     },
 
     toArray: () => {
-      return [ ...store ]
-    }
-  }
+      return [...store];
+    },
+  };
 }
 
 /**
- * Returns index of data in an ascended sorted array using a binary search.
+ * Returns first index of data in an ascended sorted array using a binary search.
  * Returns -1 if data was not found.
  * ```js
  * indexOf([1,2,3], 3); // 2
  * indexOf([1,2,3], 0); // -1, not found
  * ```
- * 
- * By default uses Javascript comparision semantics. 
+ *
+ * By default uses Javascript comparision semantics.
  * Passing in `comparer` is needed when working with an array of objects.
  * @param data Array of data
  * @param sought Item to search for
- * @param start Start point
- * @param end End index
  * @param comparer Comparer (by default uses JS semantics)
  * @returns Index of sought item or -1 if not found.
  */
-export const indexOf = <T>(data: T[], sought: T, start = 0, end: number = data.length, comparer: Comparer<T> = defaultComparer): number => {
-  const debug = false; //(sought == 1);
-  if (debug) console.log(`indexOf: sought: ${ sought } start: ${ start } end: ${ end }`)
-  if (end < start) {
-    if (debug) console.log(`indexOf   end <= start ${ end } <= ${ start }`);
+export function indexOf<T>(data: T[], sought: T, comparer: Comparer<T> = defaultComparer): number {
+  const range = matchingRange(data, sought, comparer);
+  if (!range)
     return -1;
+  return range.startIndex;
+}
+
+/**
+ * Returns the lower bound index for an item in a sorted array using a binary search.
+ * This is the index at which the item would be inserted to maintain sorted order, and is to the left of any existing entries in the case of equal values.
+ *
+ * ```js
+ * const data = [2, 8, 16, 32];
+ * Sorted.lowerBound(data, 1);   // 0
+ * Sorted.lowerBound(data, 2);   // 0
+ * Sorted.lowerBound(data, 3);   // 1
+ * Sorted.lowerBound(data, 320); // 4
+ * ```
+ *
+ * If item is past the end of the array, the length of the array is returned (which is the index at which the item would be inserted to maintain sorted order).
+ *
+ * @param sortedList Sorted list
+ * @param item Item
+ * @param comparer Comparer
+ * @returns Index
+ */
+export function lowerBound<T>(
+  sortedList: T[],
+  item: T,
+  comparer: Comparer<T> = defaultComparer,
+): number {
+  let left = 0;
+  let right = sortedList.length;
+  while (left < right) {
+    const mid = (left + right) >> 1;
+    if (comparer(sortedList[mid], item) < 0) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
   }
-  const mid = Math.floor((start + end) / 2);
-  const result = comparer(data[ mid ], sought);
 
-  if (debug) console.log(`indexOf   mid: ${ mid } result: ${ result }`);
+  return left;
+}
 
-  // Result is at the middle
-  if (result === 0) return mid;
-
-  if (result > 0) {
-    // data[mid] should come after sought. Must be in left side
-    return indexOf(data, sought, start, mid - 1, comparer);
+/**
+ * Returns the upper bound index for an item in a sorted array using a binary search.
+ * This is the index at which the item would be inserted to maintain sorted order, and is to the right of any existing entries in the case of equal values.
+ * ```js
+ * const data = [2, 8, 16, 32];
+ * Sorted.upperBound(data, 1); // 0
+ * Sorted.upperBound(data, 2); // 1
+ * Sorted.upperBound(data, 3); // 1
+ * ```
+ * It the item is past the end of the array, the length of the array is returned (which is the index at which the item would be inserted to maintain sorted order).
+ * @param sortedList
+ * @param item
+ * @param comparer
+ */
+export function upperBound<T>(sortedList: T[], item: T, comparer: Comparer<T> = defaultComparer): number {
+  let left = 0;
+  let right = sortedList.length;
+  while (left < right) {
+    const mid = (left + right) >> 1;
+    if (comparer(sortedList[mid], item) <= 0) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
   }
+  return left;
+}
 
-  // data[mid] is less than sought, must be in right side
-  return indexOf(data, sought, mid + 1, end, comparer);
+/**
+ * Returns the index range of `sortedList` where item(s) equal to `item` are found. Returns `undefined`
+ * if `item` was not found.
+ *
+ * ```js
+ * const data = [2, 8, 16, 16, 32];
+ * Sorted.matchingRange(data, 1);  // _undefined_
+ * Sorted.matchingRange(data, 8);  // { startIndex: 1, endIndex: 1 }
+ * Sorted.matchingRange(data, 16); // { startIndex: 2, endIndex: 3 }
+ * ```
+ * @param sortedList
+ * @param item
+ * @param comparer
+ * @returns Matching range of `item`, or _undefined_ if not found.
+ */
+export function matchingRange<T>(
+  sortedList: T[],
+  item: T,
+  comparer: Comparer<T> = defaultComparer,
+): { startIndex: number; endIndex: number } | undefined {
+  if (sortedList.length === 0)
+    return;
+
+  // Lower bound
+  const start = lowerBound(sortedList, item, comparer);
+
+  // Not found at all
+  if (start >= sortedList.length || comparer(sortedList[start], item) !== 0)
+    return;
+
+  // Upper bound
+  const end = upperBound(sortedList, item, comparer) - 1;
+
+  return { startIndex: start, endIndex: end };
 }
 
 /**
  * Returns index to insert data into a sorted array using a binary search.
  * Adds to the right of existing entries in the case of equal values.
- * 
- * By default uses Javascript comparision semantics. 
+ *
+ * By default uses Javascript comparision semantics.
  * Passing in `comparer` is needed when working with an array of objects.
- * @param data 
- * @param toInsert 
- * @param start 
- * @param end 
- * @param comparer 
+ * @param data
+ * @param toInsert
+ * @param comparer
  */
-export const insertionIndex = <T>(data: T[], toInsert: T, start = 0, end: number = data.length, comparer: Comparer<T> = defaultComparer): number => {
-  const mid = Math.floor((start + end) / 2);
+export function insertionIndex<T>(data: T[], toInsert: T, comparer: Comparer<T> = defaultComparer): number {
+  if (typeof comparer !== `function`)
+    throw new TypeError(`Param 'comparer' is not a function`);
+  if (!Array.isArray(data))
+    throw new TypeError(`Param 'data' is not an array`);
 
-  const result = comparer(data[ mid ], toInsert);
-  //console.log(`mid index: ${ mid } mid data: ${ data[ mid ] } result: ${ result } value: ${ toInsert } start: ${ start } end: ${ end }`);
-
-  // Result is at the middle
-  if (result === 0) return mid + 1;
-
-  if (result > 0) {
-    if (start === end) return start;
-    if (start + 1 === end) return start;
-    // data[mid] is greater than sought, must be in left side
-    return insertionIndex(data, toInsert, start, mid - 1, comparer);
-  }
-
-  // data[mid] is less than sought, must be in right side
-  return insertionIndex(data, toInsert, mid + 1, end, comparer);
+  return upperBound(data, toInsert, comparer);
 }
 
 /**
@@ -208,72 +282,76 @@ export const insertionIndex = <T>(data: T[], toInsert: T, start = 0, end: number
  * data = insert(data, 2.5);
  * // [ 1, 2, 2.5, 3, 4 ]
  * ```
- * 
- * By default uses Javascript comparision semantics. 
+ *
+ * By default uses Javascript comparision semantics.
  * Passing in `comparer` is needed when working with an array of objects.
  * @param sortedArray Sorted array
  * @param toInsert Data to insert
  * @param comparer Comparer, uses JS default semantics if not specified.
- * @returns 
+ * @returns Returns new array with item inserted.
  */
-export const insert = <T>(sortedArray: T[], toInsert: T, comparer: Comparer<T> = defaultComparer): T[] => {
-  const index = insertionIndex(sortedArray, toInsert, 0, sortedArray.length, comparer);
+export function insert<T>(sortedArray: T[], toInsert: T, comparer: Comparer<T> = defaultComparer): T[] {
+  // const index = insertionIndex(sortedArray, toInsert, 0, sortedArray.length, comparer);
+  const index = insertionIndex(sortedArray, toInsert, comparer);
   const pre = sortedArray.slice(0, index);
   const post = sortedArray.slice(index);
-  return [ ...pre, toInsert, ...post ];
+  return [...pre, toInsert, ...post];
 }
 
 /**
- * Removes item from a sorted array if it exists, returning the modified array.
+ * Removes item(s) from a sorted array if it exists, returning the modified array.
+ *
  * ```js
- * let data = [ 1, 2, 3, 4 ];
+ * let data = [ 1, 2, 3, 3, 4 ];
  * data = remove(data, 3);
  * // [ 1, 2, 4 ]
  * ```
- * 
- * By default uses Javascript comparision semantics. 
+ *
+ * By default uses Javascript comparision semantics, removing items considered equal to `toRemove`.
  * Passing in `comparer` is needed when working with an array of objects.
- * @param data 
- * @param toRemove 
- * @param comparer 
- * @returns 
+ *
+ * @param sortedArray Sorted array
+ * @param toRemove Item to remove
+ * @param comparer Comparer
+ * @returns Returns new array with item removed if it was found, otherwise original array.
  */
-export const remove = <T>(data: T[], toRemove: T, comparer: Comparer<T> = defaultComparer): T[] => {
-  const index = indexOf(data, toRemove, 0, data.length, comparer);
+export function remove<T>(sortedArray: T[], toRemove: T, comparer: Comparer<T> = defaultComparer): T[] {
+  const range = matchingRange(sortedArray, toRemove, comparer);
+  if (!range)
+    return sortedArray;
 
-  if (index === -1) return data;
-  const pre = data.slice(0, index);
-  const post = data.slice(index + 1);
-  return [ ...pre, ...post ];
+  const pre = sortedArray.slice(0, range.startIndex);
+  const post = sortedArray.slice(range.endIndex + 1);
+  return [...pre, ...post];
 }
 
 /**
  * Merges two sorted arrays, returning result.
- * 
+ *
  * ```js
  * const a = [ 4, 7, 10 ]
  * const b = [ 1, 2, 9, 11 ]
  * const c = merge(a, b);
  * // [ 1, 2, 4, 7, 9, 10, 11 ]
  * ```
- * 
+ *
  * Undefined behaviour if either input array is not sorted.
- * 
- * By default uses Javascript comparision semantics. 
+ *
+ * By default uses Javascript comparision semantics.
  * Passing in `comparer` is needed when working with an array of objects.
  * @param a Sorted array
  * @param b Sorted array
  * @param comparer Comparator
  */
-export const merge = <T>(a: T[], b: T[], comparer: Comparer<T> = defaultComparer): T[] => {
+export function merge<T>(a: T[], b: T[], comparer: Comparer<T> = defaultComparer): T[] {
   // Adapted from https://github.com/larrybotha/building-algorithms-using-typescript/blob/master/src/09-merge-sort-algorithm.ts
   const t: T[] = [];
   let aIndex = 0;
   let bIndex = 0;
 
   while (aIndex + bIndex < a.length + b.length) {
-    const aItem = a[ aIndex ];
-    const bItem = b[ bIndex ];
+    const aItem = a[aIndex];
+    const bItem = b[bIndex];
     const comp = comparer(aItem, bItem);
     if (aItem === undefined) {
       t.push(bItem);
@@ -282,11 +360,9 @@ export const merge = <T>(a: T[], b: T[], comparer: Comparer<T> = defaultComparer
       t.push(aItem);
       aIndex++;
     } else if (comp < 0) {
-
       t.push(aItem);
       aIndex++;
     } else {
-
       t.push(bItem);
 
       bIndex++;
@@ -295,9 +371,10 @@ export const merge = <T>(a: T[], b: T[], comparer: Comparer<T> = defaultComparer
   return t;
 }
 
-const sortMerge = <T>(data: T[] | readonly T[], comparer: Comparer<T> = defaultComparer): T[] => {
+function sortMerge<T>(data: T[] | readonly T[], comparer: Comparer<T> = defaultComparer): T[] {
   // Adapted from https://github.com/larrybotha/building-algorithms-using-typescript/blob/master/src/09-merge-sort-algorithm.ts
-  if (data.length <= 1) return [ ...data ];
+  if (data.length <= 1)
+    return [...data];
   const mIndex = Math.floor(data.length / 2);
   const a = data.slice(0, mIndex);
   const b = data.slice(mIndex);
@@ -312,30 +389,30 @@ export type SortAlgorithm = `default` | `merge`;
  * const data = [ 10, 2, 9, 5 ]
  * sort(data, `merge`); // [ 2, 5, 9, 20 ]
  * ```
- * 
+ *
  * By default uses in-built semantics for comparison. But a function can be provided.
  * Return 0 if `a` and `b` are equal, above 0 if `a` is considered higher than `b` or below zero if `b` is considered higher than `a`.
- * 
+ *
  * In the below example, the default sorting semantics are reversed:
- * ```js 
+ * ```js
  * const reverse = (a, b) => {
  *   if (a === b) return 0;
- *   if (a > b) return -1; 
+ *   if (a > b) return -1;
  *   if (a < b) return 1;
  *   return 0; // equal
  * }
  * sort(data, reverse); // [ 20, 9, 5, 2 ]
  * ```
- * @param data 
- * @param algo 
- * @param comparer 
+ * @param data
+ * @param algo
+ * @param comparer
  * @returns Sorted array
  */
-export const sort = <T>(data: T[] | readonly T[], algo: SortAlgorithm = `default`, comparer: Comparer<T> = defaultComparer): T[] => {
+export function sort<T>(data: T[] | readonly T[], algo: SortAlgorithm = `default`, comparer: Comparer<T> = defaultComparer): T[] {
   switch (algo) {
     case `merge`:
       return sortMerge(data, comparer);
-    case "default":
+    case `default`:
       return data.toSorted(comparer);
   }
 }
--- packages/collections/tests/sorted-array.test.ts
+++ packages/collections/tests/sorted-array.test.ts
@@ -1,42 +1,43 @@
-import { test, expect } from 'vitest';
-import * as Sorted from '../src/sorted-array.js';
 import { defaultComparer } from '@ixfx/core';
+import { expect, it } from 'vitest';
+import * as Sorted from '../src/sorted-array.js';
 
-test(`index-of`, () => {
-  const array1 = [ 1, 2, 2.5, 3, 4, 5 ];
-  expect(Sorted.indexOf(array1, 1)).toBe(0);
-  expect(Sorted.indexOf(array1, 3)).toBe(3);
-  expect(Sorted.indexOf(array1, 5)).toBe(5);
-  expect(Sorted.indexOf(array1, 0)).toBe(-1);
-  expect(Sorted.indexOf(array1, 3.5)).toBe(-1);
-  expect(Sorted.indexOf(array1, 6)).toBe(-1);
-  expect(Sorted.indexOf(array1, 4)).toEqual(4);
+it(`index-of`, () => {
+  const a1 = [1, 2, 2.5, 3, 4, 5];
+  expect(Sorted.indexOf(a1, 1)).toBe(0);
+  expect(Sorted.indexOf(a1, 3)).toBe(3);
+  expect(Sorted.indexOf(a1, 5)).toBe(5);
+  expect(Sorted.indexOf(a1, 0)).toBe(-1);
+  expect(Sorted.indexOf(a1, 3.5)).toBe(-1);
+  expect(Sorted.indexOf(a1, 6)).toBe(-1);
+  expect(Sorted.indexOf(a1, 4)).toEqual(4);
 
+  const a2 = [1, 2, 3, 3, 4, 5];
+  expect(Sorted.indexOf(a2, 3)).toBe(2);
 });
 
-test(`wrap`, () => {
-  const w = Sorted.wrapSorted([ 1, 2, 3, 4, 5, 6 ]);
+it(`wrap`, () => {
+  const w = Sorted.wrapSorted([1, 2, 3, 4, 5, 6]);
   expect(w.at(0)).toEqual(1);
   expect(w.at(-1)).toEqual(6);
-  expect(w.data).toEqual([ 1, 2, 3, 4, 5, 6 ]);
-  expect(w.toArray()).toEqual([ 1, 2, 3, 4, 5, 6 ]);
+  expect(w.data).toEqual([1, 2, 3, 4, 5, 6]);
+  expect(w.toArray()).toEqual([1, 2, 3, 4, 5, 6]);
   expect(w.indexOf(4)).toEqual(3);
   expect(w.length).toEqual(6);
   expect(w.insertionIndex(2.5)).toEqual(2);
 
   const w2 = w.insert(2.5);
-  expect(w.toArray()).toEqual([ 1, 2, 3, 4, 5, 6 ]); // immutability
-  expect(w2.toArray()).toEqual([ 1, 2, 2.5, 3, 4, 5, 6 ]);
+  expect(w.toArray()).toEqual([1, 2, 3, 4, 5, 6]); // immutability
+  expect(w2.toArray()).toEqual([1, 2, 2.5, 3, 4, 5, 6]);
 
   const w3 = w2.remove(4);
-  expect(w.toArray()).toEqual([ 1, 2, 3, 4, 5, 6 ]); // immutability
-  expect(w2.toArray()).toEqual([ 1, 2, 2.5, 3, 4, 5, 6 ]); // immutability
-  expect(w3.toArray()).toEqual([ 1, 2, 2.5, 3, 5, 6 ]);
-
+  expect(w.toArray()).toEqual([1, 2, 3, 4, 5, 6]); // immutability
+  expect(w2.toArray()).toEqual([1, 2, 2.5, 3, 4, 5, 6]); // immutability
+  expect(w3.toArray()).toEqual([1, 2, 2.5, 3, 5, 6]);
 });
 
-test(`insertionIndex`, () => {
-  const array1 = [ 1, 2, 3, 4, 5 ];
+it(`insertionIndex`, () => {
+  const array1 = [1, 2, 3, 4, 5];
   expect(Sorted.insertionIndex(array1, 0)).toBe(0);
   expect(Sorted.insertionIndex(array1, -10)).toBe(0);
 
@@ -45,60 +46,128 @@ test(`insertionIndex`, () => {
   expect(Sorted.insertionIndex(array1, 4)).toBe(4);
 
   expect(Sorted.insertionIndex(array1, 10)).toBe(5);
+
+  const array2 = [1, 2, 3, 4];
+  expect(Sorted.insertionIndex(array2, 0)).toBe(0);
+  expect(Sorted.insertionIndex(array2, 1)).toBe(1);
+  expect(Sorted.insertionIndex(array2, 1.5)).toBe(1);
+  expect(Sorted.insertionIndex(array2, 2)).toBe(2);
+  expect(Sorted.insertionIndex(array2, 3)).toBe(3);
+  expect(Sorted.insertionIndex(array2, 3.5)).toBe(3);
+  expect(Sorted.insertionIndex(array2, 4)).toBe(4);
+  expect(Sorted.insertionIndex(array2, 4.5)).toBe(4);
+  expect(Sorted.insertionIndex(array2, 5)).toBe(4);
+
+  const array3 = [2, 8, 16, 32];
+  expect(Sorted.insertionIndex(array3, 0)).toBe(0);
+  expect(Sorted.insertionIndex(array3, 1)).toBe(0);
+  expect(Sorted.insertionIndex(array3, 2)).toBe(1);
+  expect(Sorted.insertionIndex(array3, 3)).toBe(1);
+  expect(Sorted.insertionIndex(array3, 8)).toBe(2);
+  expect(Sorted.insertionIndex(array3, 9)).toBe(2);
+  expect(Sorted.insertionIndex(array3, 32)).toBe(4);
+  expect(Sorted.insertionIndex(array3, 320)).toBe(4);
 });
 
-test(`insert`, () => {
-  const array1 = [ 1, 2, 3, 4, 5 ];
-  expect(Sorted.insert(array1, 0)).toEqual([ 0, 1, 2, 3, 4, 5 ]);
-  expect(Sorted.insert(array1, 1.5)).toEqual([ 1, 1.5, 2, 3, 4, 5 ]);
-  expect(Sorted.insert(array1, 3)).toEqual([ 1, 2, 3, 3, 4, 5 ]);
-  expect(Sorted.insert(array1, -10)).toEqual([ -10, 1, 2, 3, 4, 5 ]);
-  expect(Sorted.insert(array1, 6)).toEqual([ 1, 2, 3, 4, 5, 6 ]);
-  expect(Sorted.insert(array1, 50)).toEqual([ 1, 2, 3, 4, 5, 50 ]);
+it(`lowerBound`, () => {
+  const array2 = [2, 8, 16, 32];
+  expect(Sorted.lowerBound(array2, 0)).toBe(0);
+  expect(Sorted.lowerBound(array2, 1)).toBe(0);
+  expect(Sorted.lowerBound(array2, 2)).toBe(0);
+  expect(Sorted.lowerBound(array2, 3)).toBe(1);
+  expect(Sorted.lowerBound(array2, 8)).toBe(1);
+  expect(Sorted.lowerBound(array2, 9)).toBe(2);
+  expect(Sorted.lowerBound(array2, 32)).toBe(3);
+  expect(Sorted.lowerBound(array2, 320)).toBe(4);
 });
 
-test(`remove`, () => {
-  const array1 = [ 1, 2, 3, 4, 5 ];
-  expect(Sorted.remove(array1, 3)).toEqual([ 1, 2, 4, 5 ]);
-  expect(Sorted.remove(array1, 0)).toEqual([ 1, 2, 3, 4, 5 ]);
-  expect(Sorted.remove(array1, 1)).toEqual([ 2, 3, 4, 5 ]);
-  expect(Sorted.remove(array1, 5)).toEqual([ 1, 2, 3, 4 ]);
-  expect(Sorted.remove(array1, 3.5)).toEqual([ 1, 2, 3, 4, 5 ]);
-  expect(Sorted.remove(array1, 50)).toEqual([ 1, 2, 3, 4, 5 ]);
+it(`upperBound`, () => {
+  const array2 = [2, 8, 16, 32];
+  expect(Sorted.upperBound(array2, 0)).toBe(0);
+  expect(Sorted.upperBound(array2, 1)).toBe(0);
+  expect(Sorted.upperBound(array2, 2)).toBe(1);
+  expect(Sorted.upperBound(array2, 3)).toBe(1);
+  expect(Sorted.upperBound(array2, 8)).toBe(2);
+  expect(Sorted.upperBound(array2, 9)).toBe(2);
+  expect(Sorted.upperBound(array2, 32)).toBe(4);
+  expect(Sorted.upperBound(array2, 320)).toBe(4);
+});
 
-  const array2 = [ 3 ];
-  expect(Sorted.remove(array2, 0)).toEqual([ 3 ]);
+it(`matchingRange`, () => {
+  const a1 = [2, 8, 16, 32];
+  expect(Sorted.matchingRange(a1, 0)).toBeUndefined();
+  expect(Sorted.matchingRange(a1, 1)).toBeUndefined();
+  expect(Sorted.matchingRange(a1, 2)).toEqual({ startIndex: 0, endIndex: 0 });
+  expect(Sorted.matchingRange(a1, 3)).toBeUndefined();
+  expect(Sorted.matchingRange(a1, 8)).toEqual({ startIndex: 1, endIndex: 1 });
+  expect(Sorted.matchingRange(a1, 9)).toBeUndefined();
+  expect(Sorted.matchingRange(a1, 32)).toEqual({ startIndex: 3, endIndex: 3 });
+  expect(Sorted.matchingRange(a1, 320)).toBeUndefined();
+
+  const a2 = [2, 8, 8, 8, 16, 32];
+  expect(Sorted.matchingRange(a2, 8)).toEqual({ startIndex: 1, endIndex: 3 });
+
+  const a3 = [2, 8, 16, 32, 32, 32];
+  expect(Sorted.matchingRange(a3, 32)).toEqual({ startIndex: 3, endIndex: 5 });
+
+  const a4 = [2, 2, 2, 8, 16, 32];
+  expect(Sorted.matchingRange(a4, 2)).toEqual({ startIndex: 0, endIndex: 2 });
+});
+
+it(`insert`, () => {
+  const array1 = [1, 2, 3, 4, 5];
+  expect(Sorted.insert(array1, 0)).toEqual([0, 1, 2, 3, 4, 5]);
+  expect(Sorted.insert(array1, 1.5)).toEqual([1, 1.5, 2, 3, 4, 5]);
+  expect(Sorted.insert(array1, 3)).toEqual([1, 2, 3, 3, 4, 5]);
+  expect(Sorted.insert(array1, -10)).toEqual([-10, 1, 2, 3, 4, 5]);
+  expect(Sorted.insert(array1, 6)).toEqual([1, 2, 3, 4, 5, 6]);
+  expect(Sorted.insert(array1, 50)).toEqual([1, 2, 3, 4, 5, 50]);
+});
+
+it(`remove`, () => {
+  const array1 = [1, 2, 3, 4, 5];
+  expect(Sorted.remove(array1, 3)).toEqual([1, 2, 4, 5]);
+  expect(Sorted.remove(array1, 0)).toEqual([1, 2, 3, 4, 5]);
+  expect(Sorted.remove(array1, 1)).toEqual([2, 3, 4, 5]);
+  expect(Sorted.remove(array1, 5)).toEqual([1, 2, 3, 4]);
+  expect(Sorted.remove(array1, 3.5)).toEqual([1, 2, 3, 4, 5]);
+  expect(Sorted.remove(array1, 50)).toEqual([1, 2, 3, 4, 5]);
+
+  const array2 = [3];
+  expect(Sorted.remove(array2, 0)).toEqual([3]);
   expect(Sorted.remove(array2, 3)).toEqual([]);
 
   expect(defaultComparer(2.5, 2)).toEqual(1);
   expect(defaultComparer(2, 2)).toEqual(0);
   expect(defaultComparer(2, 2.5)).toEqual(-1);
 
-  expect(Sorted.remove([ 1, 2, 2.5, 3, 4, 5, 6 ], 4)).toEqual([ 1, 2, 2.5, 3, 5, 6 ])
+  expect(Sorted.remove([1, 2, 2.5, 3, 4, 5, 6], 4)).toEqual([1, 2, 2.5, 3, 5, 6]);
 
+  const a3 = [1, 2, 3, 3, 3, 5, 6];
+  expect(Sorted.remove(a3, 3)).toEqual([1, 2, 5, 6]);
 });
 
-test(`merge`, () => {
-  const a = [ 4, 7, 10 ]
-  const b = [ 1, 2, 9, 11 ]
-  expect(Sorted.merge(a, b)).toEqual([ 1, 2, 4, 7, 9, 10, 11 ]);
+it(`merge`, () => {
+  const a = [4, 7, 10];
+  const b = [1, 2, 9, 11];
+  expect(Sorted.merge(a, b)).toEqual([1, 2, 4, 7, 9, 10, 11]);
 
-  expect(Sorted.merge([ 1, 2, 3 ], [ 4, 5, 6 ])).toEqual([ 1, 2, 3, 4, 5, 6 ]);
-  expect(Sorted.merge([ 4, 5, 6 ], [ 1, 2, 3 ])).toEqual([ 1, 2, 3, 4, 5, 6 ]);
-  expect(Sorted.merge([ 1, 2, 3 ], [ 1, 2, 3 ])).toEqual([ 1, 1, 2, 2, 3, 3 ]);
+  expect(Sorted.merge([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+  expect(Sorted.merge([4, 5, 6], [1, 2, 3])).toEqual([1, 2, 3, 4, 5, 6]);
+  expect(Sorted.merge([1, 2, 3], [1, 2, 3])).toEqual([1, 1, 2, 2, 3, 3]);
 });
 
-test(`sort-merge`, () => {
-  expect(Sorted.sort([ 10, 2, 5, 9 ])).toEqual([ 2, 5, 9, 10 ]);
-  expect(Sorted.sort([ 10, 2, 5, 9 ], `merge`)).toEqual([ 2, 5, 9, 10 ]);
+it(`sort-merge`, () => {
+  expect(Sorted.sort([10, 2, 5, 9])).toEqual([2, 5, 9, 10]);
+  expect(Sorted.sort([10, 2, 5, 9], `merge`)).toEqual([2, 5, 9, 10]);
 
   const rev = (a: number, b: number): number => {
-    if (a === b) return 0;
-    if (a > b) return -1;
+    if (a === b)
+      return 0;
+    if (a > b)
+      return -1;
     return 1;
-  }
-  expect(Sorted.sort([ 10, 2, 5, 9 ], `default`, rev)).toEqual([ 10, 9, 5, 2 ]);
-  expect(Sorted.sort([ 10, 2, 5, 9 ], `merge`, rev)).toEqual([ 10, 9, 5, 2 ]);
-
-
+  };
+  expect(Sorted.sort([10, 2, 5, 9], `default`, rev)).toEqual([10, 9, 5, 2]);
+  expect(Sorted.sort([10, 2, 5, 9], `merge`, rev)).toEqual([10, 9, 5, 2]);
 });
