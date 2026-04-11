e61198112169bcc2c67168bdca5da674f4e8f86d
CH <clint@thestaticvoid.net>
2026-04-11 16:46:39 +0200

    Update circular-array.test.ts

--- packages/collections/tests/circular-array.test.ts
+++ packages/collections/tests/circular-array.test.ts
@@ -1,7 +1,7 @@
-import { assert, describe, test, expect, it } from "vitest";
+import { expect, it } from "vitest";
 import { CircularArray } from '../src/circular-array.js';
 
-test(`circularArray`, () => {
+it(`circularArray`, () => {
   const ca1 = new CircularArray<string>(5);
   const ca2 = ca1.add(`a`);
   const ca3 = ca2.add(`b`);
@@ -22,8 +22,8 @@ test(`circularArray`, () => {
   expect(ca4 !== ca5).toBe(true);
   expect(ca5 !== ca6).toBe(true);
 
-  //t.like(ca6 as string[], [ `a`, `b`, `c`, `d`, `e` ]);
-  expect(ca6 as string[]).toEqual([ `a`, `b`, `c`, `d`, `e` ]);
+  // t.like(ca6 as string[], [ `a`, `b`, `c`, `d`, `e` ]);
+  expect(ca6 as string[]).toEqual([`a`, `b`, `c`, `d`, `e`]);
 
   expect(ca1.isFull).toBe(false);
   expect(ca2.isFull).toBe(false);
@@ -39,12 +39,10 @@ test(`circularArray`, () => {
   expect(ca5.pointer === 4).toBe(true);
   expect(ca6.pointer === 0).toBe(true);
 
-
   const ca7 = ca6.add(`f`);
   expect(ca7.length === 5).toBe(true);
   expect(ca7.pointer === 1).toBe(true);
 
-  expect(ca6 as string[]).toEqual([ `a`, `b`, `c`, `d`, `e` ]);
-  expect(ca7 as string[]).toEqual([ `f`, `b`, `c`, `d`, `e` ]);
-
+  expect(ca6 as string[]).toEqual([`a`, `b`, `c`, `d`, `e`]);
+  expect(ca7 as string[]).toEqual([`f`, `b`, `c`, `d`, `e`]);
 });
