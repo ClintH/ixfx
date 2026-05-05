0f9a6aed235ac93aa6bba47fb37303ce14053847
CH <clint@thestaticvoid.net>
2026-05-05 12:50:27 +0200

    Added 'findIndex' and 'findIndexReverse' both with start/end params. Added filterWithIn dex

--- packages/arrays/src/filter.ts
+++ packages/arrays/src/filter.ts
@@ -1,14 +1,96 @@
-
 import { arrayIndexTest, arrayTest, resultThrow } from "@ixfx/guards";
 
+/**
+ * Like Array.findIndex but with optional `startAt` and `length` parameters to limit the search to a specific section of the array.
+ *
+ * ```js
+ * const data = ["red","blue","red","blue"]
+ * data.findIndex(v => v === `red`); // 0 - finds first match
+ * findIndex(data, v => v === `red`, 1); // 2 - finds first match after start index of 1
+ * ```
+ *
+ * Use {@link findIndexReverse} to search backwards through the array.
+ * @param array
+ * @param predicate
+ * @param startInclusive
+ * @param endExclusive End index (exclusive). By default, uses array.length
+ */
+export function findIndex<V>(array: readonly V[], predicate: (value: V, index: number, obj: readonly V[]) => boolean, startInclusive?: number, endExclusive?: number): number {
+  const _start = startInclusive ?? 0;
+  const _end = endExclusive ?? array.length;
 
+  if (_start >= array.length)
+    throw new RangeError(`Start ${_start} is out of bounds for array of length ${array.length}`);
+  if (_end > array.length)
+    throw new RangeError(`End ${_end} is out of bounds for array of length ${array.length}`);
+  if (_start > _end)
+    throw new RangeError(`Start ${_start} is greater than end ${_end}`);
+
+  for (let i = _start; i < _end; i++) {
+    if (predicate(array[i], i, array)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+/**
+ * Returns a matching index, starting at index `start` and working backwards up until `end` (both inclusive).
+ * ```
+ * const data = ["red","blue","red","blue","red"];
+ * findIndexReverse(data, v=> v === `red`);     // 4
+ * findIndexReverse(data, v=> v === `red`, 3);  // 2
+ * findIndexReverse(data, v=> v === `red`, 2);  // 2
+ * ```
+ * @param array
+ * @param predicate
+ * @param startInclusive
+ * @param endInclusive
+ * @returns
+ */
+export function findIndexReverse<V>(array: readonly V[], predicate: (value: V, index: number, obj: readonly V[]) => boolean, startInclusive?: number, endInclusive?: number): number {
+  const _start = startInclusive ?? array.length - 1;
+  const _end = endInclusive ?? 0;
+  if (_start >= array.length)
+    throw new RangeError(`Start ${_start} is out of bounds for array of length ${array.length}`);
+  if (_end < 0)
+    throw new RangeError(`End ${_end} is out of bounds`);
+  if (_start < _end)
+    throw new RangeError(`Start ${_start} is less than end ${_end}`);
+  for (let i = _start; i >= _end; i--) {
+    if (predicate(array[i], i, array)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+/**
+ * Enumerates the index of all array values that match `predicate`.
+ *
+ * ```js
+ * const data =  [`red`,`blue`,`red`,`blue`,`red`];
+ * for (const index of filterWithIndex(data, v=> v === `red`)) {
+ *  // Yields 0, 2, 4
+ * }
+ * ```
+ * @param array
+ * @param predicate
+ */
+export function *filterWithIndex<V>(array: readonly V[], predicate: (value: V, index: number, obj: readonly V[]) => boolean): Generator<number> {
+  for (let i = 0; i < array.length; i++) {
+    if (predicate(array[i], i, array)) {
+      yield i;
+    }
+  }
+}
 
 /**
  * Returns two separate arrays of everything that `filter` returns _true_,
- * and everything it returns _false_ on. 
- * 
+ * and everything it returns _false_ on.
+ *
  * Same idea as the in-built Array.filter, but that only returns values for one case.
- * 
+ *
  * ```js
  * const [ matching, nonMatching ] = filterAB(data, v => v.enabled);
  * // `matching` is a list of items from `data` where .enabled is true
@@ -18,18 +100,16 @@ import { arrayIndexTest, arrayTest, resultThrow } from "@ixfx/guards";
  * @param filter Function which returns _true_ to add items to the A list, or _false_ for items to add to the B list
  * @returns Array of two elements. The first is items that match `filter`, the second is items that do not.
  */
-export const filterAB = <V>(
-  data: readonly V[],
-  filter: (a: V) => boolean
-): [ a: V[], b: V[] ] => {
+export function filterAB<V>(data: readonly V[], filter: (a: V) => boolean): [ a: V[], b: V[] ] {
   const a: V[] = [];
   const b: V[] = [];
   for (const datum of data) {
-    if (filter(datum)) a.push(datum);
+    if (filter(datum))
+      a.push(datum);
     else b.push(datum);
   }
-  return [ a, b ];
-};
+  return [a, b];
+}
 
 /**
  * Yields elements from `array` that match a given `predicate`, and moreover are between
@@ -48,25 +128,27 @@ export const filterAB = <V>(
  * @param startIndex Start index (defaults to 0)
  * @param endIndex End index (by default runs until end)
  */
-export function* filterBetween<V>(
+export function *filterBetween<V>(
   array: readonly V[] | V[],
   predicate: (
     value: V,
     index: number,
-    array: readonly V[] | V[]
+    array: readonly V[] | V[],
   ) => boolean,
   startIndex?: number,
-  endIndex?: number
+  endIndex?: number,
 ): Generator<V> {
   resultThrow(arrayTest(array, `array`));
-  if (typeof startIndex === `undefined`) startIndex = 0;
-  if (typeof endIndex === `undefined`) endIndex = array.length; //- 1;
+  if (typeof startIndex === `undefined`)
+    startIndex = 0;
+  if (typeof endIndex === `undefined`)
+    endIndex = array.length; // - 1;
 
   resultThrow(arrayIndexTest(array, startIndex, `startIndex`));
   resultThrow(arrayIndexTest(array, endIndex - 1, `endIndex`));
 
   for (let index = startIndex; index < endIndex; index++) {
-    if (predicate(array[ index ], index, array)) yield array[ index ];//t.push(array[ index ]);
+    if (predicate(array[index], index, array))
+      yield array[index];// t.push(array[ index ]);
   }
 };
-
--- packages/arrays/tests/filter.test.ts
+++ packages/arrays/tests/filter.test.ts
@@ -1,11 +1,47 @@
-import { test, expect } from 'vitest';
+import { expect, it } from 'vitest';
 import * as Arrays from '../src/index.js';
 
-test(`filter`, () => {
+it(`filter`, () => {
   const numbers = `1 2 3 4 5 6 7 8 9 10`.split(` `).map(v => Number.parseInt(v));
 
-  const [ a, b ] = Arrays.filterAB(numbers, v => v % 2 === 0);
+  const [a, b] = Arrays.filterAB(numbers, v => v % 2 === 0);
 
   expect(a.join(` `)).toEqual(`2 4 6 8 10`);
   expect(b.join(` `)).toEqual(`1 3 5 7 9`);
-})
+});
+
+it(`find-index`, () => {
+  //              0      1       2      3      4
+  const data = [`red`, `blue`, `red`, `blue`, `red`];
+  expect(Arrays.findIndex(data, v => v === `red`)).toBe(0);
+  expect(Arrays.findIndex(data, v => v === `red`, 1)).toBe(2);
+  expect(Arrays.findIndex(data, v => v === `red`, 2)).toBe(2);
+  expect(Arrays.findIndex(data, v => v === `red`, 3)).toBe(4);
+  expect(Arrays.findIndex(data, v => v === `red`, 4)).toBe(4);
+  expect(() => Arrays.findIndex(data, v => v === `red`, 5)).toThrow();
+  expect(() => Arrays.findIndex(data, v => v === `red`, 5, 2)).toThrow();
+
+  //              0    1    2    3    4
+  const data2 = [`a`, `b`, `c`, `d`, `e`];
+  expect(Arrays.findIndex(data2, v => v === `e`, 1, 1)).toBe(-1);
+  expect(Arrays.findIndex(data2, v => v === `e`, 1, 2)).toBe(-1);
+  expect(Arrays.findIndex(data2, v => v === `e`, 1, 5)).toBe(4);
+});
+
+it(`find-index-reverse`, () => {
+  //              0      1       2      3      4
+  const data = [`red`, `blue`, `red`, `blue`, `red`];
+  expect(Arrays.findIndexReverse(data, v => v === `red`)).toBe(4);
+  expect(Arrays.findIndexReverse(data, v => v === `red`, 1)).toBe(0);
+  expect(Arrays.findIndexReverse(data, v => v === `red`, 2)).toBe(2);
+  expect(Arrays.findIndexReverse(data, v => v === `red`, 3)).toBe(2);
+  expect(Arrays.findIndexReverse(data, v => v === `red`, 4)).toBe(4);
+
+  //              0    1    2    3    4
+  const data2 = [`a`, `b`, `c`, `d`, `e`];
+  expect(Arrays.findIndexReverse(data2, v => v === `a`)).toBe(0);
+  expect(Arrays.findIndexReverse(data2, v => v === `a`, data2.length - 1)).toBe(0);
+
+  expect(Arrays.findIndexReverse(data2, v => v === `a`, data2.length - 1, 1)).toBe(-1);
+  expect(Arrays.findIndexReverse(data2, v => v === `a`, data2.length - 1, 0)).toBe(0);
+});
