3a7d47a9f705897568aa29fa99be03fd2f3314f4
CH <clint@thestaticvoid.net>
2026-05-27 11:23:02 +0200

    Linting

--- packages/geometry/src/line/reverse.ts
+++ packages/geometry/src/line/reverse.ts
@@ -1,5 +1,6 @@
-import { guard } from "./guard.js";
 import type { Line } from "./line-type.js";
+import { resultThrow } from "@ixfx/guards";
+import { lineTest } from "./guard.js";
 
 /**
  * Reverses a line.
@@ -9,10 +10,10 @@ import type { Line } from "./line-type.js";
  * const line = reverse({ a, b });
  * // { a: { x: 100, y: 200 }, b: { x: 10, y: 20 } }
  * ```
- * @param line 
- * @returns 
+ * @param line
+ * @returns
  */
 export function reverse(line: Line): Line {
-  guard(line, `line`);
+  resultThrow(lineTest(line, `line`));
   return { a: line.b, b: line.a };
 }
