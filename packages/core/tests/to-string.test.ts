a26d8782e6f9cc7feb394ab80d65cb841be3c5cf
CH <clint@thestaticvoid.net>
2026-05-27 11:22:31 +0200

    Test for toStringOrdered

--- packages/core/tests/to-string.test.ts
+++ packages/core/tests/to-string.test.ts
@@ -1,24 +1,30 @@
-import { test, expect } from 'vitest';
-import { toStringDefault } from '../src/to-string.js';
+import { describe, expect, it } from 'vitest';
+import { toStringDefault, toStringOrdered } from '../src/to-string.js';
 
-test(`toStringDefault`, () => {
+describe(`toStringDefault`, () => {
   const a = {
-    name: "Blah blah",
+    name: `Blah blah`,
     age: 30,
     alive: true,
-    height: 192.4
-  }
+    height: 192.4,
+  };
 
   const aa = {
-    name: "Blah blah",
+    name: `Blah blah`,
     age: 30,
     alive: true,
-    height: 192.4
-  }
+    height: 192.4,
+  };
+  it(`default`, () => {
+    const b = `Blah blah`;
+    const bb = `Blah blah`;
 
-  const b = "Blah blah";
-  const bb = "Blah blah";
+    expect(toStringDefault(a)).toBe(toStringDefault(aa));
+    expect(toStringDefault(b)).toBe(toStringDefault(bb));
+  });
 
-  expect(toStringDefault(a)).toBe(toStringDefault(aa));
-  expect(toStringDefault(b)).toBe(toStringDefault(bb));
-})
+  it(`ordered`, () => {
+    expect(toStringOrdered(a)).toBe(toStringOrdered(`{"age":30,"alive":true,"height":192.4,"name":"Blah blah"}`));
+    expect(toStringOrdered(aa)).toBe(toStringOrdered(`{"age":30,"alive":true,"height":192.4,"name":"Blah blah"}`));
+  });
+});
