1af515bc0a3bd3f6330ef6344a4f6e11bba0d533
CH <clint@thestaticvoid.net>
2026-05-27 11:21:04 +0200

    Use Result instead of exception

--- packages/geometry/src/point/guard.ts
+++ packages/geometry/src/point/guard.ts
@@ -1,14 +1,16 @@
+import type { Result } from '@ixfx/guards';
 import type { Point, Point3d } from "./point-type.js";
-import { errorResult, numberTest, resultThrow, type Result } from '@ixfx/guards';
+import { errorResult, numberTest, resultThrow } from '@ixfx/guards';
 
 /**
  * Returns true if xy (and z, if present) are _null_.
  * @param p
- * @returns
+ * @returns True if all props are null
  */
-export const isNull = (p: Point): boolean => {
+export function isNull(p: Point): boolean {
   if (isPoint3d(p)) {
-    if (p.z !== null) return false;
+    if (p.z !== null)
+      return false;
   }
   return p.x === null && p.y === null;
 }
@@ -16,53 +18,66 @@ export const isNull = (p: Point): boolean => {
 /***
  * Returns true if either x, y, z isNaN.
  */
-export const isNaN = (p: Point): boolean => {
+export function isNaN(p: Point): boolean {
   if (isPoint3d(p)) {
-    if (!Number.isNaN(p.z)) return false;
+    if (!Number.isNaN(p.z))
+      return false;
   }
-  return Number.isNaN(p.x) || Number.isNaN(p.y)
+  return Number.isNaN(p.x) || Number.isNaN(p.y);
 }
 
-export function test(p: Point, name = `Point`, extraInfo = ``): Result<Point, string> {
+export function pointTest(p: Point, name = `Point`, extraInfo = ``): Result<Point, string> {
   if (p === undefined) {
-    return errorResult(`'${ name }' is undefined. Expected {x,y} got ${ JSON.stringify(p) }`, extraInfo);
+    return errorResult(`'${name}' is undefined. Expected {x,y} got ${JSON.stringify(p)}`, extraInfo);
   }
   if (p === null) {
     return errorResult(
-      `'${ name }' is null. Expected {x,y} got ${ JSON.stringify(p) }`, extraInfo
+      `'${name}' is null. Expected {x,y} got ${JSON.stringify(p)}`,
+      extraInfo,
     );
   }
-  if (typeof p !== `object`) return errorResult(
-    `'${ name }' is type '${ typeof p }'. Expected object.`, extraInfo
-  );
+  if (typeof p !== `object`) {
+    return errorResult(
+      `'${name}' is type '${typeof p}'. Expected object.`,
+      extraInfo,
+    );
+  }
   if (p.x === undefined) {
     return errorResult(
-      `'${ name }.x' is undefined. Expected {x,y} got ${ JSON.stringify(p) }`, extraInfo
+      `'${name}.x' is undefined. Expected {x,y} got ${JSON.stringify(p)}`,
+      extraInfo,
     );
   }
   if (p.y === undefined) {
     return errorResult(
-      `'${ name }.y' is undefined. Expected {x,y} got ${ JSON.stringify(p) }`, extraInfo
+      `'${name}.y' is undefined. Expected {x,y} got ${JSON.stringify(p)}`,
+      extraInfo,
     );
   }
   if (typeof p.x !== `number`) {
-    return errorResult(`'${ name }.x' must be a number. Got ${ typeof p.x }`, extraInfo);
+    return errorResult(`'${name}.x' must be a number. Got ${typeof p.x}`, extraInfo);
   }
   if (typeof p.y !== `number`) {
-    return errorResult(`'${ name }.y' must be a number. Got ${ typeof p.y }`, extraInfo);
+    return errorResult(`'${name}.y' must be a number. Got ${typeof p.y}`, extraInfo);
   }
   if (p.z !== undefined) {
-    if (typeof p.z !== `number`) return errorResult(`${ name }.z must be a number. Got: ${ typeof p.z }`, extraInfo)
-    if (Number.isNaN(p.z)) return errorResult(`'${ name }.z' is NaN. Got: ${ JSON.stringify(p) }`, extraInfo);
+    if (typeof p.z !== `number`)
+      return errorResult(`${name}.z must be a number. Got: ${typeof p.z}`, extraInfo);
+    if (Number.isNaN(p.z))
+      return errorResult(`'${name}.z' is NaN. Got: ${JSON.stringify(p)}`, extraInfo);
   }
 
-  if (p.x === null) return errorResult(`'${ name }.x' is null`, extraInfo);
-  if (p.y === null) return errorResult(`'${ name }.y' is null`, extraInfo);
+  if (p.x === null)
+    return errorResult(`'${name}.x' is null`, extraInfo);
+  if (p.y === null)
+    return errorResult(`'${name}.y' is null`, extraInfo);
 
-  if (Number.isNaN(p.x)) return errorResult(`'${ name }.x' is NaN`, extraInfo);
-  if (Number.isNaN(p.y)) return errorResult(`'${ name }.y' is NaN`, extraInfo);
+  if (Number.isNaN(p.x))
+    return errorResult(`'${name}.x' is NaN`, extraInfo);
+  if (Number.isNaN(p.y))
+    return errorResult(`'${name}.y' is NaN`, extraInfo);
 
-  return { success: true, value: p }
+  return { success: true, value: p };
 }
 
 /**
@@ -71,7 +86,7 @@ export function test(p: Point, name = `Point`, extraInfo = ``): Result<Point, st
  * @param name
  */
 export function guard(p: Point, name = `Point`, info?: string): void {
-  resultThrow(test(p, name, info))
+  resultThrow(pointTest(p, name, info));
   // if (p === undefined) {
   //   throw new Error(
   //     `'${ name }' is undefined. Expected {x,y} got ${ JSON.stringify(p) }`
@@ -115,66 +130,73 @@ export function guard(p: Point, name = `Point`, info?: string): void {
 /**
  * Throws if parameter is not a valid point, or either x or y is 0
  * @param pt
- * @returns
+ * @returns Throws an error if not a valid point or zero.
  */
-export const guardNonZeroPoint = (pt: Point | Point3d, name = `pt`) => {
+export function guardNonZeroPoint(pt: Point | Point3d, name = `pt`) {
   guard(pt, name);
   resultThrow(
-    numberTest(pt.x, `nonZero`, `${ name }.x`),
-    numberTest(pt.y, `nonZero`, `${ name }.y`),
+    numberTest(pt.x, `nonZero`, `${name}.x`),
+    numberTest(pt.y, `nonZero`, `${name}.y`),
     () => {
       if (typeof pt.z !== `undefined`) {
-        return numberTest(pt.z, `nonZero`, `${ name }.z`);
+        return numberTest(pt.z, `nonZero`, `${name}.z`);
       }
-    }
+    },
   );
   return true;
-};
+}
 
 /**
  * Returns _true_ if `p` has x & y properties.
  * Returns _false_ if `p` is undefined, null or does not contain properties.
  * Use {@link isPoint3d} to check further check for `z`.
- * @param p 
- * @returns 
+ * @param p
+ * @returns True if `p` has x & y props
  */
-// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
 export function isPoint(p: number | unknown): p is Point {
-  if (p === undefined) return false;
-  if (p === null) return false;
-  if ((p as Point).x === undefined) return false;
-  if ((p as Point).y === undefined) return false;
+  if (p === undefined)
+    return false;
+  if (p === null)
+    return false;
+  if ((p as Point).x === undefined)
+    return false;
+  if ((p as Point).y === undefined)
+    return false;
   return true;
 }
 
 /**
  * Returns _true_ if `p` has x, y, & z properties.
  * Returns _false_ if `p` is undefined, null or does not contain properties.
- * @param p 
- * @returns 
+ * @param p
+ * @returns _True_ if `p` has x, y, & z props
  */
-// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-export const isPoint3d = (p: Point | unknown): p is Point3d => {
-  if (p === undefined) return false;
-  if (p === null) return false;
-  if ((p as Point3d).x === undefined) return false;
-  if ((p as Point3d).y === undefined) return false;
-  if ((p as Point3d).z === undefined) return false;
+export function isPoint3d(p: Point | unknown): p is Point3d {
+  if (p === undefined)
+    return false;
+  if (p === null)
+    return false;
+  if ((p as Point3d).x === undefined)
+    return false;
+  if ((p as Point3d).y === undefined)
+    return false;
+  if ((p as Point3d).z === undefined)
+    return false;
   return true;
-};
+}
 
 /**
  * Returns true if both xy (and z, if present) are 0.
  * Use `Points.Empty` to return an empty point.
  * @param p
- * @returns
+ * @returns _True_ is all props are 0
  */
-export const isEmpty = (p: Point): boolean => {
+export function isEmpty(p: Point): boolean {
   if (isPoint3d(p)) {
-    if (p.z !== 0) return false;
+    if (p.z !== 0)
+      return false;
   }
-  return p.x === 0 && p.y === 0
-
+  return p.x === 0 && p.y === 0;
 }
 
 /**
@@ -183,11 +205,12 @@ export const isEmpty = (p: Point): boolean => {
  *
  * Use Points.Placeholder to return a placeholder point.
  * @param p
- * @returns
+ * @returns True if all props are NaN
  */
-export const isPlaceholder = (p: Point): boolean => {
+export function isPlaceholder(p: Point): boolean {
   if (isPoint3d(p)) {
-    if (!Number.isNaN(p.z)) return false;
+    if (!Number.isNaN(p.z))
+      return false;
   }
   return Number.isNaN(p.x) && Number.isNaN(p.y);
 }
