63ef2771e96a2ac087adbcb4b9b1f1e7ec61222c
CH <clint@thestaticvoid.net>
2026-05-27 11:22:01 +0200

    Linting

--- packages/core/src/to-string.ts
+++ packages/core/src/to-string.ts
@@ -1,55 +1,59 @@
-
 // Via Vuejs
 
 import { removeCircularReferences } from "./records/circular.js";
 
-// eslint-disable-next-line @typescript-eslint/unbound-method
-const objectToString = Object.prototype.toString
-const toTypeString = (value: unknown): string =>
-  objectToString.call(value)
+const objectToString = Object.prototype.toString;
+function toTypeString(value: unknown): string {
+  return objectToString.call(value);
+}
 
 /**
  * Returns _true_ if `value` is a Map type
  * @param value
- * @returns 
+ * @returns
  */
-export const isMap = (value: unknown): value is Map<any, any> =>
-  toTypeString(value) === `[object Map]`
+export function isMap(value: unknown): value is Map<any, any> {
+  return toTypeString(value) === `[object Map]`;
+}
 
 /**
  * Returns _true_ if `value` is a Set type
- * @param value 
- * @returns 
+ * @param value
+ * @returns
  */
-export const isSet = (value: unknown): value is Set<any> =>
-  toTypeString(value) === `[object Set]`
+export function isSet(value: unknown): value is Set<any> {
+  return toTypeString(value) === `[object Set]`;
+}
 
 /**
  * A default converter to string that uses JSON.stringify if its an object, or the thing itself if it's a string
  */
-export const toStringDefault = <V>(itemToMakeStringFor: V): string =>
-  typeof itemToMakeStringFor === `string`
+export function toStringDefault<V>(itemToMakeStringFor: V): string {
+  return typeof itemToMakeStringFor === `string`
     ? itemToMakeStringFor
     : JSON.stringify(itemToMakeStringFor);
-
+}
 
 /**
  * Converts a value to string form.
  * For simple objects, .toString() is used, other JSON.stringify is used.
  * It is meant for creating debugging output or 'hash' versions of objects, and does
  * not necessarily maintain full fidelity of the input
- * @param value 
- * @returns 
+ * @param value
+ * @returns
  */
-export const defaultToString = (value: null | boolean | string | object): string => {
-  //ECMA specification: http://www.ecma-international.org/ecma-262/6.0/#sec-tostring
-  if (value === null) return `null`;
+export function defaultToString(value: null | boolean | string | object): string {
+  // ECMA specification: http://www.ecma-international.org/ecma-262/6.0/#sec-tostring
+  if (value === null)
+    return `null`;
   if (typeof value === `boolean` || typeof value === `number`) {
     return value.toString();
   }
 
-  if (typeof value === `string`) return value;
-  if (typeof value === `symbol`) throw new TypeError(`Symbol cannot be converted to string`);
+  if (typeof value === `string`)
+    return value;
+  if (typeof value === `symbol`)
+    throw new TypeError(`Symbol cannot be converted to string`);
   try {
     const s = JSON.stringify(value);
     return s;
@@ -61,4 +65,29 @@ export const defaultToString = (value: null | boolean | string | object): string
       throw error;
     }
   }
-};
+}
+
+/**
+ * If input is a string, it is returned.
+ * Otherwise, it returns the result of JSON.stringify() with fields ordered.
+ *
+ * This allows for more consistent comparisons when object field orders are different but values the same.
+ * @param itemToMakeStringFor
+ */
+export function toStringOrdered(itemToMakeStringFor: unknown): string {
+  if (typeof itemToMakeStringFor === `string`)
+    return itemToMakeStringFor;
+  // const allKeys = new Set<string>();
+  const replacer = (key: string, value: unknown) =>
+    value instanceof Object && !(Array.isArray(value))
+      ? Object.keys(value)
+          .sort()
+          .reduce((sorted, key) => {
+            (sorted as any)[key] = (value as any)[key];
+            return sorted;
+          }, {})
+      : value;
+
+  return JSON.stringify(itemToMakeStringFor, replacer);// (key: string, value: unknown) => (allKeys.add(key), value));
+  // return JSON.stringify(itemToMakeStringFor, [...allKeys.values()].toSorted());
+}
