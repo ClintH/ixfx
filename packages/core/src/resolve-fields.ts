17d679882bb0cd90882ecf62cdf17a4eb95b96d9
CH <clint@thestaticvoid.net>
2026-05-27 11:21:54 +0200

    Linting

--- packages/core/src/resolve-fields.ts
+++ packages/core/src/resolve-fields.ts
@@ -1,38 +1,38 @@
-import { resolve, resolveSync, type ReactiveNonInitial, type ResolveToValue } from "@ixfx/core";
+import type { ReactiveNonInitial, ResolveToValue } from "@ixfx/core";
+import { resolve, resolveSync } from "@ixfx/core";
 import { zip } from "./util/zip.js";
 
 /**
  * An object that can be 'resolved'.
  * @see {@link resolveFields}
  */
-export type ResolvedObject<T extends Record<string, ResolveToValue<any>>> =
-  { [ K in keyof T ]:
-    T[ K ] extends number ? number :
-    T[ K ] extends string ? string :
-    T[ K ] extends boolean ? boolean :
-    T[ K ] extends bigint ? bigint :
-    T[ K ] extends () => Promise<any> ? Awaited<ReturnType<T[ K ]>> :
-    T[ K ] extends () => any ? ReturnType<T[ K ]> :
-    T[ K ] extends ReactiveNonInitial<infer V> ? V :
-    T[ K ] extends Generator<infer V> ? V :
-    T[ K ] extends AsyncGenerator<infer V> ? V :
-    T[ K ] extends IterableIterator<infer V> ? V :
-    T[ K ] extends AsyncIterableIterator<infer V> ? V :
-    T[ K ] extends (infer V)[] ? V : // array needed?
-    T[ K ] extends object ? T[ K ] : never
+export type ResolvedObject<T extends Record<string, ResolveToValue<any>>>
+  = { [ K in keyof T ]:
+    T[K] extends number ? number
+      : T[K] extends string ? string
+        : T[K] extends boolean ? boolean
+          : T[K] extends bigint ? bigint
+            : T[K] extends () => Promise<any> ? Awaited<ReturnType<T[K]>>
+              : T[K] extends () => any ? ReturnType<T[K]>
+                : T[K] extends ReactiveNonInitial<infer V> ? V
+                  : T[K] extends Generator<infer V> ? V
+                    : T[K] extends AsyncGenerator<infer V> ? V
+                      : T[K] extends IterableIterator<infer V> ? V
+                        : T[K] extends AsyncIterableIterator<infer V> ? V
+                          : T[K] extends Array<infer V> ? V // array needed?
+                            : T[K] extends object ? T[K] : never
   };
-
 
 /**
  * Returns a copy of `object`, with the same properties. For each property
  * that has a basic value (string, number, boolean, object), the value is set
  * for the return object. If the property is a function or generator, its value
  * is used instead. Async functions and generators are also usable.
- * 
+ *
  * Use {@link resolveFieldsSync} for a synchronous version.
- * 
+ *
  * Not recursive.
- * 
+ *
  * In the below example, the function for the property `random` is invoked.
  * ```js
  * const state = {
@@ -42,12 +42,12 @@ export type ResolvedObject<T extends Record<string, ResolveToValue<any>>> =
  * const x = resolveFields(state);
  * // { length: 10, random: 0.1235 }
  * ```
- * 
+ *
  * It also works with generators. Probably best with those that are infinite.
- * 
+ *
  * ```js
  * import { count } from './numbers.js';
- * 
+ *
  * const state = {
  *  length: 10,
  *  index: count(2) // Generator that yields: 0, 1 and then ends
@@ -57,16 +57,16 @@ export type ResolvedObject<T extends Record<string, ResolveToValue<any>>> =
  * // Generator finishes after counting twice:
  * resolveFields(state); // { length: 10, index: undefined }
  * ```
- * @param object 
- * @returns 
+ * @param object
+ * @returns
  */
 export async function resolveFields<T extends Record<string, ResolveToValue<any>>>(object: T): Promise<ResolvedObject<T>> {
   const resolvers: any[] = [];
   const keys: string[] = [];
   for (const entry of Object.entries(object)) {
-    const resolvable = entry[ 1 ] as ResolveToValue<typeof entry[ 1 ]>;
+    const resolvable = entry[1] as ResolveToValue<typeof entry[1]>;
     resolvers.push(resolve(resolvable));
-    keys.push(entry[ 0 ]);
+    keys.push(entry[0]);
   }
   const results = await Promise.all(resolvers);
   const entries = zip(keys, results);
@@ -76,19 +76,18 @@ export async function resolveFields<T extends Record<string, ResolveToValue<any>
 /**
  * 'Resolves' all the fields of `object` in a synchronous manner.
  * Uses {@link resolveSync} under-the-hood
- * @param object 
- * @returns 
+ * @param object
+ * @returns
  */
 export function resolveFieldsSync<T extends Record<string, ResolveToValue<any>>>(object: T): ResolvedObject<T> {
-  const entries: [ key: string, value: any ][] = [];
+  const entries: Array<[ key: string, value: any ]> = [];
   for (const entry of Object.entries(object)) {
-    const resolvable = entry[ 1 ] as ResolveToValue<typeof entry[ 1 ]>;
+    const resolvable = entry[1] as ResolveToValue<typeof entry[1]>;
     const value = resolveSync(resolvable);
-    entries.push([ entry[ 0 ], value ]);
+    entries.push([entry[0], value]);
   }
   return Object.fromEntries(entries) as ResolvedObject<T>;
 }
-
 
 /**
  * Returns a function that resolves `object`.
