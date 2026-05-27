37af5e97560d0403351c4f7368f2272481b86f5a
CH <clint@thestaticvoid.net>
2026-05-27 11:19:48 +0200

    Use Result instead of exception

--- packages/geometry/src/line/guard.ts
+++ packages/geometry/src/line/guard.ts
@@ -1,5 +1,6 @@
-import { isPoint } from "../point/guard.js";
+import type { Result } from "@ixfx/guards";
 import type { Line, PolyLine } from "./line-type.js";
+import { isPoint } from "../point/guard.js";
 
 /**
  * Returns true if `p` is a valid line, containing `a` and `b` Points.
@@ -9,40 +10,50 @@ import type { Line, PolyLine } from "./line-type.js";
  * @param p Value to check
  * @returns True if a valid line.
  */
-export const isLine = (p: any): p is Line => {
-  if (p === undefined) return false;
-  if ((p as Line).a === undefined) return false;
-  if ((p as Line).b === undefined) return false;
-  if (!isPoint((p as Line).a)) return false;
-  if (!isPoint((p as Line).b)) return false;
+export function isLine(p: any): p is Line {
+  if (p === undefined)
+    return false;
+  if ((p as Line).a === undefined)
+    return false;
+  if ((p as Line).b === undefined)
+    return false;
+  if (!isPoint((p as Line).a))
+    return false;
+  if (!isPoint((p as Line).b))
+    return false;
   return true;
-};
+}
 
 /**
  * Returns true if `p` is a {@link PolyLine}, ie. an array of {@link Line}s.
  * Validates all items in array.
- * @param p 
+ * @param p
  * @returns
  */
 
-export const isPolyLine = (p: any): p is PolyLine => {
-  if (!Array.isArray(p)) return false;
+export function isPolyLine(p: any): p is PolyLine {
+  if (!Array.isArray(p))
+    return false;
 
   const valid = !p.some(v => !isLine(v));
   return valid;
-};
+}
 
 /**
- * Throws an exception if:
- * * line is undefined
- * * a or b parameters are missing
- * 
+ * Returns a failure if:
+ * - line is undefined
+ * - a or b parameters are missing
+ *
  * Does not validate points
- * @param line 
- * @param name 
+ * @param line
+ * @param name
  */
-export const guard = (line: Line, name = `line`): void => {
-  if (line === undefined) throw new Error(`${ name } undefined`);
-  if (line.a === undefined) throw new Error(`${ name }.a undefined. Expected {a:Point, b:Point}. Got: ${ JSON.stringify(line) }`);
-  if (line.b === undefined) throw new Error(`${ name }.b undefined. Expected {a:Point, b:Point} Got: ${ JSON.stringify(line) }`);
-};
+export function lineTest(line: Line, name = `line`): Result<boolean, string> {
+  if (line === undefined)
+    return { success: false, error: `${name} undefined` };
+  if (line.a === undefined)
+    return { success: false, error: `${name}.a undefined. Expected {a:Point, b:Point}. Got: ${JSON.stringify(line)}` };
+  if (line.b === undefined)
+    return { success: false, error: `${name}.b undefined. Expected {a:Point, b:Point} Got: ${JSON.stringify(line)}` };
+  return { success: true, value: true };
+}
