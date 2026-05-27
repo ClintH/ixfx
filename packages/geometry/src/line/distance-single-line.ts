dbafb5ed3ba8347e0d47f293402f712221c6c61f
CH <clint@thestaticvoid.net>
2026-05-27 11:22:41 +0200

    Linting

--- packages/geometry/src/line/distance-single-line.ts
+++ packages/geometry/src/line/distance-single-line.ts
@@ -1,22 +1,22 @@
 import type { Point } from "../point/point-type.js";
 import type { Line } from "./line-type.js";
-import { guard } from './guard.js';
-import { guard as guardPoint } from '../point/guard.js';
+import { resultThrow } from "@ixfx/guards";
+import { pointTest } from "../point/guard.js";
+import { lineTest } from './guard.js';
 import { length } from "./length.js";
 import { nearest } from "./nearest.js";
 /**
  * Returns the distance of `point` to the nearest point on `line`
- * 
+ *
  * ```js
  * const distance = Lines.distanceSingleLine(line, pt);
  * ```
  * @param line Line
  * @param point Target point
- * @returns 
+ * @returns
  */
-export const distanceSingleLine = (line: Line, point: Point): number => {
-  guard(line, `line`);
-  guardPoint(point, `point`);
+export function distanceSingleLine(line: Line, point: Point): number {
+  resultThrow(lineTest(line, `line`), pointTest(point, `point`));
 
   if (length(line) === 0) {
     // Line is really a point
@@ -25,4 +25,4 @@ export const distanceSingleLine = (line: Line, point: Point): number => {
 
   const near = nearest(line, point);
   return length(near, point);
-};
+}
