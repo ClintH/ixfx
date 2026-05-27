f4052fcc28d421cbc057ccdc1c68f01f71c71b1c
CH <clint@thestaticvoid.net>
2026-05-27 11:19:35 +0200

    Linting

--- packages/core/src/comparers.ts
+++ packages/core/src/comparers.ts
@@ -12,22 +12,23 @@ export type Comparer<V> = (a: V, b: V) => CompareResult;
  * [10, 4, 5, 0].sort(comparerInverse(numericComparer));
  * // Yields: [ 10, 5, 4, 0]
  * ```
- * 
+ *
  * Returns:
- * * 0: values are equal
- * * negative: `a` should be before `b`
- * * positive: `a` should come after `b`
+ * 0: values are equal
+ * negative: `a` should be before `b`
+ * positive: `a` should come after `b`
  * @param a
  * @param b
  * @returns
  */
-export const numericComparer = (a: number, b: number): CompareResult => {
+export function numericComparer(a: number, b: number): CompareResult {
   // ✔️ Unit tested
-  if (a === b) return 0;
-  if (a > b) return 1;
+  if (a === b)
+    return 0;
+  if (a > b)
+    return 1;
   return -1;
-};
-
+}
 
 /**
  * Default sort comparer, following same sematics as Array.sort.
@@ -38,7 +39,7 @@ export const numericComparer = (a: number, b: number): CompareResult => {
  * [ 10, 20, 5, 100 ].sort(jsComparer); // same as .sort()
  * // Yields: [10, 100, 20, 5]
  * ```
- * 
+ *
  * Returns -1 if x is less than y
  * Returns 1 if x is greater than y
  * Returns 0 if x is the same as y
@@ -47,42 +48,47 @@ export const numericComparer = (a: number, b: number): CompareResult => {
  * @returns
  */
 
-export const jsComparer = (x: any, y: any): CompareResult => {
+export function jsComparer(x: any, y: any): CompareResult {
   // ✔️ Unit tested
 
   // Via https://stackoverflow.com/questions/47334234/how-to-implement-array-prototype-sort-default-compare-function
-  if (x === undefined && y === undefined) return 0;
-  if (x === undefined) return 1;
-  if (y === undefined) return -1;
+  if (x === undefined && y === undefined)
+    return 0;
+  if (x === undefined)
+    return 1;
+  if (y === undefined)
+    return -1;
 
   const xString = defaultToString(x);
   const yString = defaultToString(y);
 
-  if (xString < yString) return -1;
-  if (xString > yString) return 1;
+  if (xString < yString)
+    return -1;
+  if (xString > yString)
+    return 1;
   return 0;
-};
+}
 
 /**
  * Inverts the source comparer.
  * @param comparer
  * @returns
  */
-export const comparerInverse = <V>(comparer: Comparer<V>): Comparer<V> => {
+export function comparerInverse<V>(comparer: Comparer<V>): Comparer<V> {
   return (x: V, y: V) => {
     const v = comparer(x, y);
     return v * -1;
   };
-};
+}
 
 /**
  * Compares numbers by numeric value, otherwise uses the default
  * logic of string comparison.
  *
  * Is an ascending sort:
- * * b, a, c -> a, b, c
- * * 10, 5, 100 -> 5, 10, 100
- * 
+ * b, a, c -> a, b, c
+ * 10, 5, 100 -> 5, 10, 100
+ *
  * Returns -1 if x is less than y
  * Returns 1 if x is greater than y
  * Returns 0 if x is the same as y
@@ -91,9 +97,9 @@ export const comparerInverse = <V>(comparer: Comparer<V>): Comparer<V> => {
  * @see {@link comparerInverse} Inverted order
  * @returns
  */
-export const defaultComparer = (x: any, y: any): CompareResult => {
+export function defaultComparer(x: any, y: any): CompareResult {
   if (typeof x === `number` && typeof y === `number`) {
     return numericComparer(x, y);
   }
   return jsComparer(x, y);
-};
+}
