52d32b6c583cf092e7e3fdf0485db23e8689025b
CH <clint@thestaticvoid.net>
2026-05-27 11:23:18 +0200

    Linting

--- packages/guards/src/string.ts
+++ packages/guards/src/string.ts
@@ -1,23 +1,24 @@
 import type { Result } from "./types.js";
 
-export type StringGuardRange = `` | `non-empty`
+export type StringGuardRange = `` | `non-empty`;
 
 /**
  * Throws an error if parameter is not an string
  * @param value
  * @param parameterName
  */
-export const stringTest = (value: unknown, range: StringGuardRange = ``, parameterName = `?`): Result<string, string> => {
-  if (typeof value !== `string`) return { success: false, error: `Param '${ parameterName } is not type string. Got: ${ typeof value }` };
+export function stringTest(value: unknown, range: StringGuardRange = ``, parameterName = `?`): Result<string, string> {
+  if (typeof value !== `string`)
+    return { success: false, error: `Param '${parameterName} is not type string. Got: ${typeof value}` };
   switch (range) {
     case `non-empty`:
-      if (value.length === 0) return { success: false, error: `Param '${ parameterName } is empty` };
+      if (value.length === 0)
+        return { success: false, error: `Param '${parameterName} is empty` };
       break;
   }
   return { success: true, value };
-};
+}
 
 // export const throwStringTest = (value: unknown, range: StringGuardRange = ``, parameterName = `?`) => {
 //   throwFromResult(stringTest(value, range, parameterName));
 // }
-
