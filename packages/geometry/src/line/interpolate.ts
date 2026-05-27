0f109909dc12854f21d3297e978b4f7a03279e23
CH <clint@thestaticvoid.net>
2026-05-27 11:22:56 +0200

    Improved interpolator

--- packages/geometry/src/line/interpolate.ts
+++ packages/geometry/src/line/interpolate.ts
@@ -1,21 +1,22 @@
-import { numberTest, percentTest, resultThrow } from "@ixfx/guards";
 import type { Point } from "../point/point-type.js";
 import type { Line } from "./line-type.js";
+import { numberTest, percentTest, resultThrow } from "@ixfx/guards";
 import { getPointParameter } from "./get-points-parameter.js";
+import { lineTest } from "./guard.js";
 import { length } from "./length.js";
 import { reverse } from "./reverse.js";
 /**
  * Calculates a point in-between `a` and `b`.
- * 
+ *
  * If an interpolation amount below 0 or above 1 is given, _and_
  * `allowOverflow_ is true, a point will be returned that is extended
  * past `line`. This is useful for easing functions which might
  * briefly go past the limits.
- * 
+ *
  * ```js
  * // Get {x,y} at 50% along line
  * Lines.interpolate(0.5, line);
- * 
+ *
  * // Get {x,y} at 80% between point A and B
  * Lines.interpolate(0.8, ptA, ptB);
  * ```
@@ -28,14 +29,14 @@ export function interpolate(amount: number, a: Point, pointB: Point, allowOverfl
 
 /**
  * Calculates a point in-between `line`'s start and end points.
- * 
+ *
  * ```js
  * // Get {x, y } at 50% along line
  * Lines.interpolate(0.5, line);
  * ```
- * 
+ *
  * Any additional properties from `b`  are returned on the result as well.
- * @param amount 0..1 
+ * @param amount 0..1
  * @param line Line
  * @param allowOverflow If true, interpolation amount is permitted to exceed 0..1, extending the line
  */
@@ -43,40 +44,75 @@ export function interpolate(amount: number, line: Line, allowOverflow?: boolean)
 
 /**
  * Calculates a point in-between a line's start and end points.
- * 
+ *
  * @param amount Interpolation amount
  * @param aOrLine Line, or first point
  * @param pointBOrAllowOverflow Second point (if needed) or allowOverflow.
  * @param allowOverflow If true, interpolation amount is permitted to exceed 0..1, extending the line.
- * @returns 
  */
 export function interpolate(amount: number, aOrLine: Point | Line, pointBOrAllowOverflow?: Point | boolean, allowOverflow?: boolean): Point {
-
   if (typeof pointBOrAllowOverflow === `boolean`) {
     allowOverflow = pointBOrAllowOverflow;
     pointBOrAllowOverflow = undefined;
   }
 
-
-  if (!allowOverflow) resultThrow(percentTest(amount, `amount`));
+  if (!allowOverflow)
+    resultThrow(percentTest(amount, `amount`));
   else resultThrow(numberTest(amount, ``, `amount`));
 
-  const [ a, b ] = getPointParameter(aOrLine, pointBOrAllowOverflow);
+  const [a, b] = getPointParameter(aOrLine, pointBOrAllowOverflow);
 
-  const d = length(a, b);
-  const d2 = d * (1 - amount);
+  return interpolator({ a, b }, allowOverflow)(amount);
+  // const d = length(a, b);
+  // const d2 = d * (1 - amount);
 
-  // Points are identical, return a copy of b
-  if (d === 0 && d2 === 0) return Object.freeze({ ...b });
+  // // Points are identical, return a copy of b
+  // if (d === 0 && d2 === 0)
+  //   return Object.freeze({ ...b });
 
-  const x = b.x - (d2 * (b.x - a.x) / d);
-  const y = b.y - (d2 * (b.y - a.y) / d);
+  // const x = b.x - (d2 * (b.x - a.x) / d);
+  // const y = b.y - (d2 * (b.y - a.y) / d);
 
-  return Object.freeze({
-    ...b,
-    x: x,
-    y: y
-  });
+  // return Object.freeze({
+  //   ...b,
+  //   x,
+  //   y,
+  // });
+}
+
+/**
+ * Returns a function that interpolates along `line`, returning a {@link Point}.
+ * ```js
+ * const i = interpolator(a: {x:0,y:0}, b:{x:100, y:100});
+ * i(0.5); // Returns point 50% between a and b.
+ * ```
+ * @param line Line to interpolate along
+ * @param allowOverflow If _true_ interpolation amount can exceed 0..1, extending the line.
+ */
+export function interpolator(line: Line, allowOverflow = false): (amount: number) => Point {
+  resultThrow(lineTest(line));
+  const d = length(line);
+  const { a, b } = line;
+
+  return (amount: number) => {
+    if (!allowOverflow)
+      resultThrow(percentTest(amount, `amount`));
+    else resultThrow(numberTest(amount, ``, `amount`));
+
+    const d2 = d * (1 - amount);
+
+    // Points are identical, return a copy of b
+    if (d === 0 && d2 === 0)
+      return Object.freeze({ ...b });
+    const x = b.x - (d2 * (b.x - a.x) / d);
+    const y = b.y - (d2 * (b.y - a.y) / d);
+
+    return Object.freeze({
+      ...b,
+      x,
+      y,
+    });
+  };
 }
 
 /**
@@ -84,10 +120,11 @@ export function interpolate(amount: number, aOrLine: Point | Line, pointBOrAllow
  * @param line Line
  * @param distance Distance
  * @param fromA If _true_ (default) returns from A. Use _false_ to calculate from end
- * @returns 
+ * @returns Point at distance along line
  */
 export function pointAtDistance(line: Line, distance: number, fromA = true): Point {
-  if (!fromA) line = reverse(line);
+  if (!fromA)
+    line = reverse(line);
 
   const dx = line.b.x - line.a.x;
   const dy = line.b.y - line.a.y;
