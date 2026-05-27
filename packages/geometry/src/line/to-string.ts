0a2af209b6d8d65be657b07d459b09b21ba20477
CH <clint@thestaticvoid.net>
2026-05-27 11:23:09 +0200

    Linting

--- packages/geometry/src/line/to-string.ts
+++ packages/geometry/src/line/to-string.ts
@@ -1,39 +1,41 @@
 import type { Point } from "../point/point-type.js";
-import { guard, isLine } from "./guard.js";
 import type { Line } from "./line-type.js";
+import { resultThrow } from "@ixfx/guards";
 import { toString as PointsToString } from '../point/index.js';
+import { isLine, lineTest } from "./guard.js";
 /**
  * Returns a string representation of two points
  * ```js
  * console.log(Lines.toString(a, b)));
  * ```
- * @param a 
- * @param b 
- * @returns 
+ * @param a
+ * @param b
+ * @returns String representation of two points
  */
 export function toString(a: Point, b: Point): string;
 
 /**
- * Returns a string representation of a line 
+ * Returns a string representation of a line
  * ```js
  * Lines.toString(line);
  * ```
- * @param line 
+ * @param line
  */
 export function toString(line: Line): string;
 
 /**
  * Returns a string representation of a line or two points.
  * @param a
- * @param b 
- * @returns 
+ * @param b
+ * @returns String representation of a line or two points
  */
 export function toString(a: Point | Line, b?: Point): string {
   if (isLine(a)) {
-    guard(a, `a`);
+    resultThrow(lineTest(a, `line`));
     b = a.b;
     a = a.a;
-  } else if (b === undefined) throw new Error(`Expect second point if first is a point`);
-  return PointsToString(a) + `-` + PointsToString(b);
+  } else if (b === undefined) {
+    throw new Error(`Expect second point if first is a point`);
+  }
+  return `${PointsToString(a)}-${PointsToString(b)}`;
 }
-
