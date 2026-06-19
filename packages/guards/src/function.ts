31f0931fdcc13c4be9933a911f7866f10bfcee1e
CH <clint@thestaticvoid.net>
2026-06-20 00:22:33 +0200

    Linting

--- packages/guards/src/function.ts
+++ packages/guards/src/function.ts
@@ -1,17 +1,14 @@
 import type { Result } from "./types.js";
 
-export const isFunction = (object: unknown): object is (...args: any[]) => any => object instanceof Function;
+export const isFunction = (object: unknown): object is (...args: any[]) => any => typeof object === `function`;
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-export const functionTest = (value: unknown, parameterName = `?`): Result<Function, string> => {
-  if (value === undefined) return { success: false, error: `Param '${ parameterName }' is undefined. Expected: function.` };
-  if (value === null) return { success: false, error: `Param '${ parameterName }' is null. Expected: function.` };
-  if (typeof value !== `function`) return { success: false, error: `Param '${ parameterName }' is type '${ typeof value }'. Expected: function` };
+// eslint-disable-next-line ts/no-unsafe-function-type
+export function functionTest(value: unknown, parameterName = `?`): Result<Function, string> {
+  if (value === undefined)
+    return { success: false, error: `Param '${parameterName}' is undefined. Expected: function.` };
+  if (value === null)
+    return { success: false, error: `Param '${parameterName}' is null. Expected: function.` };
+  if (typeof value !== `function`)
+    return { success: false, error: `Param '${parameterName}' is type '${typeof value}'. Expected: function` };
   return { success: true, value };
 }
-
-// export const throwFunctionTest = (value: unknown, parameterName = `?`) => {
-//   const [ ok, message ] = functionTest(value, parameterName);
-//   if (ok) return;
-//   throw new TypeError(message);
-// }
