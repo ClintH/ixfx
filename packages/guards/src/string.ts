bbafb32b5460f00668a83af89eb8c22c427a1c5d
CH <clint@thestaticvoid.net>
2026-06-20 00:22:50 +0200

    Linting

--- packages/guards/src/string.ts
+++ packages/guards/src/string.ts
@@ -4,8 +4,9 @@ export type StringGuardRange = `` | `non-empty`;
 
 /**
  * Throws an error if parameter is not an string
- * @param value
- * @param parameterName
+ * @param value Value to check
+ * @param range Range conditions, if any
+ * @param parameterName Name of parameter, for customising error message
  */
 export function stringTest(value: unknown, range: StringGuardRange = ``, parameterName = `?`): Result<string, string> {
   if (typeof value !== `string`)
