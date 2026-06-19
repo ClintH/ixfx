5c78c78ec7d4f698b5de83b97e9734cb5a89c5b4
CH <clint@thestaticvoid.net>
2026-06-20 00:22:37 +0200

    Update index.ts

--- packages/guards/src/index.ts
+++ packages/guards/src/index.ts
@@ -1,5 +1,5 @@
-
 export * from './arrays.js';
+export * from './boolean.js';
 export * from './empty.js';
 export * from './function.js';
 export * from './numbers.js';
@@ -8,4 +8,3 @@ export * from './range.js';
 export * from './result.js';
 export * from './string.js';
 export * from './types.js';
-
